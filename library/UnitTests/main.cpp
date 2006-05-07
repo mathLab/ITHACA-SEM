f66c62fa508ffbb8e635e03dc60e7c836886d649
Blake Nelson <bnelson@cs.utah.edu>
2006-05-07 15:23:29 +0000

    Added tests for boost utilities.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@12 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/UnitTests/main.cpp
+++ library/UnitTests/main.cpp
@@ -15,6 +15,7 @@ using boost::unit_test_framework::test_suite;
 
 #include <UnitTests/testNekPoint.h>
 #include <UnitTests/testNekVector.h>
+#include <UnitTests/testBoostUtil.h>
 
 // The boost unit test framework provides the main function for us.
 // All we need to do is provide a test suite.
@@ -30,11 +31,17 @@ test_suite* init_unit_test_suite( int, char* [] )
     test->add(BOOST_TEST_CASE(&Nektar::UnitTests::testNekVectorOperators), 0);
     test->add(BOOST_TEST_CASE(&Nektar::UnitTests::testNekVectorArithmetic), 0);
 
+
+    test->add(BOOST_TEST_CASE(&Nektar::UnitTests::testMakePtr), 0);
+
     return test;
 }
 
 /**
     $Log: main.cpp,v $
+    Revision 1.1  2006/05/04 18:59:55  kirby
+    *** empty log message ***
+
     Revision 1.2  2006/04/11 02:02:13  bnelson
     Added more tests.
 

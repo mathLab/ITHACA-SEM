331204b701985d12138bb79c7ac103f895f33dfa
Joe Frazier <jfrazier@nektar.info>
2006-05-23 19:40:02 +0000

    Reads a 2D mesh.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@46 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/Demos/SpatialDomains/Graph2D.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream> 
+#include "SpatialDomains/MeshGraph2D.h"
+
+using namespace Nektar;
+using namespace SpatialDomains; 
+using namespace std;
+
+// compile using Builds/Demos/SpatialDomains -> make DEBUG=1 Graph1D
+
+int main(int argc, char *argv[]){
+
+    //if(argc != 2){
+    //  cerr << "usage: Graph1D file" << endl;
+    //  exit(1);
+    //}
+
+    //string in(argv[argc-1]);
+    string in("C:\\Data\\PhD\\Research\\dev\\Nektar++\\libs\\Demos\\SpatialDomains\\meshdef2D.xml");
+    MeshGraph2D graph2D;
+
+    graph2D.Read(in);
+
+    return 0;
+}

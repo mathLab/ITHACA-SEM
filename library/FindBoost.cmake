c053d1a8467f26ca813e611661ce58f28375937b
Blake Nelson <bnelson@cs.utah.edu>
2006-08-14 02:36:04 +0000

    Added additional libraries to the search.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@118 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/FindBoost.cmake
+++ library/FindBoost.cmake
@@ -1,15 +1,71 @@
+SET(BOOST_INCLUDE_SEARCH_PATH /usr/include /usr/local/include)
 
-FIND_PATH(BOOST_INCLUDE_DIR boost/weak_ptr.hpp /usr/include /usr/local/include 
+IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
+	SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH} 
 	"C:\\Program Files\\Microsoft Visual Studio .NET 2003\\Vc7\\include"
 	c:\\Boost\\include )
+ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
 
-#FIND_LIBRARY( BOOST_UNIT_TEST_LIB NAMES boost_unit_test_framework
-#	      PATHS /usr/lib /usr/local/lib C:\\Boost\\lib )
-#FIND_LIBRARY( BOOST_PROGRAM_OPTIONS_LIB NAMES boost_program_options
-#	      PATHS /usr/lib /usr/local/lib C:\\Boost\\lib )
-#FIND_LIBRARY( BOOST_FILESYSTEM_LIB NAMES boost_filesystem
-#	      PATHS /usr/lib /usr/local/lib C:\\Boost\\lib )
+IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
+	SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH} 
+		"C:\\Program Files (x86)\\Microsoft Visual Studio 8\\VC\\include"
+		"C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include"
+		c:\\Boost\\include)
+ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
 
+FIND_PATH(BOOST_INCLUDE_DIR boost/weak_ptr.hpp ${BOOST_INCLUDE_SEARCH_PATH} )
+
+SET(BoostFileSystemName "")
+SET(BoostFileSystemDebugName "")
+
+IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
+	SET(BoostFileSystemName "libboost_filesystem-vc71-mt")
+	SET(BoostFileSystemDebugName "libboost_filesystem-vc71-mt-gd")	
+	SET(BoostThreadName "boost_thread-vc71-mt")
+	SET(BoostThreadDebugName "boost_thread-vc71-mt-gd")
+ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
+
+IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
+	SET(BoostFileSystemName "libboost_filesystem-vc80-mt")
+	SET(BoostFileSystemDebugName "libboost_filesystem-vc80-mt-gd")	
+ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
+
+IF( ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles" )
+	IF( ${CMAKE_COMPILER_IS_GNUCXX} )
+		SET(BoostFileSystemName "boost_filesystem-gcc-mt")
+		SET(BoostFileSystemDebugName "boost_filesystem-gcc-mt-d")
+	ENDIF( ${CMAKE_COMPILER_IS_GNUCXX} )
+ENDIF( ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles" )
+
+
+FIND_LIBRARY( BOOST_FILESYSTEM_LIB NAMES ${BoostFileSystemName}
+	      PATHS 
+	      ${BOOST_INCLUDE_DIR}/../lib
+	      /usr/local/lib 
+	      /usr/lib 
+	      C:\\Boost\\lib )
+
+FIND_LIBRARY( BOOST_FILESYSTEM_DEBUG_LIB NAMES ${BoostFileSystemDebugName}
+	      PATHS 
+	      ${BOOST_INCLUDE_DIR}/../lib
+	      /usr/local/lib 
+	      /usr/lib 
+	      C:\\Boost\\lib )
+
+FIND_LIBRARY( BOOST_THREAD_LIB NAMES ${BoostThreadName}
+	      PATHS 
+	      ${BOOST_INCLUDE_DIR}/../lib
+	      /usr/local/lib 
+	      /usr/lib 
+	      C:\\Boost\\lib )
+
+FIND_LIBRARY( BOOST_THREAD_DEBUG_LIB NAMES ${BoostThreadDebugName}
+	      PATHS 
+	      ${BOOST_INCLUDE_DIR}/../lib
+	      /usr/local/lib 
+	      /usr/lib 
+	      C:\\Boost\\lib )
+	      
 IF (BOOST_INCLUDE_DIR)
   SET(BOOST_FOUND TRUE)
 ENDIF (BOOST_INCLUDE_DIR)

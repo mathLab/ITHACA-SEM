f30c0f00343764153ec2c9a87a0811d9fdfab0e9
Blake Nelson <bnelson@cs.utah.edu>
2006-08-25 03:05:37 +0000

    Added support for finding the threading library on linux.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@138 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/FindBoost.cmake
+++ library/FindBoost.cmake
@@ -1,16 +1,16 @@
 SET(BOOST_INCLUDE_SEARCH_PATH /usr/include /usr/local/include)
 
 IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
-	SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH} 
-	"C:\\Program Files\\Microsoft Visual Studio .NET 2003\\Vc7\\include"
-	c:\\Boost\\include )
+    SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH}
+    "C:\\Program Files\\Microsoft Visual Studio .NET 2003\\Vc7\\include"
+    c:\\Boost\\include )
 ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
 
 IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
-	SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH} 
-		"C:\\Program Files (x86)\\Microsoft Visual Studio 8\\VC\\include"
-		"C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include"
-		c:\\Boost\\include)
+    SET(BOOST_INCLUDE_SEARCH_PATH ${BOOST_INCLUDE_SEARCH_PATH}
+        "C:\\Program Files (x86)\\Microsoft Visual Studio 8\\VC\\include"
+        "C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include"
+        c:\\Boost\\include)
 ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
 
 FIND_PATH(BOOST_INCLUDE_DIR boost/weak_ptr.hpp ${BOOST_INCLUDE_SEARCH_PATH} )
@@ -19,53 +19,55 @@ SET(BoostFileSystemName "")
 SET(BoostFileSystemDebugName "")
 
 IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
-	SET(BoostFileSystemName "libboost_filesystem-vc71-mt")
-	SET(BoostFileSystemDebugName "libboost_filesystem-vc71-mt-gd")	
-	SET(BoostThreadName "boost_thread-vc71-mt")
-	SET(BoostThreadDebugName "boost_thread-vc71-mt-gd")
+    SET(BoostFileSystemName "libboost_filesystem-vc71-mt")
+    SET(BoostFileSystemDebugName "libboost_filesystem-vc71-mt-gd")
+    SET(BoostThreadName "boost_thread-vc71-mt")
+    SET(BoostThreadDebugName "boost_thread-vc71-mt-gd")
 ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 7 .NET 2003" )
 
 IF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
-	SET(BoostFileSystemName "libboost_filesystem-vc80-mt")
-	SET(BoostFileSystemDebugName "libboost_filesystem-vc80-mt-gd")	
+    SET(BoostFileSystemName "libboost_filesystem-vc80-mt")
+    SET(BoostFileSystemDebugName "libboost_filesystem-vc80-mt-gd")
 ENDIF( ${CMAKE_GENERATOR} STREQUAL "Visual Studio 8 2005" )
 
 IF( ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles" )
-	IF( ${CMAKE_COMPILER_IS_GNUCXX} )
-		SET(BoostFileSystemName "boost_filesystem-gcc-mt")
-		SET(BoostFileSystemDebugName "boost_filesystem-gcc-mt-d")
-	ENDIF( ${CMAKE_COMPILER_IS_GNUCXX} )
+    IF( ${CMAKE_COMPILER_IS_GNUCXX} )
+        SET(BoostFileSystemName "boost_filesystem-gcc-mt")
+        SET(BoostFileSystemDebugName "boost_filesystem-gcc-mt-d")
+        SET(BoostThreadName "boost_thread-gcc-mt")
+        SET(BoostThreadDebugName "boost_thread-gcc-mt-d")
+    ENDIF( ${CMAKE_COMPILER_IS_GNUCXX} )
 ENDIF( ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles" )
 
 
 FIND_LIBRARY( BOOST_FILESYSTEM_LIB NAMES ${BoostFileSystemName}
-	      PATHS 
-	      ${BOOST_INCLUDE_DIR}/../lib
-	      /usr/local/lib 
-	      /usr/lib 
-	      C:\\Boost\\lib )
+          PATHS
+          ${BOOST_INCLUDE_DIR}/../lib
+          /usr/local/lib
+          /usr/lib
+          C:\\Boost\\lib )
 
 FIND_LIBRARY( BOOST_FILESYSTEM_DEBUG_LIB NAMES ${BoostFileSystemDebugName}
-	      PATHS 
-	      ${BOOST_INCLUDE_DIR}/../lib
-	      /usr/local/lib 
-	      /usr/lib 
-	      C:\\Boost\\lib )
+          PATHS
+          ${BOOST_INCLUDE_DIR}/../lib
+          /usr/local/lib
+          /usr/lib
+          C:\\Boost\\lib )
 
 FIND_LIBRARY( BOOST_THREAD_LIB NAMES ${BoostThreadName}
-	      PATHS 
-	      ${BOOST_INCLUDE_DIR}/../lib
-	      /usr/local/lib 
-	      /usr/lib 
-	      C:\\Boost\\lib )
+          PATHS
+          ${BOOST_INCLUDE_DIR}/../lib
+          /usr/local/lib
+          /usr/lib
+          C:\\Boost\\lib )
 
 FIND_LIBRARY( BOOST_THREAD_DEBUG_LIB NAMES ${BoostThreadDebugName}
-	      PATHS 
-	      ${BOOST_INCLUDE_DIR}/../lib
-	      /usr/local/lib 
-	      /usr/lib 
-	      C:\\Boost\\lib )
-	      
+          PATHS
+          ${BOOST_INCLUDE_DIR}/../lib
+          /usr/local/lib
+          /usr/lib
+          C:\\Boost\\lib )
+
 IF (BOOST_INCLUDE_DIR)
   SET(BOOST_FOUND TRUE)
 ENDIF (BOOST_INCLUDE_DIR)

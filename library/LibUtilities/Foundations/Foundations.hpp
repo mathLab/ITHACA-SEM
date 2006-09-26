1c1f25a8c6b27e87fd561f7e11760f03e559c54f
Mike Kirby <kirby@sci.utah.edu>
2006-09-26 18:11:43 +0000

    *** empty log message ***
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@162 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/LibUtilities/Foundations/Foundations.hpp
@@ -0,0 +1,91 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File Foundations.hpp
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: Definition of enum lists and constants
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef FOUNDATIONS_H
+#define FOUNDATIONS_H
+
+#include <string>
+
+namespace Nektar
+{
+    namespace LibUtiltities
+    {
+        enum BasisType
+        {
+            eOrtho_A,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^a_p(z_i)\f$
+            eOrtho_B,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^b_{pq}(z_i)\f$
+            eOrtho_C,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^c_{pqr}(z_i)\f$
+            eModified_A,  //!< Principle Modified Functions \f$ \phi^a_p(z_i) \f$
+            eModified_B,  //!< Principle Modified Functions \f$ \phi^b_{pq}(z_i) \f$
+            eModified_C,  //!< Principle Modified Functions \f$ \phi^c_{pqr}(z_i) \f$
+            eFourier,     //!< Fourier Expansion \f$ \exp(i p\pi  z_i)\f$
+            eGLL_Lagrange,//!< Lagrange for SEM basis \f$ h_p(z_i) \f$
+            eLegendre,    //!< Legendre Polynomials \f$ L_p(z_i) = P^{0,0}_p(z_i)\f$. Same as Ortho_A
+            eChebyshev,   //!< Chebyshev Polynomials \f$ T_p(z_i) = P^{-1/2,-1/2}_p(z_i)\f$
+            SIZE_BasisType //!< Length of enum list
+        };
+    
+
+        enum PointsType
+        {
+            eGauss,               //!< 1D Gauss quadrature points
+            eLobatto,             //!< 1D Gauss Lobatto quadrature points
+            eRadauM,              //!< 1D Gauss Radau quadrature points including (z=-1)
+            eRadauP,              //!< 1D Gauss Radau quadrature points including (z=+1)
+            ePolyEvenSp,          //!< 1D Evenly-spaced points using Lagrange polynomial
+            eFourierEvenSp,       //!< 1D Evenly-spaced points using Fourier Fit
+            eArbitrary,           //!< 1D Arbitrary point distribution
+            eNodalTriElec,        //!< 2D Nodal Electrostatic Points on a Triangle
+            eNodalTriFekete,      //!< 2D Nodal Fekete Points on a Triangle
+            eNodalTetElec,        //!< 3D Nodal Electrostatic Points on a Tetrahedron
+            SIZE_PointsType       //!< Length of enum list
+        };
+
+        enum PointsIdentifier
+        {
+            eWildcard,             //!< Indentifier to use when PointType is sufficient to uniquely specify things
+            eChebyshevFirstKind,   //!< Gauss \f$ \alpha = -1/2, \beta = -1/2} \f$
+            eChebyshevSecondKind,  //!< Gauss \f$ \alpha =  1/2, \beta =  1/2} \f$
+            eLegendre,             //!< Gauss \f$ \alpha =    0, \beta =    0} \f$
+            eGaussAlpha0Beta1,     //!< Gauss \f$ \alpha =    0, \beta =    1} \f$
+            eGaussAlpha0Beta2,     //!< Gauss \f$ \alpha =    0, \beta =    2} \f$
+            SIZE_PointsIdentifier  //!< Length of enum list
+        };
+
+    } // end of namespace
+} // end of namespace
+
+#endif //FOUNDATIONS_H
+
---
+++ library/LibUtilities/Foundations/Points.hpp
@@ -0,0 +1,307 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: Points.hpp
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// 
+// Description: Header file of Points definition 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef POINTS_HPP
+#define POINTS_HPP
+
+#include <math.h>
+#include <LibUtilities/BasicUtils/ErrorUtil.hpp>
+#include <LibUtilities/LinearAlgebra/NekMatrix.hpp>
+
+namespace Nektar
+{
+    namespace LibUtilities
+    {
+
+        typedef boost::shared_ptr<NekMatrix> NekMatrixSharedPtr;
+
+        class PointsKey
+        {
+        public:
+
+            PointsKey()
+            {
+                NEKERROR(ErrorUtil::efatal,"Default Constructor for PointsKey should not be called");
+            }
+
+            PointsKey(int numpoints, PointsType pointstype, PointsIdentifier pointsid)
+            {
+                m_numpoints = numpoints;
+                m_pointstype = pointstype;
+                m_pointsid = pointsid;
+            }
+
+            virtual ~PointsKey()
+            {
+            }
+
+            PointsKey(const PointsKey &key)
+            {
+                *this = key;
+            }
+
+            PointsKey& operator=(const PointsKey &key)
+            {
+                m_numpoints = key.m_numpoints;
+                m_pointstype = key.m_pointstype;
+                m_pointsid = key.m_pointsid;
+                return *this;
+            }
+
+            inline int GetNumPoints() const
+            {
+                return m_numpoints;
+            }
+
+            inline PointsType GetPointsType() const
+            {
+                return m_pointstype;
+            }
+
+            inline PointsIdentifier GetPointsId() const
+            {
+                return m_pointsid;
+            }
+
+        protected:
+            int m_numpoints;              //!< Number of points
+            PointsType m_pointstype       //!< Type of Points
+            PointsIdentifier m_pointsid;  //!< Unique indentifier (when needed) 
+        };
+
+        template<typename DataType, unsigned int dim>
+        class Points
+        {
+        public:
+            Points()
+            {
+                NEKERROR(ErrorUtil::efatal,"Default Constructor for Points should not be called");
+            }
+
+            Points(const PointsKey &key)
+            {
+                m_pkey = key;
+
+                     for(unsigned int i = 0; i < dim; ++i)
+                    {
+                        delete[] m_points[i];
+                    }
+                    delete[] m_weights;
+                    key.m_numpoints = 0;
+                }
+
+            }
+
+
+           friend bool operator  == (const BasisKey& x, const BasisKey& y);
+
+            virtual ~Points()
+            {
+                if(m_numpoints)
+                {
+                    for(unsigned int i = 0; i < dim; ++i)
+                    {
+                        delete[] m_points[i];
+                    }
+                    delete[] m_weights;
+                    key.m_numpoints = 0;
+                }
+            }
+  
+            inline int GetNumPoints() const
+            {
+                return key.GetNumPoints();
+            }
+
+            inline PointsType GetPointsType() const
+            {
+                return key.GetPointsType();
+            }
+
+            inline PointsIdentifier GetPointsId() const
+            {
+                return key.GetPointsId();
+            }
+
+            inline double *GetZ() const
+            {
+                BOOST_STATIC_ASSERT(dim == 1);
+                return m_points[0];
+            }
+
+            inline double *GetW() const 
+            {
+                return m_weights; 
+            } 
+      
+            inline void GetZW(const double *&z, const double *&w) const 
+            {
+                BOOST_STATIC_ASSERT(dim == 1);
+	            z = m_points[0];
+            	w = m_weights;
+            }
+
+            inline void GetPoints(const double *&x) const
+            {
+                BOOST_STATIC_ASSERT(dim == 1);
+                x = m_points[0];
+            }
+
+            inline void GetPoints(const double *&x, const double *&y) const
+            {
+                BOOST_STATIC_ASSERT(dim == 2);
+                x = m_points[0];
+                y = m_points[1];
+            }
+
+            inline void GetPoints(const double *&x, const double *&y, const double *&z) const
+            {
+                BOOST_STATIC_ASSERT(dim == 3);
+                x = m_points[0];
+                y = m_points[1];
+                z = m_points[2];
+            }
+
+            inline const NekMatrix<DataType,FULL> *GetD() const
+            {
+                return m_derivmatrix;
+            }
+        
+        protected:
+            PointsKey m_pointskey;
+            DataType * m_points[dim];
+            DataType * m_weights;
+            NekMatrix<DataType,eFull> m_derivmatrix;
+
+        private:
+            virtual void CalculatePoints() = 0;
+            virtual void CalculateWeights() = 0;
+            virtual void CalculateDerivMatrix() = 0;
+        };
+
+
+        bool operator == (const PointsKey& x, const PointsKey& y)
+        {
+            if( (x.m_numpoints == y.m_numpoints) &&
+                (x.m_pointstype == y.m_pointstype) &&
+                (x.m_pointsid == y.m_pointsid) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        bool operator == (const PointsKey* x, const PointsKey& y)
+        {
+            if( ((*x).m_numpoints == y.m_numpoints) &&
+                ((*x).m_pointstype == y.m_pointstype) &&
+                ((*x).m_pointsid == y.m_pointsid) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        bool operator == (const PointsKey& x, const PointsKey *y)
+        {
+            if( (x.m_numpoints == (*y).m_numpoints) &&
+                (x.m_pointstype == (*y).m_pointstype) &&
+                (x.m_pointsid == (*y).m_pointsid) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        bool operator != (const PointsKey& x, const PointsKey& y)
+        {
+            if( (x.m_numpoints == y.m_numpoints) &&
+                (x.m_pointstype == y.m_pointstype) &&
+                (x.m_pointsid == y.m_pointsid) )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        bool operator != (const PointsKey* x, const PointsKey& y)
+        {
+            if( ((*x).m_numpoints == y.m_numpoints) &&
+                ((*x).m_pointstype == y.m_pointstype) &&
+                ((*x).m_pointsid == y.m_pointsid) )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        bool operator != (const PoinstKey& x, const PointsKey *y)
+        {
+            if( (x.m_numpoints == (*y).m_numpoints) &&
+                (x.m_pointstype == (*y).m_pointstype) &&
+                (x.m_pointsid == (*y).m_pointsid) )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    } // end of namespace
+} // end of namespace 
+
+#endif //POINTS_HPP

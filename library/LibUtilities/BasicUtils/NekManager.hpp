08badaaf7bfe460f10babc64e1d218a6112f0362
Blake Nelson <bnelson@cs.utah.edu>
2006-12-10 21:35:43 +0000

    Changed the interface, removed the possibility of multiple objects per key.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@210 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/LibUtilities/BasicUtils/NekManager.hpp
+++ library/LibUtilities/BasicUtils/NekManager.hpp
@@ -1,104 +1,132 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: NekManager.hpp
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef NEKTAR_LIB_UTILITIES_BASIC_UTILS_NEK_MANAGER_HPP
+#define NEKTAR_LIB_UTILITIES_BASIC_UTILS_NEK_MANAGER_HPP
+
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 #include <functional> // for bind1st
 #include <iostream>
 
 #include <boost/mem_fn.hpp>
 #include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/call_traits.hpp>
 
 #include <LibUtilities/BasicUtils/ErrorUtil.hpp>
 
 using namespace std;
 
 template <typename keyT, typename valueT,
-    typename valueContainer = vector< pair<keyT, valueT> >,
-    typename creatorContainer = vector< pair<keyT, valueT (*)(const keyT &key)> > >
+    typename valueContainer = std::map<keyT, valueT>,
+    typename creatorContainer = std::map<keyT, boost::function<valueT (const keyT& key)> > >
 class NekManager
 {
-public:
-    typedef std::pair<keyT, valueT> KeyValuePair;
-    typedef valueT (*CreateFunc)(const keyT &key);
-    typedef std::pair<keyT, CreateFunc> KeyCreatorPair;
+    public:
+        typedef boost::function<valueT (const keyT& key)> CreateFunc;
 
-    NekManager() {};
-    ~NekManager()
-	{
-		m_keyValues.clear();
-		m_keyCreators.clear();
-	};
-
-    void RegisterCreator(keyT key, CreateFunc createFunc)
-    {
-        KeyCreatorPair createPair;
-
-        createPair.first = key;
-        createPair.second = createFunc;
-        m_keyCreators.push_back(createPair);
-    }
-
-    void AddValue(keyT key, valueT value)
-    {
-		KeyValuePair keyValuePair(key, value);
-
-        m_keyValues.push_back(keyValuePair);
-    }
-
-    valueT GetValue(const keyT key)
-    {
-        valueT returnval;
-
-        // Search the values first, if not there
-        // find a creator.  If no creator is found,
-        // it is a fatal error.  If a creator is found
-        // use the creator to create a value, store it,
-        // then return the value.
-
-        valueContainer::iterator loc;
-
-        loc = find_if(m_keyValues.begin(), m_keyValues.end(),
-            boost::bind<bool>(&NekManager<keyT, valueT>::IsFound<valueT>, _1, key));
-
-        if (loc != m_keyValues.end())
+        NekManager() :
+            m_Values(), 
+            m_CreateFuncs()
         {
-            returnval = (*loc).second;
+        };
+        
+        ~NekManager() {}
+	
+        void RegisterCreator(typename boost::call_traits<keyT>::const_reference key, 
+                             const CreateFunc& createFunc)
+        {
+            m_CreateFuncs[key] = createFunc;
         }
-        else
+
+        void AddValue(typename boost::call_traits<keyT>::const_reference key, 
+                      typename boost::call_traits<valueT>::const_reference value)
         {
-			// Not found.  Try to create one using the registered creator.
-            creatorContainer::iterator creatorIter;
-
-            creatorIter = find_if(m_keyCreators.begin(), m_keyCreators.end(),
-                boost::bind<bool>(&NekManager<keyT, valueT>::IsFound<CreateFunc>, _1, key));
-
-            if (creatorIter != m_keyCreators.end())
+            typename valueContainer::iterator found = m_Values.find(key);
+            if( found == m_Values.end() )
             {
-				// Create the instance using the registered creator.
-                returnval = (*((*creatorIter).second))(key);
-
-                KeyValuePair pairT;
-                pairT.first = key;
-                pairT.second = returnval;
-                m_keyValues.push_back(pairT);
+                m_Values[key] = value;
             }
             else
             {
-				// No creator registered for this key.
-				NEKERROR(ErrorUtil::efatal, "Unable to find a creator registered for key.");
+                std::string errorMessage = "Adding an object for key " +
+                        boost::lexical_cast<std::string>(key) + 
+                        " to a NekManager, but a value for this key already exists.";
+
+                throw std::runtime_error(errorMessage);
             }
         }
 
-        return returnval;
-    }
+        typename boost::call_traits<valueT>::reference 
+        GetValue(typename boost::call_traits<keyT>::const_reference key)
+        {
+            typename valueContainer::iterator found = m_Values.find(key);
+            if( found == m_Values.end() )
+            {
+                // No object, create a new one.
+                typename creatorContainer::iterator keyFound = m_CreateFuncs.find(key);
+                if( keyFound != m_CreateFuncs.end() )
+                {
+                    valueT newObj = (*keyFound).second(key);
+                    m_Values[key] = newObj;
+                    return m_Values[key];
+                }
+                else
+                {
+                    std::string errorMessage = "Attempting to obtain an object for key " +
+                            boost::lexical_cast<std::string>(key) + 
+                            " in a NekManager but no create function has been registered";
+                    throw std::runtime_error(errorMessage);
+                }
+            }
+            else
+            {
+                return (*found).second;
+            }
+        }
 
-private:
-	template <typename itemT>
-    static bool IsFound(const pair<keyT, itemT> itemPair, keyT key) 
-    {
-        return (itemPair.first == key);
-    }
+    private:
+        NekManager(const NekManager<keyT, valueT, valueContainer, creatorContainer>& rhs);
+        NekManager<keyT, valueT, valueContainer, creatorContainer>& operator=(const NekManager<keyT, valueT, valueContainer, creatorContainer>& rhs);
 
-    valueContainer m_keyValues;
-    creatorContainer m_keyCreators;
+        valueContainer m_Values;
+        creatorContainer m_CreateFuncs;
 };
+
+#endif //NEKTAR_LIB_UTILITIES_BASIC_UTILS_NEK_MANAGER_HPP

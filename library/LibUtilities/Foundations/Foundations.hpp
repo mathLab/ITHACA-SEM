0e5f683bd2c946bd91519488c681455937aa38f7
Mike Kirby <kirby@sci.utah.edu>
2007-01-20 20:32:11 +0000

    *** empty log message ***
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@270 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/LibUtilities/Foundations/Foundations.hpp
+++ library/LibUtilities/Foundations/Foundations.hpp
@@ -42,16 +42,16 @@ namespace Nektar
     {
         enum BasisType
         {
-            eOrtho_A,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^a_p(z_i)\f$
-            eOrtho_B,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^b_{pq}(z_i)\f$
-            eOrtho_C,     //!< Principle Orthogonal Functions \f$\widetilde{\psi}^c_{pqr}(z_i)\f$
-            eModified_A,  //!< Principle Modified Functions \f$ \phi^a_p(z_i) \f$
-            eModified_B,  //!< Principle Modified Functions \f$ \phi^b_{pq}(z_i) \f$
-            eModified_C,  //!< Principle Modified Functions \f$ \phi^c_{pqr}(z_i) \f$
-            eFourier,     //!< Fourier Expansion \f$ \exp(i p\pi  z_i)\f$
-            eGLL_Lagrange,//!< Lagrange for SEM basis \f$ h_p(z_i) \f$
-            eLegendre,    //!< Legendre Polynomials \f$ L_p(z_i) = P^{0,0}_p(z_i)\f$. Same as Ortho_A
-            eChebyshev,   //!< Chebyshev Polynomials \f$ T_p(z_i) = P^{-1/2,-1/2}_p(z_i)\f$
+            eOrtho_A,      //!< Principle Orthogonal Functions \f$\widetilde{\psi}^a_p(z_i)\f$
+            eOrtho_B,      //!< Principle Orthogonal Functions \f$\widetilde{\psi}^b_{pq}(z_i)\f$
+            eOrtho_C,      //!< Principle Orthogonal Functions \f$\widetilde{\psi}^c_{pqr}(z_i)\f$
+            eModified_A,   //!< Principle Modified Functions \f$ \phi^a_p(z_i) \f$
+            eModified_B,   //!< Principle Modified Functions \f$ \phi^b_{pq}(z_i) \f$
+            eModified_C,   //!< Principle Modified Functions \f$ \phi^c_{pqr}(z_i) \f$
+            eFourier,      //!< Fourier Expansion \f$ \exp(i p\pi  z_i)\f$
+            eGLL_Lagrange, //!< Lagrange for SEM basis \f$ h_p(z_i) \f$
+            eLegendre,     //!< Legendre Polynomials \f$ L_p(z_i) = P^{0,0}_p(z_i)\f$. Same as Ortho_A
+            eChebyshev,    //!< Chebyshev Polynomials \f$ T_p(z_i) = P^{-1/2,-1/2}_p(z_i)\f$
             SIZE_BasisType //!< Length of enum list
         };
 
@@ -71,13 +71,18 @@ namespace Nektar
 
         enum PointsType
         {
-            eGauss,               //!< 1D Gauss quadrature points
-            eLobatto,             //!< 1D Gauss Lobatto quadrature points
-            eRadauM,              //!< 1D Gauss Radau quadrature points including (z=-1)
-            eRadauP,              //!< 1D Gauss Radau quadrature points including (z=+1)
-            ePolyEvenSp,          //!< 1D Evenly-spaced points using Lagrange polynomial
-            eFourierEvenSp,       //!< 1D Evenly-spaced points using Fourier Fit
-            eArbitrary,           //!< 1D Arbitrary point distribution
+            eGaussGaussLegendre,     //!< 1D Gauss quadrature points
+            eGaussRadauMLegendre,
+            eGaussRadauPLegendre,
+            eGaussLobattoLegendre,  //!< 1D Gauss quadrature points
+            eGaussGaussChebyshev,
+            eGaussRadauMChebyshev,
+            eGaussRadauPChebyshev,
+            eGaussLobattoChebyshev,  //!< 1D Gauss quadrature points
+            eGaussRadauMAlpha0Beta1,          //!< Gauss \f$ \alpha =    0, \beta =    1 \f$
+            eGaussRadauMAlpha0Beta2,          //!< Gauss \f$ \alpha =    0, \beta =    2 \f$
+            ePolyEvenlySpaced,          //!< 1D Evenly-spaced points using Lagrange polynomial
+            eFourierEvenlySpaced,       //!< 1D Evenly-spaced points using Fourier Fit
             eNodalTriElec,        //!< 2D Nodal Electrostatic Points on a Triangle
             eNodalTriFekete,      //!< 2D Nodal Fekete Points on a Triangle
             eNodalTetElec,        //!< 3D Nodal Electrostatic Points on a Tetrahedron
@@ -86,37 +91,8 @@ namespace Nektar
 
         const char* const PointsTypeMap[] = 
         {
-            "Gauss",
-            "Lobatto",
-            "RadauM",
-            "RadauP",
-            "PolyEvenSp",
-            "FourierEvenSp",
-            "Arbitrary",
-            "NodalTriElec",
-            "NodalTriFekete",
-            "NodalTetElec"
-        };
-
-        enum PointsIdentifier
-        {
-            eWildcard,                  //!< Indentifier to use when PointType is sufficient to uniquely specify things
-            eGaussChebyshevFirstKind,   //!< Gauss \f$ \alpha = -1/2, \beta = -1/2 \f$
-            eGaussChebyshevSecondKind,  //!< Gauss \f$ \alpha =  1/2, \beta =  1/2 \f$
-            eGaussLegendre,             //!< Gauss \f$ \alpha =    0, \beta =    0 \f$
-            eGaussAlpha0Beta1,          //!< Gauss \f$ \alpha =    0, \beta =    1 \f$
-            eGaussAlpha0Beta2,          //!< Gauss \f$ \alpha =    0, \beta =    2 \f$
-            SIZE_PointsIdentifier       //!< Length of enum list
-        };
-
-        const char* const PointsIdentifierMap[] = 
-        {
-            "Wildcard",
-            "GaussChebyshevFirstKind",
-            "GaussChebyshevSecondKind",
-            "GaussLegendre",
-            "GaussAlpha0Beta1",
-            "GaussAlpha0Beta2"
+         
+            
         };
     } // end of namespace
 } // end of namespace

57f6ba584bafe960d1db069ecb5f54cf835fdcdf
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2007-03-04 20:42:14 +0000

    Keys for matrix managers
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@402 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/LocalRegions/MatrixKey.cpp
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File MatrixKey.cpp
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: Definition of MatrixKey based on StdMatrixKey
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include <LocalRegions/MatrixKey.h>
+
+namespace Nektar
+{
+    namespace LocalRegions
+    {
+	
+	// Register Mass Matrix creator. 
+	MatrixKey::MatrixKey(StdRegions::MatrixType matrixType,
+			     StdRegions::ShapeType shapeType,
+			     StdRegions::StdExpansion &stdExpansion)
+	{
+	    
+	    m_stdMatKey =  MemoryManager::AllocateSharedPtr<StdRegions::StdMatrixKey>(matrixType,shapeType,stdExpansion);
+
+	    //m_stdMatKey.reset(new StdRegions::StdMatrixKey(matrixType,shapeType,stdExpansion));	    
+	    m_minfo = stdExpansion.GetMinfo(); 
+
+	}
+	    
+
+        bool MatrixKey::opLess::operator()(const MatrixKey &lhs, const MatrixKey &rhs)
+        {	    
+	    {
+		return (lhs.GetMatrixType() < rhs.GetMatrixType());
+	    }
+	}
+
+	bool operator<(const MatrixKey &lhs, const MatrixKey &rhs)
+	{
+	    int i;
+
+	    
+	    if(lhs.GetMatrixType() < rhs.GetMatrixType())
+	    {
+		return true;
+	    }
+
+	    if(lhs.GetMatrixType() > rhs.GetMatrixType())
+	    {
+		return false;
+	    }
+	    
+	    if(lhs.GetNcoeffs() < rhs.GetNcoeffs())
+	    {
+		return true;
+	    }
+	    
+	    if(lhs.GetNcoeffs() > rhs.GetNcoeffs())
+	    {
+		return false;
+	    }
+	    
+	    if(lhs.m_minfo < rhs.m_minfo)
+	    {
+		return true;
+	    }
+
+	    for(i = 0; i < StdRegions::ShapeTypeDimMap[lhs.GetShapeType()]; ++i)
+	    {
+		if(lhs.GetBase()[i] < rhs.GetBase()[i])
+		{
+		    return true;
+		}
+	    }
+	    
+	    return false;
+	}
+
+        std::ostream& operator<<(std::ostream& os, const MatrixKey& rhs)
+        {
+
+            os << *rhs.GetStdMatKey();
+	    
+            return os;
+        }
+    }
+}
+
+/**
+* $Log: $
+***/
+
---
+++ library/LocalRegions/MatrixKey.h
@@ -0,0 +1,122 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File MatrixKeys.h
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: Headers for MatrixKey
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef MATRIXKEY_H
+#define MATRIXKEY_H
+
+#include <LocalRegions/LocalRegions.hpp>
+#include <StdRegions/StdMatrixKey.h>
+#include <LocalRegions/MetricRelatedInfo.h>
+
+namespace Nektar
+{
+    namespace LocalRegions
+    {
+	
+        class MatrixKey
+        {
+        public:
+            MatrixKey( StdRegions::MatrixType matrixType, 
+		       StdRegions::ShapeType shapeType, 
+		       StdRegions::StdExpansion &stdExpansion);
+	    
+            virtual ~MatrixKey()
+            {
+            }
+
+            /// Used to lookup the create function in NekManager.
+            struct opLess
+            {
+                bool operator()(const MatrixKey &lhs, const MatrixKey &rhs);
+            };
+
+            /// Used for finding value given the key in NekManager.
+            friend bool operator<(const MatrixKey &lhs, const MatrixKey &rhs);
+            friend bool opLess::operator()(const MatrixKey &lhs, 
+					   const MatrixKey &rhs);
+
+	    StdRegions::MatrixType GetMatrixType() const
+	    {
+		return m_stdMatKey->GetMatrixType(); 
+	    }
+
+	    StdRegions::ShapeType GetShapeType() const
+	    {
+		return m_stdMatKey->GetShapeType();
+	    }
+	    
+	    int GetNcoeffs() const
+	    {
+		return m_stdMatKey->GetNcoeffs();
+	    }
+
+	    boost::shared_array<LibUtilities::BasisSharedPtr> GetBase() const
+	    {
+		return m_stdMatKey->GetBase();
+	    }
+	    
+	    StdRegions::StdMatrixKeySharedPtr GetStdMatKey() const 
+	    {
+		return m_stdMatKey;
+	    }
+
+	    MetricRelatedInfoSharedPtr GetMinfo() const
+	    {
+		return m_minfo;
+	    }
+
+	protected:
+            MatrixKey();
+	    
+	    StdRegions::StdMatrixKeySharedPtr  m_stdMatKey;
+	    MetricRelatedInfoSharedPtr  m_minfo; 
+	    
+        private:
+        };
+
+        std::ostream& operator<<(std::ostream& os, const MatrixKey& rhs);
+	
+    } // end of namespace
+} // end of namespace
+
+#endif //STDMATRIXKEY_H
+
+/**
+* $Log: StdMatrixKey.h,v $
+* Revision 1.1  2007/02/28 19:05:11  sherwin
+* Moved key definitions to their own files to make things more transparent
+*
+*
+***/

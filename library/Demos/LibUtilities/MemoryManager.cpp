82ee2fcf2d28c79693b0fccbec16ba15ed3196a3
Blake Nelson <bnelson@cs.utah.edu>
2007-05-27 16:12:09 +0000

    Commented everything out since none of it is valid anymore.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@586 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/Demos/LibUtilities/MemoryManager.cpp
+++ library/Demos/LibUtilities/MemoryManager.cpp
@@ -48,44 +48,44 @@ class Enabled
 
 int main()
 {
-    // Object allocation.
-    Disabled* t1 = MemoryManager::Allocate<Disabled>();
-    MemoryManager::Deallocate(t1);
-    assert(t1 == NULL);
-
-    const Disabled* t2 = MemoryManager::Allocate<Disabled>();
-    MemoryManager::Deallocate(t2);
-    assert(t2 == NULL);
-
-    Enabled* t3 = MemoryManager::Allocate<Enabled>();
-    MemoryManager::Deallocate(t3);
-    assert(t3 == NULL);
-
-    boost::shared_ptr<Disabled> t4 = MemoryManager::AllocateSharedPtr<Disabled>();
-    boost::shared_ptr<Enabled> t5 = MemoryManager::AllocateSharedPtr<Enabled>();
-
-    // This doesn't compile, it really doesn't make sense to allow it to.
-    //double* d = MemoryManager::Allocate<double>();
-
-    cout << "\nDouble Array." << endl;
-    double* d = MemoryManager::AllocateArray<10, double>();
-    MemoryManager::DeallocateArray<10>(d);
-
-    cout << "\nDisabled Array" << endl;
-    Disabled* a1 = MemoryManager::AllocateArray<2, Disabled>();
-    MemoryManager::DeallocateArray<2>(a1);
-
-    cout << "\nEnabled Array." << endl;
-    Enabled* a2 = MemoryManager::AllocateArray<3, Enabled>();
-    MemoryManager::DeallocateArray<3>(a2);
-
-    boost::shared_array<Disabled> a3 = MemoryManager::AllocateSharedArray<3, Disabled>();
-    boost::shared_array<Enabled> a4 = MemoryManager::AllocateSharedArray<4, Enabled>();
-    boost::shared_array<double> a5 = MemoryManager::AllocateSharedArray<5, double>();
-
-    boost::shared_array<double> a6 = MemoryManager::AllocateSharedArray<double>(12);
-
-    cout << "\nDestroy all shared pointers." << endl;
+//     // Object allocation.
+//     Disabled* t1 = MemoryManager::Allocate<Disabled>();
+//     MemoryManager::Deallocate(t1);
+//     assert(t1 == NULL);
+// 
+//     const Disabled* t2 = MemoryManager::Allocate<Disabled>();
+//     MemoryManager::Deallocate(t2);
+//     assert(t2 == NULL);
+// 
+//     Enabled* t3 = MemoryManager::Allocate<Enabled>();
+//     MemoryManager::Deallocate(t3);
+//     assert(t3 == NULL);
+// 
+//     boost::shared_ptr<Disabled> t4 = MemoryManager::AllocateSharedPtr<Disabled>();
+//     boost::shared_ptr<Enabled> t5 = MemoryManager::AllocateSharedPtr<Enabled>();
+// 
+//     // This doesn't compile, it really doesn't make sense to allow it to.
+//     //double* d = MemoryManager::Allocate<double>();
+// 
+//     cout << "\nDouble Array." << endl;
+//     double* d = MemoryManager::AllocateArray<10, double>();
+//     MemoryManager::DeallocateArray<10>(d);
+// 
+//     cout << "\nDisabled Array" << endl;
+//     Disabled* a1 = MemoryManager::AllocateArray<2, Disabled>();
+//     MemoryManager::DeallocateArray<2>(a1);
+// 
+//     cout << "\nEnabled Array." << endl;
+//     Enabled* a2 = MemoryManager::AllocateArray<3, Enabled>();
+//     MemoryManager::DeallocateArray<3>(a2);
+// 
+//     boost::shared_array<Disabled> a3 = MemoryManager::AllocateSharedArray<3, Disabled>();
+//     boost::shared_array<Enabled> a4 = MemoryManager::AllocateSharedArray<4, Enabled>();
+//     boost::shared_array<double> a5 = MemoryManager::AllocateSharedArray<5, double>();
+// 
+//     boost::shared_array<double> a6 = MemoryManager::AllocateSharedArray<double>(12);
+// 
+//     cout << "\nDestroy all shared pointers." << endl;
 
 
 //     boost::shared_array<Class2> a = MemoryManager::AllocateArray<Class2>();

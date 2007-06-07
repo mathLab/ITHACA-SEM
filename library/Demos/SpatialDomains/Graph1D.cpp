bc5eca2cf31fdfe8c532e6d6cd9335f463f97b6b
Joe Frazier <jfrazier@nektar.info>
2007-06-07 23:57:24 +0000

    Added processing of boundary conditions.  Intermediate revision that is not complete.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@611 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/Demos/SpatialDomains/Graph1D.cpp
+++ library/Demos/SpatialDomains/Graph1D.cpp
@@ -1,7 +1,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream> 
-#include "SpatialDomains/MeshGraph1D.h"
+#include <SpatialDomains/MeshGraph1D.h>
+#include <SpatialDomains/BoundaryConditions.h>
+
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace Nektar;
 using namespace SpatialDomains; 
@@ -11,15 +16,20 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-    if(argc != 2){
-      cerr << "usage: Graph1D file" << endl;
-      exit(1);
-    }
+    //if(argc != 2){
+    //  cerr << "usage: Graph1D file" << endl;
+    //  exit(1);
+    //}
 
-    string in(argv[argc-1]);
+    //string in(argv[argc-1]);
+    string meshfile = "C:\\Data\\PhD\\Research\\dev\\Nektar++\\library\\Demos\\SpatialDomains\\meshdef1D.xml";
+    string bcfile = "c:\\Data\\PhD\\Research\\dev\\Nektar++\\library\\Demos\\SpatialDomains\\BC1.xml";
+
     MeshGraph1D graph1D;
+    BoundaryConditions bcs(&graph1D);
 
-    graph1D.Read(in);
+    graph1D.Read(meshfile);
+    bcs.Read(bcfile);
 
     return 0;
 }

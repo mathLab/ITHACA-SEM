6b1ec35e63da379b358e038323ca0d29bba1ebc8
Joe Frazier <jfrazier@nektar.info>
2007-08-10 03:33:31 +0000

    Static initialization contained in here.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@832 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/LibUtilities/BasicUtils/NekManager.cpp
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: NekManager.hpp
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: NekManager.cpp
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <LibUtilities/BasicUtils/NekManager.hpp>
+#include <LibUtilities/Foundations/Points.h>
+#include <LibUtilities/Foundations/Basis.h>
+#include <LibUtilities/LinearAlgebra/nektypedefs.hpp>
+#include <StdRegions/StdMatrixKey.h>
+#include <LocalRegions/MatrixKey.h>
+
+namespace Nektar
+{
+    namespace LibUtilities
+    {
+        NekManager<PointsKey, Points<NekDouble>, PointsKey::opLess>::ValueContainerPool NekManager<PointsKey, Points<NekDouble>, PointsKey::opLess>::m_ValueContainerPool;
+        NekManager<BasisKey, Basis, BasisKey::opLess>::ValueContainerPool NekManager<BasisKey, Basis, BasisKey::opLess>::m_ValueContainerPool;
+        NekManager<PointsKey, DNekMat, PointsKey::opLess>::ValueContainerPool NekManager<PointsKey, DNekMat, PointsKey::opLess>::m_ValueContainerPool;
+        NekManager<StdRegions::StdMatrixKey, DNekMat, StdRegions::StdMatrixKey::opLess>::ValueContainerPool NekManager<StdRegions::StdMatrixKey, DNekMat, StdRegions::StdMatrixKey::opLess>::m_ValueContainerPool;
+        NekManager<LocalRegions::MatrixKey, DNekScalMat, LocalRegions::MatrixKey::opLess>::ValueContainerPool NekManager<LocalRegions::MatrixKey, DNekScalMat, LocalRegions::MatrixKey::opLess>::m_ValueContainerPool;
+    };
+};

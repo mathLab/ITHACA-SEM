548201e4585db78733ed16388189f8f45311d38b
Sophia Han <ehan@nektar.info>
2008-02-05 00:41:37 +0000

    Added initial tetrahedral expansion.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1104 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/LocalRegions/TetExp.h
+++ library/LocalRegions/TetExp.h
@@ -36,9 +36,13 @@
 #ifndef TETEXP_H
 #define TETEXP_H
 
-#include <StdRegions/StdTetExp.h>
+#include <LocalRegions/LocalRegions.hpp>
 
+#include <StdRegions/StdTetExp.h>
 #include <SpatialDomains/TetGeom.h>
+
+#include <SpatialDomains/GeomFactors.h>
+#include <LocalRegions/MatrixKey.h>
 
 namespace Nektar
 {
@@ -49,41 +53,194 @@ namespace Nektar
     {
 
     public:
-    
-      /** \brief Constructor using BasisKey class for quadrature
-      points and order definition */
-      TetExp(const StdRegions::BasisKey &Ba, const StdRegions::BasisKey &Bb, 
-         const StdRegions::BasisKey &Bc);
 
-      /** \brief Constructor using BasisKey class for quadrature points
-      and order definition where _coeffs and _phys are all set. */
-      TetExp(const StdRegions::BasisKey &Ba, const StdRegions::BasisKey &Bb, 
-         const StdRegions::BasisKey &Bc, double *coeffs, double *phys);
-      
+	/** \brief Constructor using BasisKey class for quadrature
+        points and order definition */
+        TetExp(const LibUtilities::BasisKey &Ba,
+               const LibUtilities::BasisKey &Bb,
+	       const LibUtilities::BasisKey &Bc,
+               const SpatialDomains::TetGeomSharedPtr &geom);
+
+        TetExp(const LibUtilities::BasisKey &Ba,
+	       const LibUtilities::BasisKey &Bb,
+	       const LibUtilities::BasisKey &Bc);
+
+	    
       /// Copy Constructor
-      TetExp(TetExp &T);
-    
+      TetExp(const TetExp &T);
+
       /// Destructor
       ~TetExp();
-    
-      /// Return Shape of region, using  ShapeType enum list. i.e. Tetrahedron
-      StdRegions::ShapeType DetShapeType() 
-      { 
-        return StdRegions::eTetrahedron; 
-      }
-    
+
+        void GetCoords(Array<OneD,NekDouble> &coords_0,
+		       Array<OneD,NekDouble> &coords_1,
+          	       Array<OneD,NekDouble> &coords_2);
+
+	void GetCoord(const ConstArray<OneD,NekDouble> &Lcoords, 
+                      Array<OneD,NekDouble> &coords);
+
+        //----------------------------
+        // Integration Methods
+        //----------------------------
+
+        /// \brief Integrate the physical point list \a inarray over region
+       NekDouble Integral(const ConstArray<OneD,NekDouble> &inarray);    
+
+       void IProductWRTBase(const ConstArray<OneD,NekDouble> &inarray, Array<OneD,NekDouble> &outarray);
+       void FwdTrans(const ConstArray<OneD,NekDouble> & inarray,Array<OneD,NekDouble> &outarray);
+
+       NekDouble PhysEvaluate(const ConstArray<OneD,NekDouble> &coord);
+
+
+	/// Return Shape of region, using  ShapeType enum list. i.e. Tetrahedron
+	StdRegions::ShapeType DetShapeType() 
+	{ 
+	return StdRegions::eTetrahedron; 
+	}
+
+        SpatialDomains::TetGeomSharedPtr GetGeom()
+        {
+            return m_geom;
+        }
+
+        void WriteToFile(FILE *outfile);
+	void WriteToFile(std::ofstream &outfile, const int dumpVar);
+
+
     protected:
-      
-      SpatialDomains::TetGeom * m_geom;
+
+        void GenMetricInfo();
+
+	void IProductWRTBase(const ConstArray<OneD,NekDouble> &base0, 
+			     const ConstArray<OneD,NekDouble> &base1, 
+			     const ConstArray<OneD,NekDouble> &base2, 
+			     const ConstArray<OneD,NekDouble> &inarray,
+			     Array<OneD,NekDouble> &outarray);
+
+
+	DNekMatSharedPtr GetStdMatrix(const StdRegions::StdMatrixKey &mkey);
+        DNekScalMatSharedPtr    CreateMatrix(const MatrixKey &mkey);
+
+        DNekBlkMatSharedPtr GetStdStaticCondMatrix(const StdRegions::StdMatrixKey &mkey);
+        DNekScalBlkMatSharedPtr  CreateStaticCondMatrix(const MatrixKey &mkey);
+
+	SpatialDomains::TetGeomSharedPtr m_geom;
+        SpatialDomains::GeomFactorsSharedPtr  m_metricinfo;
+
+	LibUtilities::NekManager<MatrixKey, DNekScalMat, MatrixKey::opLess> m_matrixManager;
+        LibUtilities::NekManager<MatrixKey, DNekScalBlkMat, MatrixKey::opLess> m_staticCondMatrixManager;
+
 
     private:
-
+      TetExp();
+	
       virtual StdRegions::ShapeType v_DetShapeType() const
       {
-        return DetShapeType();
-      }  
+	 return DetShapeType();
+      }
+
+      virtual SpatialDomains::GeomFactorsSharedPtr v_GetMetricInfo() const
+        {
+            return m_metricinfo;
+        }
+
+         virtual void v_GetCoords(Array<OneD, NekDouble> &coords_0,
+                                 Array<OneD, NekDouble> &coords_1 = NullNekDouble1DArray,
+                                 Array<OneD, NekDouble> &coords_2 = NullNekDouble1DArray)
+         {
+             GetCoords(coords_0, coords_1, coords_2);
+         }
+
+        virtual void v_GetCoord(const ConstArray<OneD, NekDouble> &lcoord, 
+                                Array<OneD, NekDouble> &coord)
+        {
+            GetCoord(lcoord, coord);
+        }
+
+        virtual int v_GetCoordim()
+        {
+            return m_geom->GetCoordim();
+        }
+
+
+        virtual void v_WriteToFile(FILE *outfile)
+        {
+            WriteToFile(outfile);
+        }
+        
+        virtual void v_WriteToFile(std::ofstream &outfile, const int dumpVar)
+        {
+            WriteToFile(outfile,dumpVar);
+        }
+
+        /** \brief Virtual call to integrate the physical point list \a inarray
+        over region (see SegExp::Integral) */
+        virtual NekDouble v_Integral(const ConstArray<OneD, NekDouble> &inarray )
+        {
+            return Integral(inarray);
+        }
+
+        /** \brief Virtual call to TriExp::IProduct_WRT_B */
+        virtual void v_IProductWRTBase(const ConstArray<OneD, NekDouble> &inarray,
+                                       Array<OneD, NekDouble> &outarray)
+        {
+            IProductWRTBase(inarray,outarray);
+        }
+
+        virtual void v_IProductWRTDerivBase (const int dir,
+                                             const ConstArray<OneD,NekDouble> &inarray,
+                                             Array<OneD, NekDouble> &outarray)
+        {
+            IProductWRTDerivBase(dir,inarray,outarray);
+        }
+
+        /// Virtual call to SegExp::FwdTrans
+        virtual void v_FwdTrans(const ConstArray<OneD, NekDouble> &inarray,
+                                Array<OneD, NekDouble> &outarray)
+        {
+            FwdTrans(inarray, outarray);
+        }
+        
+        /// Virtual call to TetExp::Evaluate
+        virtual NekDouble v_PhysEvaluate(const ConstArray<OneD, NekDouble> &coords)
+        {
+            return PhysEvaluate(coords);
+        }
+
+        virtual NekDouble v_Linf(const ConstArray<OneD, NekDouble> &sol)
+        {
+            return Linf(sol);
+        }
+
+        virtual NekDouble v_Linf()
+        {
+            return Linf();
+        }
+
+        virtual NekDouble v_L2(const ConstArray<OneD, NekDouble> &sol)
+        {
+            return StdExpansion::L2(sol);
+        }
+
+
+        virtual NekDouble v_L2()
+        {
+            return StdExpansion::L2();
+        }
+
+        virtual DNekScalMatSharedPtr v_GetLocMatrix(const MatrixKey &mkey)
+        {
+            return m_matrixManager[mkey];
+        }
+
+        virtual DNekScalBlkMatSharedPtr v_GetLocStaticCondMatrix(const MatrixKey &mkey)
+        {
+            return m_staticCondMatrixManager[mkey];
+        }
+
+
     };
-    
+
     // type defines for use of TetExp in a boost vector
     typedef boost::shared_ptr<TetExp> TetExpSharedPtr;
     typedef std::vector< TetExpSharedPtr > TetExpVector;
@@ -96,6 +253,9 @@ namespace Nektar
 
 /** 
  *    $Log: TetExp.h,v $
+ *    Revision 1.6  2007/07/22 23:04:19  bnelson
+ *    Backed out Nektar::ptr.
+ *
  *    Revision 1.5  2007/07/20 00:45:51  bnelson
  *    Replaced boost::shared_ptr with Nektar::ptr
  *

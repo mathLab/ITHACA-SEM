e63a2d71ce67828def53a0082201644e0d2096b6
Sophia Han <ehan@nektar.info>
2008-02-05 00:40:19 +0000

    Added initial pyramidic expansion.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1103 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/LocalRegions/PyrExp.h
+++ library/LocalRegions/PyrExp.h
@@ -29,19 +29,23 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 //
-// Description: Header file for PrismExp routines
+// Description: Header file for PyrExp routines
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef PYREXP_H
 #define PYREXP_H
 
-#include <StdRegions/StdBasis.h>
-#include <StdRegions/StdPyrExp.h>
+#include <LocalRegions/LocalRegions.hpp>
 
+#include <StdRegions/StdPyrExp.h>
 #include <SpatialDomains/PyrGeom.h>
 
-#include <StdRegions/StdRegions.hpp>
+#include <SpatialDomains/GeomFactors.h>
+#include <LocalRegions/MatrixKey.h>
+
+
+
 
 namespace Nektar
 {
@@ -55,35 +59,189 @@ namespace Nektar
 
       /** \brief Constructor using BasisKey class for quadrature points and order
       definition */
-      PyrExp(const StdRegions::BasisKey &Ba, const StdRegions::BasisKey &Bb,
-         const StdRegions::BasisKey &Bc);
+        PyrExp(const LibUtilities::BasisKey &Ba,
+               const LibUtilities::BasisKey &Bb,
+	       const LibUtilities::BasisKey &Bc,
+               const SpatialDomains::PyrGeomSharedPtr &geom);
 
-      /** \brief Constructor using BasisKey class for quadrature
-      points and order definition where _coeffs and _phys are all
-      set. */
-      PyrExp(const StdRegions::BasisKey &Ba, const StdRegions::BasisKey &Bb,
-         const StdRegions::BasisKey &Bc, double *coeffs, double *phys);
+        PyrExp(const LibUtilities::BasisKey &Ba,
+	       const LibUtilities::BasisKey &Bb,
+	       const LibUtilities::BasisKey &Bc);
 
-    /// Copy Constructor
-      PyrExp(PyrExp &T);
+	    
+      /// Copy Constructor
+      PyrExp(const PyrExp &T);
 
       /// Destructor
       ~PyrExp();
 
+       void GetCoords(Array<OneD,NekDouble> &coords_0,
+		       Array<OneD,NekDouble> &coords_1,
+          	       Array<OneD,NekDouble> &coords_2);
+
+	void GetCoord(const ConstArray<OneD,NekDouble> &Lcoords, 
+                      Array<OneD,NekDouble> &coords);
+
+	 //----------------------------
+        // Integration Methods
+        //----------------------------
+
+        /// \brief Integrate the physical point list \a inarray over region
+       NekDouble Integral(const ConstArray<OneD,NekDouble> &inarray);    
+
+       void IProductWRTBase(const ConstArray<OneD,NekDouble> &inarray, Array<OneD,NekDouble> &outarray);
+       void FwdTrans(const ConstArray<OneD,NekDouble> & inarray,Array<OneD,NekDouble> &outarray);
+
+       NekDouble PhysEvaluate(const ConstArray<OneD,NekDouble> &coord);
+
       /// Return Shape of region, using  ShapeType enum list. i.e. Pyramid
       StdRegions::ShapeType DetShapeType()
       {
-    return StdRegions::ePyramid;
+    	return StdRegions::ePyramid;
       }
 
+       SpatialDomains::PyrGeomSharedPtr GetGeom()
+      {
+          return m_geom;
+      }
+
+      void WriteToFile(FILE *outfile);
+      void WriteToFile(std::ofstream &outfile, const int dumpVar);
+
+
     protected:
-      SpatialDomains::PyrGeom * m_geom;
+      //SpatialDomains::PyrGeom * m_geom;
+	void GenMetricInfo();
+
+	void IProductWRTBase(const ConstArray<OneD,NekDouble> &base0, 
+			     const ConstArray<OneD,NekDouble> &base1, 
+			     const ConstArray<OneD,NekDouble> &base2, 
+			     const ConstArray<OneD,NekDouble> &inarray,
+			     Array<OneD,NekDouble> &outarray);
+
+
+	DNekMatSharedPtr GetStdMatrix(const StdRegions::StdMatrixKey &mkey);
+        DNekScalMatSharedPtr    CreateMatrix(const MatrixKey &mkey);
+
+        DNekBlkMatSharedPtr GetStdStaticCondMatrix(const StdRegions::StdMatrixKey &mkey);
+        DNekScalBlkMatSharedPtr  CreateStaticCondMatrix(const MatrixKey &mkey);
+
+	SpatialDomains::PyrGeomSharedPtr m_geom;
+        SpatialDomains::GeomFactorsSharedPtr  m_metricinfo;
+
+	LibUtilities::NekManager<MatrixKey, DNekScalMat, MatrixKey::opLess> m_matrixManager;
+        LibUtilities::NekManager<MatrixKey, DNekScalBlkMat, MatrixKey::opLess> m_staticCondMatrixManager;
+
 
     private:
+      PyrExp();
+
       virtual StdRegions::ShapeType v_DetShapeType() const
       {
         return DetShapeType();
       }
+
+      virtual SpatialDomains::GeomFactorsSharedPtr v_GetMetricInfo() const
+        {
+            return m_metricinfo;
+        }
+
+         virtual void v_GetCoords(Array<OneD, NekDouble> &coords_0,
+                                 Array<OneD, NekDouble> &coords_1 = NullNekDouble1DArray,
+                                 Array<OneD, NekDouble> &coords_2 = NullNekDouble1DArray)
+         {
+             GetCoords(coords_0, coords_1, coords_2);
+         }
+
+        virtual void v_GetCoord(const ConstArray<OneD, NekDouble> &lcoord, 
+                                Array<OneD, NekDouble> &coord)
+        {
+            GetCoord(lcoord, coord);
+        }
+
+        virtual int v_GetCoordim()
+        {
+            return m_geom->GetCoordim();
+        }
+
+
+        virtual void v_WriteToFile(FILE *outfile)
+        {
+            WriteToFile(outfile);
+        }
+        
+        virtual void v_WriteToFile(std::ofstream &outfile, const int dumpVar)
+        {
+            WriteToFile(outfile,dumpVar);
+        }
+
+        /** \brief Virtual call to integrate the physical point list \a inarray
+        over region (see SegExp::Integral) */
+        virtual NekDouble v_Integral(const ConstArray<OneD, NekDouble> &inarray )
+        {
+            return Integral(inarray);
+        }
+
+        /** \brief Virtual call to TriExp::IProduct_WRT_B */
+        virtual void v_IProductWRTBase(const ConstArray<OneD, NekDouble> &inarray,
+                                       Array<OneD, NekDouble> &outarray)
+        {
+            IProductWRTBase(inarray,outarray);
+        }
+
+        virtual void v_IProductWRTDerivBase (const int dir,
+                                             const ConstArray<OneD,NekDouble> &inarray,
+                                             Array<OneD, NekDouble> &outarray)
+        {
+            IProductWRTDerivBase(dir,inarray,outarray);
+        }
+
+        /// Virtual call to SegExp::FwdTrans
+        virtual void v_FwdTrans(const ConstArray<OneD, NekDouble> &inarray,
+                                Array<OneD, NekDouble> &outarray)
+        {
+            FwdTrans(inarray, outarray);
+        }
+        
+        /// Virtual call to PyrExp::Evaluate
+        virtual NekDouble v_PhysEvaluate(const ConstArray<OneD, NekDouble> &coords)
+        {
+            return PhysEvaluate(coords);
+        }
+
+        virtual NekDouble v_Linf(const ConstArray<OneD, NekDouble> &sol)
+        {
+            return Linf(sol);
+        }
+
+        virtual NekDouble v_Linf()
+        {
+            return Linf();
+        }
+
+        virtual NekDouble v_L2(const ConstArray<OneD, NekDouble> &sol)
+        {
+            return StdExpansion::L2(sol);
+        }
+
+
+        virtual NekDouble v_L2()
+        {
+            return StdExpansion::L2();
+        }
+
+        virtual DNekScalMatSharedPtr v_GetLocMatrix(const MatrixKey &mkey)
+        {
+            return m_matrixManager[mkey];
+        }
+
+        virtual DNekScalBlkMatSharedPtr v_GetLocStaticCondMatrix(const MatrixKey &mkey)
+        {
+            return m_staticCondMatrixManager[mkey];
+        }
+
+
+
 
     };
 
@@ -99,6 +257,9 @@ namespace Nektar
 
 /**
  *    $Log: PyrExp.h,v $
+ *    Revision 1.7  2007/07/22 23:04:18  bnelson
+ *    Backed out Nektar::ptr.
+ *
  *    Revision 1.6  2007/07/20 00:45:51  bnelson
  *    Replaced boost::shared_ptr with Nektar::ptr
  *

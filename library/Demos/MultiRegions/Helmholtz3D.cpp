bd4a242aa22c5093cd02dd016290b8568d954c85
Sophia Han <ehan@nektar.info>
2008-08-26 02:44:44 +0000

    *** empty log message ***
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1517 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/Demos/MultiRegions/Helmholtz3D.cpp
@@ -0,0 +1,139 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <MultiRegions/ContField3D.h>
+
+using namespace Nektar;
+
+int main(int argc, char *argv[])
+{
+    MultiRegions::ContField3DSharedPtr Exp, Fce;
+    MultiRegions::ExpListSharedPtr DerExp1, DerExp2, DerExp3;
+    int     i, nq,  coordim;
+    Array<OneD,NekDouble>  fce; 
+    Array<OneD,NekDouble>  xc0,xc1,xc2; 
+    NekDouble  lambda;
+
+    if(argc != 3)
+    {
+        fprintf(stderr,"Usage: Helmholtz3D  meshfile boundaryfile\n");
+        exit(1);
+    }
+
+    //----------------------------------------------
+    // Read in mesh from input file
+    string meshfile(argv[1]);
+    SpatialDomains::MeshGraph3D graph3D;
+    graph3D.ReadGeometry(meshfile);
+    graph3D.ReadExpansions(meshfile);
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // read the problem parameters from input file
+    string bcfile(argv[2]);
+    SpatialDomains::BoundaryConditions bcs(&graph3D);
+    bcs.Read(bcfile);
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // Print summary of solution details
+    lambda = bcs.GetParameter("Lambda");
+    const SpatialDomains::ExpansionVector &expansions = graph3D.GetExpansions();
+    LibUtilities::BasisKey bkey = graph3D.GetBasisKey(expansions[0],0);
+    cout << "Solving 3D Helmholtz:"  << endl;
+    cout << "         Lambda     : " << lambda << endl; 
+    cout << "         Expansion  : " << SpatialDomains::kExpansionTypeStr[expansions[0]->m_ExpansionType] << endl;
+    cout << "         No. modes  : " << (int) expansions[0]->m_NumModesEqn.Evaluate() << endl;
+    cout << endl;
+    //----------------------------------------------
+   
+    //----------------------------------------------
+    // Define Expansion 
+    Exp = MemoryManager<MultiRegions::ContField3D>::AllocateSharedPtr(graph3D,bcs);
+    //----------------------------------------------
+
+    
+    //----------------------------------------------
+    // Set up coordinates of mesh for Forcing function evaluation
+    coordim = Exp->GetCoordim(0);
+    nq      = Exp->GetPointsTot();
+    
+    xc0 = Array<OneD,NekDouble>(nq,0.0);
+    xc1 = Array<OneD,NekDouble>(nq,0.0);
+    xc2 = Array<OneD,NekDouble>(nq,0.0);
+    
+    switch(coordim)
+    {
+    case 1:
+        Exp->GetCoords(xc0);
+        break;
+    case 2:
+        Exp->GetCoords(xc0,xc1);
+        break;
+    case 3:
+        Exp->GetCoords(xc0,xc1,xc2);
+        break;
+    }
+    //----------------------------------------------
+    
+    //----------------------------------------------
+    // Define forcing function for first variable defined in file 
+    fce = Array<OneD,NekDouble>(nq);
+    SpatialDomains::ConstForcingFunctionShPtr ffunc = bcs.GetForcingFunction(bcs.GetVariable(0));
+    for(i = 0; i < nq; ++i)
+    {
+        fce[i] = ffunc->Evaluate(xc0[i],xc1[i],xc2[i]);
+    }
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // Setup expansion containing the  forcing function
+    Fce = MemoryManager<MultiRegions::ContField3D>::AllocateSharedPtr(*Exp);
+    Fce->SetPhys(fce);
+    //----------------------------------------------
+  
+    //----------------------------------------------
+    // Helmholtz solution taking physical forcing 
+    Exp->HelmSolve(*Fce, lambda);
+    //----------------------------------------------
+    
+    //----------------------------------------------
+    // Backward Transform Solution to get solved values at 
+    Exp->BwdTrans(*Exp);
+    //----------------------------------------------
+    
+    //----------------------------------------------
+    // Write solution 
+    ofstream outfile("HelmholtzFile3D.pos");
+    Exp->WriteToFile(outfile,eGmsh);
+    //----------------------------------------------
+    
+    //----------------------------------------------
+    // See if there is an exact solution, if so 
+    // evaluate and plot errors
+    SpatialDomains::ConstExactSolutionShPtr ex_sol = bcs.GetExactSolution(bcs.GetVariable(0));
+
+    if(ex_sol)
+    {
+        //----------------------------------------------
+        // evaluate exact solution 
+        for(i = 0; i < nq; ++i)
+        {
+            fce[i] = ex_sol->Evaluate(xc0[i],xc1[i],xc2[i]);
+        }
+        //----------------------------------------------
+
+        //--------------------------------------------
+        // Calculate L_inf error 
+        Fce->SetPhys(fce);
+        Fce->SetPhysState(true);
+
+
+        cout << "L infinity error: " << Exp->Linf(*Fce) << endl;
+        cout << "L 2 error:        " << Exp->L2  (*Fce) << endl;
+        //--------------------------------------------        
+    }
+    //----------------------------------------------        
+        return 0;
+}
+

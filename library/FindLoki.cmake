f71bb78d623532d8046c471973b3c2b9661b1239
Blake Nelson <bnelson@cs.utah.edu>
2009-03-02 05:05:27 +0000

    *** empty log message ***
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1763 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/FindLoki.cmake
+++ library/FindLoki.cmake
@@ -7,7 +7,10 @@ FIND_PATH(LOKI_INCLUDE_DIR loki/Typelist.h
 	/usr/local/include 
 	${CMAKE_SOURCE_DIR}/../ThirdParty/loki/include/ 
 	${CMAKE_SOURCE_DIR}/../ThirdParty/loki-0.1.3/include 
-      ${CMAKE_SOURCE_DIR}/../ThirdParty/loki-0.1.3/loki-0.1.3/include 
+        ${CMAKE_SOURCE_DIR}/../ThirdParty/loki-0.1.3/loki-0.1.3/include 
+	${CMAKE_SOURCE_DIR}/../../ThirdParty/loki/include/
+	${CMAKE_SOURCE_DIR}/../../ThirdParty/loki-0.1.3/include/
+        ${CMAKE_SOURCE_DIR}/../../ThirdParty/loki-0.1.3/loki-0.1.3/include 
 	"C:\\Program Files\\Microsoft Visual Studio .NET 2003\\Vc7\\include" 
 	"C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include" 
 )

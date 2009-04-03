7cbc45cbc03384dbc13e412db845770a71d3df91
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2009-04-03 20:32:34 +0000

    New method
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1813 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/Demos/MultiRegions/EigValsLinearAdvection2D.cpp
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <MultiRegions/ContField2D.h>
+
+using namespace Nektar;
+
+int main(int argc, char *argv[])
+{
+    MultiRegions::ContField2DSharedPtr Exp;
+    int     i, nq,  coordim;
+    Array<OneD,NekDouble>  fce; 
+    Array<OneD,NekDouble>  xc0,xc1,xc2; 
+    NekDouble  ax,ay;
+
+    if(argc != 3)
+    {
+        fprintf(stderr,"Usage: EigValsLinearAdvection2D  meshfile boundaryfile\n");
+        exit(1);
+    }
+
+    //----------------------------------------------
+    // Read in mesh from input file
+    string meshfile(argv[argc-2]);
+    SpatialDomains::MeshGraph2D graph2D; 
+
+    graph2D.ReadGeometry(meshfile);
+    graph2D.ReadExpansions(meshfile);
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // read the problem parameters from input file
+    string bcfile(argv[argc-1]);
+    SpatialDomains::BoundaryConditions bcs(&graph2D); 
+    bcs.Read(bcfile);
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // Get Advection Velocity
+    ax = bcs.GetParameter("Advection_x");
+    ay = bcs.GetParameter("Advection_y");
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // Print summary of solution details
+    const SpatialDomains::ExpansionVector &expansions = graph2D.GetExpansions();
+    LibUtilities::BasisKey bkey = graph2D.GetBasisKey(expansions[0],0);
+    cout << "Calc. LinearAdvection E-vals:"  << endl; 
+    cout << "             Advection_x    : " << ax << endl; 
+    cout << "             Advection_y    : " << ay << endl; 
+    cout << "             Expansion      : " << SpatialDomains::kExpansionTypeStr[expansions[0]->m_ExpansionType] << endl;
+    cout << "             No. modes      : " << (int) expansions[0]->m_NumModesEqn.Evaluate() << endl;
+    cout << endl;
+    //----------------------------------------------
+   
+    //----------------------------------------------
+    // Define Expansion 
+    Exp = MemoryManager<MultiRegions::ContField2D>::
+        AllocateSharedPtr(graph2D,bcs);
+    //----------------------------------------------
+
+    //----------------------------------------------
+    // Set up coordinates of mesh for Forcing function evaluation
+    coordim = Exp->GetCoordim(0);
+    nq      = Exp->GetTotPoints();
+    
+    xc0 = Array<OneD,NekDouble>(nq,0.0);
+    xc1 = Array<OneD,NekDouble>(nq,0.0);
+    xc2 = Array<OneD,NekDouble>(nq,0.0);
+    
+    switch(coordim)
+    {
+    case 1:
+        Exp->GetCoords(xc0);
+        break;
+    case 2:
+        Exp->GetCoords(xc0,xc1);
+        break;
+    case 3:
+        Exp->GetCoords(xc0,xc1,xc2);
+        break;
+    }
+    //----------------------------------------------
+    
+    //----------------------------------------------
+    // Evaluate Eigenvalues/vector of Linear Advection Op.
+    int nmodes = Exp->GetNcoeffs();
+    Array<OneD, NekDouble> Real(nmodes), Imag(nmodes), Evecs(nmodes*nmodes);
+    
+    Exp->LinearAdvectionEigs(ax,ay,Real,Imag,Evecs);
+    //----------------------------------------------
+    
+    cout << "Eignvalues: [Real, Imag]" << endl;
+    for(i = 0; i < Real.num_elements(); ++i)
+    {
+        cout << Real[i] <<", "<< Imag[i] << endl;
+    }
+    
+    return 0;
+}
+

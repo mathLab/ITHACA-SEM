3dc8e79a8b7b4231c76b9b67f9972363f2a8650e
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2009-04-22 22:32:55 +0000

    demo to read in dat file (at quadrature points) and write out .fld file
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@1834 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ library/Demos/MultiRegions/DatToFld.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <MultiRegions/ExpList2D.h>
+
+using namespace Nektar;
+
+int main(int argc, char *argv[])
+{
+    MultiRegions::ExpList2DSharedPtr Exp;
+    int     i, nq,  coordim;
+
+    if(argc != 3)
+    {
+        fprintf(stderr,"Usage: DatToFld meshfile file.dat\n");
+        exit(1);
+    }
+
+    //----------------------------------------------
+    // Read in mesh from input file
+    string meshfile(argv[argc-2]);
+    SpatialDomains::MeshGraph2D graph2D; 
+    graph2D.ReadGeometry(meshfile);
+    graph2D.ReadExpansions(meshfile);
+    //----------------------------------------------
+
+
+    //----------------------------------------------        
+    // Define Expansion 
+    Exp = MemoryManager<MultiRegions::ExpList2D>::AllocateSharedPtr(graph2D);
+    //----------------------------------------------  
+
+    //---------------------------------------------
+    // Read Dat file
+    string   in(argv[argc-1]);
+    std::ifstream infile(in.c_str());
+    Exp->ReadFromFile(infile,eTecplot);
+    //---------------------------------------------
+
+    //-----------------------------------------------
+    // Write solution to file 
+    string   out(strtok(argv[argc-1],"."));
+    string   endfile(".fld");
+    out += endfile; 
+    std::vector<SpatialDomains::FieldDefinitionsSharedPtr> FieldDef = Exp->GetFieldDefinitions();
+    std::vector<std::vector<NekDouble> > FieldData(FieldDef.size()); 
+    
+    for(i = 0; i < FieldDef.size(); ++i)
+    {
+        FieldDef[i]->m_Fields.push_back("u");
+        Exp->AppendFieldData(FieldDef[i], FieldData[i]);
+    }
+    graph2D.Write(out, FieldDef, FieldData);
+    //-----------------------------------------------
+    
+    return 0;
+}
+

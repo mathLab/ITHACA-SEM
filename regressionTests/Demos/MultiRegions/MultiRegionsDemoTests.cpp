7bc956c4539dfed4723060568a3e3a11fbfb8c38
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2010-08-22 08:51:17 +0000

    Added HDGHelmholtz3DHomo1D and Helmholtz3DHomo1D tests
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@2339 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- regressionTests/Demos/MultiRegions/MultiRegionsDemoTests.cpp
+++ regressionTests/Demos/MultiRegions/MultiRegionsDemoTests.cpp
@@ -70,6 +70,12 @@ int main(int argc, char* argv[])
     Execute("Helmholtz3D", "helmholtz3D_hex.xml","CG Helmholtz3D hex");
     Execute("Helmholtz3D", "helmholtz3D_tet.xml","CG Helmholtz3D tets");
 
+    // 3D Demos Homogeneous1D
+    
+    Execute("Helmholtz3DHomo1D", "helmholtz3D_homo1D_7modes_8nz.xml","CG Helmholtz3D Homogeneous 1D");
+    Execute("HDGHelmholtz3DHomo1D", "helmholtz3D_homo1D_7modes_8nz.xml","HDG Helmholtz3D Homogeneous 1D");
+    
+
     return 0;
 }
 

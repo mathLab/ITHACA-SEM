3bce6d5cc984da77e3edc87300617f06f57ebb8e
Chris Cantwell <c.cantwell@imperial.ac.uk>
2010-09-29 16:54:05 +0000

    Added page placeholder for documentation of Nektar++ XML file format.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@2384 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ docs/html/doxygen/Xml.h
@@ -0,0 +1,13 @@
+namespace Nektar
+{
+/**
+ * \page pageXML Nektar++ XML File Format
+ *
+ * \section sectionXMLGeometry XML Geometry definition
+ *
+ * \section sectionXMLExpansions XML Expansions definition
+ *
+ * \section sectionXMLConditions XML Conditions definition
+ *
+ */
+}

9e99bc2524a5984bd45e3c26367a383f5044bce5
Alessandro Bolis <a.bolis09@imperial.ac.uk>
2011-02-17 12:09:48 +0000

    Unsteady Advection for CFL testing
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@2666 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ solvers/ADRSolver/EquationSystems/CFLtester.h
@@ -0,0 +1,80 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File UnsteadyAdvection.h
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description:
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef NEKTAR_SOLVERS_ADRSOLVER_EQUATIONSYSTEMS_CFLTESTER_H
+#define NEKTAR_SOLVERS_ADRSOLVER_EQUATIONSYSTEMS_CFLTESTER_H
+
+#include <ADRSolver/EquationSystems/UnsteadySystem.h>
+
+namespace Nektar
+{
+    class CFLtester : public UnsteadySystem
+    {
+    public:
+        /// Creates an instance of this class
+        static EquationSystemSharedPtr create(SessionReaderSharedPtr& pSession) {
+            return MemoryManager<CFLtester>::AllocateSharedPtr(pSession);
+        }
+        /// Name of class
+        static std::string className;
+
+        CFLtester(SessionReaderSharedPtr& pSession);
+
+        virtual ~CFLtester();
+
+    protected:
+        Array<OneD, Array<OneD, NekDouble> > m_velocity;
+
+        void DoOdeRhs(const Array<OneD,  const  Array<OneD, NekDouble> > &inarray,
+                      Array<OneD,  Array<OneD, NekDouble> > &outarray,
+                      const NekDouble time);
+
+        void DoOdeProjection(const Array<OneD,  const  Array<OneD, NekDouble> > &inarray,
+                          Array<OneD,  Array<OneD, NekDouble> > &outarray,
+                          const NekDouble time);
+
+        // DG Advection routines
+        virtual void v_GetFluxVector(const int i, Array<OneD, Array<OneD, NekDouble> > &physfield, Array<OneD, Array<OneD, NekDouble> > &flux);
+
+        virtual void v_NumericalFlux(Array<OneD, Array<OneD, NekDouble> > &physfield, Array<OneD, Array<OneD, NekDouble> > &numflux);
+
+        // Print Summary
+        virtual void v_PrintSummary(std::ostream &out);
+    private:
+
+    };
+}
+
+#endif

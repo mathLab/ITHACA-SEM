c03057d574d684e78a9057c68c9f6646c56cc39f
Chris Cantwell <c.cantwell@imperial.ac.uk>
2011-08-14 19:24:11 +0000

    Added missing DriverArnoldi.cpp from r3078.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@3079 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ solvers/Auxiliary/DriverArnoldi.cpp
@@ -0,0 +1,76 @@
+/*
+ * DriverArnoldi.cpp
+ *
+ *  Created on: 14 Aug 2011
+ *      Author: cc
+ */
+
+#include <Auxiliary/DriverArnoldi.h>
+
+namespace Nektar
+{
+    /**
+     *
+     */
+    DriverArnoldi::DriverArnoldi(LibUtilities::SessionReaderSharedPtr pSession)
+            : Driver(pSession)
+    {
+    };
+
+
+    /**
+     *
+     */
+    DriverArnoldi::~DriverArnoldi()
+    {
+    };
+
+
+    /**
+     * Copy Arnoldi array to field variables which depend from
+     * either the m_fields or m_forces
+     */
+    void DriverArnoldi::CopyArnoldiArrayToField(Array<OneD, NekDouble> &array)
+    {
+        Array<OneD, MultiRegions::ExpListSharedPtr> fields;
+        if(m_TimeSteppingAlgorithm)
+        {
+            fields = m_equ[0]->UpdateFields();
+        }
+        else
+        {
+            fields = m_equ[0]->UpdateForces();
+        }
+
+        int nq = fields[0]->GetNpoints();
+
+        for (int k = 0; k < m_nfields; ++k)
+        {
+            Vmath::Vcopy(nq, &array[k*nq], 1, &fields[k]->UpdatePhys()[0], 1);
+            fields[k]->SetPhysState(true);
+        }
+    };
+
+
+    /**
+     * Copy field variables which depend from either the m_fields
+     * or m_forces array the Arnoldi array
+     */
+    void DriverArnoldi::CopyFieldToArnoldiArray(Array<OneD, NekDouble> &array)
+    {
+        Array<OneD, MultiRegions::ExpListSharedPtr> fields;
+        fields = m_equ[0]->UpdateFields();
+        int nq = fields[0]->GetNpoints();
+
+        for (int k = 0; k < m_nfields; ++k)
+        {
+            if(!m_TimeSteppingAlgorithm)
+            {
+                fields[k]->BwdTrans_IterPerExp(fields[k]->GetCoeffs(),fields[k]->UpdatePhys());
+            }
+
+            Vmath::Vcopy(nq,  &fields[k]->GetPhys()[0], 1, &array[k*nq], 1);
+            fields[k]->SetPhysState(true);
+        }
+    };
+}

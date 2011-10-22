54e8abd339016b2183b2ec0e9ed2ff49ef95cc75
Chris Cantwell <c.cantwell@imperial.ac.uk>
2011-10-22 15:24:54 +0000

    Updated webpage to include latest release. Incremented version number. Modified instructions to rename ThirdParty-[version] to ThirdParty.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@3238 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- docs/html/downloads.php
+++ docs/html/downloads.php
@@ -48,6 +48,11 @@ Material</a></p>
         <hr>
 	<ul>
       <li>
+         <h3 align="left">release 3.0.1 (October 2011) </h3>
+         <p><a href="downloads/nektar++-3.0.1.tar.gz">Nektar++ Linux/Windows Code</a></p>
+         <p><a href="downloads/ThirdParty-3.0.1.tar.gz">Required Third Party Libraries</a></p>
+      </li>
+      <li>
          <h3 align="left">release 3.0.0 (August 2011) </h3>
          <p><a href="downloads/nektar++-3.0.0.tar.gz">Nektar++ Linux/Windows Code</a></p>
          <p><a href="downloads/ThirdParty-3.0.0.tar.gz">Required Third Party Libraries</a></p>

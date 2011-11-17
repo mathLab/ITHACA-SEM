55678ebac12514fb2d8593d532f5a9a93622f8ec
Andrea Isoni <a.isoni10@imperial.ac.uk>
2011-11-17 14:28:41 +0000

    update regression test ChanStability_coupled.xml
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@3308 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- solvers/Auxiliary/DriverArpack.cpp
+++ solvers/Auxiliary/DriverArpack.cpp
@@ -318,10 +318,10 @@ namespace Nektar
               for (int k = 0; k < m_nfields; ++k)
               {
 
-        		//Vmath::Vcopy(nq, &z[k*nq], 1, &fields[k]->UpdateCoeffs()[0] , 1);   
-        		//fields[k]->SetPhysState(false);     
-        		Vmath::Vcopy(nq, &z[k*nq+(nconv-1)*n], 1, &fields[k]->UpdateCoeffs()[0] , 1);			
-        		fields[k]->SetPhysState(true);
+        		Vmath::Vcopy(nq, &z[k*nq], 1, &fields[k]->UpdateCoeffs()[0] , 1);   
+        		fields[k]->SetPhysState(false);     
+        		//Vmath::Vcopy(nq, &z[k*nq+(nconv-1)*n], 1, &fields[k]->UpdateCoeffs()[0] , 1);			
+        		//fields[k]->SetPhysState(true);
               }    	
               m_equ[0]->Output();
         }

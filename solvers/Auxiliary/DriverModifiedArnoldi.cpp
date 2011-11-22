5978a306fd7bf17fde495d7e9a9d97899a9421b4
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2011-11-22 19:41:18 +0000

    Put shift into getRealEvl routine
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@3322 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- solvers/Auxiliary/DriverModifiedArnoldi.cpp
+++ solvers/Auxiliary/DriverModifiedArnoldi.cpp
@@ -226,8 +226,8 @@ namespace Nektar
          // Process eigenvectors and write out.
          EV_post(Tseq, Kseq, ntot, min(--i, m_kdim), m_nvec, zvec, wr, wi, converged);
          
-         m_real_evl = wr;
-         m_imag_evl = wi;
+         m_real_evl = wr + m_realShift;
+         m_imag_evl = wi + m_imagShift;
          
          // Close the runtime info file.
          evlout.close();

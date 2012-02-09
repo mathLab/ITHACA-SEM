5628f8e67b40d07802f47aa9fe5dc737b9a27713
Sergey Yakovlev <yakovs@utah.sci.edu>
2012-02-09 01:18:20 +0000

    Added v_NumDGBndryCoeffs()
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@3549 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/StdRegions/StdHexExp.h
+++ library/StdRegions/StdHexExp.h
@@ -209,6 +209,7 @@ namespace Nektar
             STD_REGIONS_EXPORT virtual int  v_GetNfaces() const;
             STD_REGIONS_EXPORT virtual ExpansionType v_DetExpansionType() const;
             STD_REGIONS_EXPORT virtual int  v_NumBndryCoeffs() const;
+            STD_REGIONS_EXPORT virtual int  v_NumDGBndryCoeffs() const;
             STD_REGIONS_EXPORT virtual int  v_GetEdgeNcoeffs(const int i) const;
             STD_REGIONS_EXPORT virtual int  v_GetFaceNcoeffs(const int i) const;
             STD_REGIONS_EXPORT virtual int  v_GetFaceIntNcoeffs(

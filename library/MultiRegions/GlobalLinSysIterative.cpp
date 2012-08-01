2657c623ce8f57346e965249c4ce7d8fbc8426bd
Chris Cantwell <c.cantwell@imperial.ac.uk>
2012-08-01 14:50:00 +0000

    Reimplementation of CG solver to resolve numerical instability issues. Reverted regression test updates. Reverted default solver tolerance back to 1e-09.
    git-svn-id: https://gforge.sci.utah.edu/svn/nektar/trunk@4004 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- library/MultiRegions/GlobalLinSysIterative.cpp
+++ library/MultiRegions/GlobalLinSysIterative.cpp
@@ -74,7 +74,7 @@ namespace Nektar
          * parallel execution of the solver.
          *
          * The implemented algorithm uses a reduced-communication reordering of
-         * the standard PCG method (Amin, Sadayappan, Gudavalli, IEEE 1994)
+         * the standard PCG method (Demmel, Heath and Vorst, 1993)
          *
          * @param       pInput      Input vector of all DOFs.
          * @param       pOutput     Solution vector of all DOFs.
@@ -102,29 +102,192 @@ namespace Nektar
             int nNonDir = nGlobal - nDir;
 
             // Allocate array storage
-            Array<OneD, NekDouble> p_A    (nGlobal, 0.0);
-            Array<OneD, NekDouble> q_A    (nGlobal, 0.0);
-            Array<OneD, NekDouble> y_A    (nNonDir, 0.0);
+            Array<OneD, NekDouble> w_A    (nGlobal, 0.0);
+            Array<OneD, NekDouble> s_A    (nGlobal, 0.0);
+            Array<OneD, NekDouble> p_A    (nNonDir, 0.0);
             Array<OneD, NekDouble> r_A    (nNonDir, 0.0);
-            Array<OneD, NekDouble> z_A    (nNonDir, 0.0);
+            Array<OneD, NekDouble> q_A    (nNonDir, 0.0);
+            Array<OneD, NekDouble> tmp;
 
             // Create NekVector wrappers for linear algebra operations
-            NekVector<NekDouble> in (nNonDir,pInput + nDir, eWrapper);
+            NekVector<NekDouble> in (nNonDir,pInput  + nDir,eWrapper);
             NekVector<NekDouble> out(nNonDir,pOutput + nDir,eWrapper);
-            NekVector<NekDouble> p  (nNonDir,p_A + nDir,    eWrapper);
-            NekVector<NekDouble> q  (nNonDir,q_A + nDir,    eWrapper);
-            NekVector<NekDouble> y  (nNonDir,y_A,           eWrapper);
+            NekVector<NekDouble> w  (nNonDir,w_A + nDir,    eWrapper);
+            NekVector<NekDouble> s  (nNonDir,s_A + nDir,    eWrapper);
+            NekVector<NekDouble> p  (nNonDir,p_A,           eWrapper);
             NekVector<NekDouble> r  (nNonDir,r_A,           eWrapper);
-            NekVector<NekDouble> z  (nNonDir,z_A,           eWrapper);
+            NekVector<NekDouble> q  (nNonDir,q_A,           eWrapper);
 
             int k;
-            NekDouble alpha, beta, r_dot_d, b_dot_b, min_resid;
+            NekDouble alpha, beta, rho, rho_new, mu, eps, bb_inv, min_resid;
             Array<OneD, NekDouble> vExchange(3);
 
             // Initialise with zero as the initial guess.
             r = in;
+            m_precon->DoPreconditioner(r_A, tmp = w_A + nDir);
+            v_DoMatrixMultiply(w_A, s_A);
+            k = 0;
+
+            vExchange[0] = Vmath::Dot2(nNonDir,
+                                       r_A,
+                                       w_A + nDir,
+                                       m_map + nDir);
+
+            vExchange[1] = Vmath::Dot2(nNonDir,
+                                       s_A + nDir,
+                                       w_A + nDir,
+                                       m_map + nDir);
+
+            vExchange[2] = Vmath::Dot2(nNonDir,
+                                       r_A,
+                                       r_A,
+                                       m_map + nDir);
+
+            vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+
+            // If input vector is zero, set zero output and skip solve.
+            if (vExchange[0] < NekConstants::kNekZeroTol)
+            {
+                Vmath::Zero(nGlobal, pOutput, 1);
+                return;
+            }
+
+            rho       = vExchange[0];
+            mu        = vExchange[1];
+            beta      = 0.0;
+            alpha     = rho/mu;
+            eps       = 0.0;
+            bb_inv    = 1.0/vExchange[2];
+            min_resid = bb_inv;
+
+            // Continue until convergence
+            while (true)
+            {
+                ASSERTL0(k < 20000,
+                         "Exceeded maximum number of iterations (20000)");
+
+                ASSERTL0(eps*bb_inv <= 1.0 || k < 10,
+                         "Conjugate gradient diverged. Tolerance too small?"
+                         "Minimum residual achieved: "
+                         + boost::lexical_cast<std::string>(sqrt(min_resid)));
+
+                // Compute new search direction p_k, q_k
+                p   = w   + beta  * p;
+                q   = s   + beta  * q;
+
+                // Update solution x_{k+1}
+                out = out + alpha * p;
+
+                // Update residual vector r_{k+1}
+                r   = r   - alpha * q;
+
+                // Apply preconditioner
+                m_precon->DoPreconditioner(r_A, tmp = w_A + nDir);
+
+                // Perform the method-specific matrix-vector multiply operation.
+                v_DoMatrixMultiply(w_A, s_A);
+
+                // <r_{k+1}, w_{k+1}>
+                vExchange[0] = Vmath::Dot2(nNonDir,
+                                        r_A,
+                                        w_A + nDir,
+                                        m_map + nDir);
+                // <s_{k+1}, w_{k+1}>
+                vExchange[1] = Vmath::Dot2(nNonDir,
+                                        s_A + nDir,
+                                        w_A + nDir,
+                                        m_map + nDir);
+
+                // <r_{k+1}, r_{k+1}>
+                vExchange[2] = Vmath::Dot2(nNonDir,
+                                        r_A,
+                                        r_A,
+                                        m_map + nDir);
+
+                // Perform inner-product exchanges
+                vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+
+                rho_new = vExchange[0];
+                mu      = vExchange[1];
+                eps     = vExchange[2];
+
+                // test if norm is within tolerance
+                if (eps*bb_inv < m_tolerance * m_tolerance)
+                {
+                    break;
+                }
+                min_resid = min(min_resid, eps);
+
+                // Compute search direction and solution coefficients
+                beta  = rho_new/rho;
+                alpha = rho_new/(mu - rho_new*beta/alpha);
+                rho   = rho_new;
+
+                k++;
+            }
+        }
+
+#if 0
+        /**
+         * Solve a global linear system using the conjugate gradient method.
+         * We solve only for the non-Dirichlet modes. The operator is evaluated
+         * using the local-matrix representation. Distributed math routines are
+         * used to support parallel execution of the solver.
+         *
+         * Standard CG algorithm.
+         *
+         * @param       pInput      Input vector of non-Dirichlet DOFs.
+         * @param       pOutput     Solution vector of non-Dirichlet DOFs.
+         */
+        void GlobalLinSysIterative::v_SolveLinearSystem(
+                    const int nGlobal,
+                    const Array<OneD,const NekDouble> &pInput,
+                          Array<OneD,      NekDouble> &pOutput,
+                    const AssemblyMapSharedPtr &plocToGloMap,
+                    const int nDir)
+        {
+            // Check if preconditioner has been computed and compute if needed.
+            if (!m_precon)
+            {
+                v_UniqueMap();
+                m_precon = MemoryManager<Preconditioner>::AllocateSharedPtr(GetSharedThisPtr(),plocToGloMap);
+            }
+
+            // Get the communicator for performing data exchanges
+            LibUtilities::CommSharedPtr vComm = m_expList.lock()->GetComm()->GetRowComm();
+
+            // Get vector sizes
+            int nNonDir = nGlobal - nDir;
+
+            // Allocate array storage
+            Array<OneD, NekDouble> d_A    (nGlobal, 0.0);
+            Array<OneD, NekDouble> p_A    (nGlobal, 0.0);
+            Array<OneD, NekDouble> z_A    (nNonDir, 0.0);
+            Array<OneD, NekDouble> z_new_A(nNonDir, 0.0);
+            Array<OneD, NekDouble> r_A    (nNonDir, 0.0);
+            Array<OneD, NekDouble> r_new_A(nNonDir, 0.0);
+
+            // Create NekVector wrappers for linear algebra operations
+            NekVector<NekDouble> in(nNonDir,pInput + nDir,eWrapper);
+            NekVector<NekDouble> out(nNonDir,pOutput + nDir,eWrapper);
+            NekVector<NekDouble> r(nNonDir,r_A,eWrapper);
+            NekVector<NekDouble> r_new(nNonDir,r_new_A,eWrapper);
+            NekVector<NekDouble> z(nNonDir,z_A,eWrapper);
+            NekVector<NekDouble> z_new(nNonDir,z_new_A,eWrapper);
+            NekVector<NekDouble> d(nNonDir,d_A + nDir, eWrapper);
+            NekVector<NekDouble> p(nNonDir,p_A + nDir,eWrapper);
+
+            int k;
+            NekDouble alpha, beta, normsq, r_dot_z_old;
+            Array<OneD, NekDouble> vExchange(2);
+
+            // INVERSE of preconditioner matrix.
+            //const DNekMat &M = (*m_preconditioner);
+
+            // Initialise with zero as the initial guess.
+            r = in;
             m_precon->DoPreconditioner(r_A,z_A);
-            p = z;
+            d = z;
             k = 0;
 
             vExchange[0] = Vmath::Dot2(nNonDir, r_A, r_A, m_map + nDir);
@@ -137,73 +300,73 @@ namespace Nektar
                 Vmath::Zero(nGlobal, pOutput, 1);
                 return;
             }
-            b_dot_b = vExchange[0];
-            r_dot_d = vExchange[1];
-            min_resid = b_dot_b;
+            r_dot_z_old = vExchange[1];
 
             // Continue until convergence
             while (true)
             {
-                ASSERTL0(k < 20000,
-                         "Exceeded maximum number of iterations (20000)");
-
-                ASSERTL0(vExchange[0] <= b_dot_b || k < 10,
-                         "Conjugate gradient diverged. Tolerance too small?"
-                         "Minimum residual achieved: "
-                         + boost::lexical_cast<std::string>(sqrt(min_resid)));
-
                 // Perform the method-specific matrix-vector multiply operation.
-                v_DoMatrixMultiply(p_A, q_A);
+                v_DoMatrixMultiply(d_A, p_A);
 
-                // Apply preconditioner
-                m_precon->DoPreconditioner(q_A + nDir, y_A);
-
-                // <r_k, r_k>
+                // compute step length alpha
+                // alpha denominator
                 vExchange[0] = Vmath::Dot2(nNonDir,
-                                        r_A,
-                                        r_A,
-                                        m_map + nDir);
-                // <p_k, q_k>
-                vExchange[1] = Vmath::Dot2(nNonDir,
+                                        d_A + nDir,
                                         p_A + nDir,
-                                        q_A + nDir,
                                         m_map + nDir);
-
-                // <q_k, y_k>
-                vExchange[2] = Vmath::Dot2(nNonDir,
-                                        q_A + nDir,
-                                        y_A,
-                                        m_map + nDir);
-
-                // Perform exchange
+                // perform exchange
                 vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
 
-                min_resid = min(min_resid, vExchange[0]);
+                // compute alpha
+                alpha = r_dot_z_old/vExchange[0];
+
+                // approximate solution
+                out   = out + alpha*d;
+
+                // compute residual
+                r_new = r   - alpha*p;
+
+                // Apply preconditioner to new residual
+                m_precon->DoPreconditioner(r_new_A,z_new_A);
+
+                // beta
+                vExchange[0] = Vmath::Dot2(nNonDir,
+                                        r_new_A,
+                                        z_new_A,
+                                        m_map + nDir);
+
+                // residual
+                vExchange[1] = Vmath::Dot2(nNonDir,
+                                        r_new_A,
+                                        r_new_A,
+                                        m_map + nDir);
+
+                // perform exchange
+                vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+
+                // extract values for beta and norm
+                beta        = vExchange[0]/r_dot_z_old;
+                r_dot_z_old = vExchange[0];
+                normsq      = vExchange[1];
 
                 // test if norm is within tolerance
-                if (vExchange[0] < m_tolerance * m_tolerance)
+                if (normsq < m_tolerance * m_tolerance)
                 {
                     break;
                 }
 
-                // compute alpha
-                alpha   =  r_dot_d / vExchange[1];
-                beta    =  alpha * vExchange[2] / vExchange[1] - 1.0;
-                r_dot_d *= beta;
-
-                // Update residual vector
-                r = r   - alpha * q;
-                z = z   - alpha * y;
-
-                // Update solution
-                out   = out + alpha * p;
-
                 // Compute new search direction
-                p = z + beta * p;
+                d = z_new + beta*d;
 
+                // Next step
+                r = r_new;
+                z = z_new;
                 k++;
+
+                ASSERTL0(k < 20000,
+                         "Exceeded maximum number of iterations (20000)");
             }
         }
-
+#endif
     }
 }

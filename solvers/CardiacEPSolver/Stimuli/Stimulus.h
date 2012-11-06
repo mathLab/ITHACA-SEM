5126b6df5fb0a97e2615abaaa9920ef28f8f9748
Chang <etc04@dyn1201-254.wlan.ic.ac.uk>
2012-11-06 14:20:22 +0000

    Added basic templates for stimuli.

---
+++ solvers/CardiacEPSolver/Stimuli/Stimulus.h
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File Stimulus.h
+//
+// For more information, please see: http://www.nektar.info
+//
+//
+// Description: Stimulus protocol base class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+//#ifndef NEKTAR_SOLVERS_ADRSOLVER_CELLMODELS_CELLMODEL
+//#define NEKTAR_SOLVERS_ADRSOLVER_CELLMODELS_CELLMODEL
+
+#include <LibUtilities/BasicUtils/NekFactory.hpp>
+#include <LibUtilities/BasicUtils/SessionReader.h>
+#include <LibUtilities/BasicUtils/SharedArray.hpp>
+//#include <SpatialDomains/SpatialData.h>
+#include <MultiRegions/ExpList.h>
+#include <StdRegions/StdNodalTriExp.h>
+#include <StdRegions/StdNodalTetExp.h>
+
+namespace Nektar
+{
+    // Forward declaration
+    class Protocol;
+    
+    
+    
+    /// Protocol base class.
+    class Protocol
+    {
+        
+    };
+    
+}
+
+#endif /*PROTOCOL_H_ */

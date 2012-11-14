26c159eb85b748407531713b03d373d5efa6e756
Chris Cantwell <c.cantwell@imperial.ac.uk>
2012-11-14 13:50:59 +0000

    Tidied up XXT code Added some documentation/comments

--- library/MultiRegions/GlobalLinSysXxtFull.cpp
+++ library/MultiRegions/GlobalLinSysXxtFull.cpp
@@ -120,7 +120,8 @@ namespace Nektar
 
                 SolveLinearSystem(nGlobDofs, tmp, tmp2, pLocToGloMap);
 
-                // Enforce the Dirichlet boundary conditions after solve
+                // Enforce the Dirichlet boundary conditions on the solution
+                // array.
                 Vmath::Vadd(nGlobDofs, pOutput.get(), 1,
                                        tmp2.get(),    1,
                                        pOutput.get(), 1);
@@ -133,6 +134,10 @@ namespace Nektar
         }
 
 
+        /**
+         * Create the inverse multiplicity map.
+         * @param   locToGloMap Local to global mapping information.
+         */
         void GlobalLinSysXxtFull::CreateMap(
                     const boost::shared_ptr<AssemblyMap> &pLocToGloMap)
         {
@@ -163,8 +168,8 @@ namespace Nektar
 
 
         /**
-         * Assemble a full matrix from the block matrix stored in
-         * #m_blkMatrices and the given local to global mapping information.
+         * Construct the local matrix row index, column index and value index
+         * arrays and initialize the XXT data structure with this information.
          * @param   locToGloMap Local to global mapping information.
          */
         void GlobalLinSysXxtFull::AssembleMatrixArrays(
@@ -173,39 +178,46 @@ namespace Nektar
             ExpListSharedPtr vExp = m_expList.lock();
             unsigned int nElmt = vExp->GetNumElmts();
             DNekScalMatSharedPtr loc_mat;
-            unsigned int iCount = 0;
-            unsigned int rCount = 0;
-            unsigned int nRows = 0;
-            unsigned int i = 0, j = 0, k = 0, n = 0, cnt = 0, iCnt = 0, jCnt = 0;
-            unsigned int nEntries = 0;
-            unsigned int numDirBnd = pLocToGloMap->GetNumGlobalDirBndCoeffs();
-            unsigned int nLocal = pLocToGloMap->GetNumLocalCoeffs();
-            const Array<OneD, NekDouble> &vMapSign = pLocToGloMap->GetLocalToGlobalSign();
+            unsigned int iCount     = 0;
+            unsigned int rCount     = 0;
+            unsigned int nRows      = 0;
+            unsigned int nEntries   = 0;
+            unsigned int numDirBnd  = pLocToGloMap->GetNumGlobalDirBndCoeffs();
+            unsigned int nLocal     = pLocToGloMap->GetNumLocalCoeffs();
+            const Array<OneD, NekDouble> &vMapSign
+                                    = pLocToGloMap->GetLocalToGlobalSign();
             bool doSign = pLocToGloMap->GetSignChange();
-            int gid1, gid2;
+            unsigned int i = 0, j = 0, k = 0, n = 0;
+            int gid1;
             Array<OneD, unsigned int> vSizes(nElmt);
 
+            // First construct a map of the number of local DOFs in each block
+            // and the number of matrix entries for each block
             for (n = 0; n < nElmt; ++n)
             {
-                nRows = vExp->GetExp(vExp->GetOffset_Elmt_Id(n))->GetNcoeffs();
-                vSizes[n] = nRows;
+                i = vExp->GetOffset_Elmt_Id(n);
+                vSizes[n] = vExp->GetExp(i)->GetNcoeffs();
                 nEntries += vSizes[n]*vSizes[n];
             }
 
+            // Set up i-index, j-index and value arrays
             m_Ai = Array<OneD, unsigned int>(nEntries);
             m_Aj = Array<OneD, unsigned int>(nEntries);
             m_Ar = Array<OneD, double>(nEntries, 0.0);
+
+            // Set up the universal ID array for XXT
             Array<OneD, unsigned long> vId(nLocal);
 
-            iCount = 0;
-            for(n = cnt = 0; n < nElmt; ++n)
+            // Loop over each elemental block, extract matrix indices and value
+            // and set the universal ID array
+            for(n = iCount = 0; n < nElmt; ++n)
             {
                 loc_mat = GetBlock(vExp->GetOffset_Elmt_Id(n));
                 nRows = loc_mat->GetRows();
 
                 for(i = 0; i < nRows; ++i)
                 {
-                    gid1 = pLocToGloMap->GetLocalToGlobalMap(cnt + i);
+                    gid1 = pLocToGloMap->GetLocalToGlobalMap(iCount + i);
                     for(j = 0; j < nRows; ++j)
                     {
                             k = rCount + i*vSizes[n] + j;
@@ -214,10 +226,12 @@ namespace Nektar
                             m_Ar[k] = (*loc_mat)(i,j);
                             if (doSign)
                             {
-                                m_Ar[k] *= vMapSign[cnt+i]*vMapSign[cnt+j];
+                                m_Ar[k] *= vMapSign[iCount+i]*vMapSign[iCount+j];
                             }
                     }
 
+                    // Dirichlet DOFs are not included in the solve, so we set
+                    // these to the special XXT id=0.
                     if (gid1 < numDirBnd)
                     {
                         vId[iCount + i] = 0;
@@ -228,11 +242,11 @@ namespace Nektar
                             = pLocToGloMap->GetGlobalToUniversalMap(gid1);
                     }
                 }
-                cnt    += nRows;
                 iCount += vSizes[n];
                 rCount += vSizes[n]*vSizes[n];
             }
 
+            // Set up XXT and output some stats
             LibUtilities::CommSharedPtr vComm = pLocToGloMap->GetComm();
             m_crsData = Xxt::Init(nLocal, vId, m_Ai, m_Aj, m_Ar, vComm);
             Xxt::nektar_crs_stats(m_crsData);

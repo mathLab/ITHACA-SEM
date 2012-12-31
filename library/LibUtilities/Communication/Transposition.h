300974762dcf6d7689c09c40eb59be08ac4bf5de
Spencer Sherwin <ssherw@Spencer-Sherwins-MacBook-Air.local>
2012-12-31 09:31:21 +0000

    Renamed a few member variables

--- library/LibUtilities/Communication/Transposition.h
+++ library/LibUtilities/Communication/Transposition.h
@@ -46,104 +46,104 @@ namespace Nektar
 {
     namespace LibUtilities
     {
-		enum TranspositionDir
-		{
-			eXYtoZ,
-			eZtoXY,
-			eXtoYZ,
-			eYZtoX,
-			eYZtoZY,
-			eZYtoYZ,
-			eXtoY,
-			eYtoZ,
-			eZtoX,
-			eNoTrans
-		};
-		
+        enum TranspositionDir
+        {
+            eXYtoZ,
+            eZtoXY,
+            eXtoYZ,
+            eYZtoX,
+            eYZtoZY,
+            eZYtoYZ,
+            eXtoY,
+            eYtoZ,
+            eZtoX,
+            eNoTrans
+        };
+	
         class Transposition
         {
-		public:
-			
-			LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0, 
-											   LibUtilities::CommSharedPtr hcomm);
-			
-			LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0,
-											   const LibUtilities::BasisKey &HomoBasis1,
-											   LibUtilities::CommSharedPtr hcomm);
-			
-			LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0,
-											   const LibUtilities::BasisKey &HomoBasis1,
-											   const LibUtilities::BasisKey &HomoBasis2,
-											   LibUtilities::CommSharedPtr hcomm);
-
-			LIB_UTILITIES_EXPORT ~Transposition();
-			
-			LIB_UTILITIES_EXPORT unsigned int GetK(int i);
-			
-			LIB_UTILITIES_EXPORT Array<OneD, unsigned int> GetKs(void);
-			
-			LIB_UTILITIES_EXPORT unsigned int GetPlaneID(int i);
-			
-			LIB_UTILITIES_EXPORT Array<OneD, unsigned int> GetPlanesIDs(void);
-			
-			LIB_UTILITIES_EXPORT void Transpose(const Array<OneD,const NekDouble> &inarray,
-												Array<OneD, NekDouble> &outarray, 
-												bool UseNumMode = false, 
-												TranspositionDir dir = eNoTrans);
-			
-		protected:
-			
-			CommSharedPtr         m_hcomm;
-			
-		private:
-			
-			LIB_UTILITIES_EXPORT void TransposeXYtoZ(const Array<OneD,const NekDouble> &inarray,
-													 Array<OneD, NekDouble> &outarray, 
-													 bool UseNumMode = false);
-			
-			LIB_UTILITIES_EXPORT void TransposeZtoXY(const Array<OneD,const NekDouble> &inarray,
-													 Array<OneD, NekDouble> &outarray, 
-													 bool UseNumMode = false);
-			
-			LIB_UTILITIES_EXPORT void TransposeXtoYZ(const Array<OneD,const NekDouble> &inarray,
-													 Array<OneD, NekDouble> &outarray, 
-													 bool UseNumMode = false);
-			
-			LIB_UTILITIES_EXPORT void TransposeYZtoX(const Array<OneD,const NekDouble> &inarray,
-													 Array<OneD, NekDouble> &outarray, 
-													 bool UseNumMode = false);
-			
-			LIB_UTILITIES_EXPORT void TransposeYZtoZY(const Array<OneD,const NekDouble> &inarray,
-													 Array<OneD, NekDouble> &outarray, 
-													 bool UseNumMode = false);
-			
-			LIB_UTILITIES_EXPORT void TransposeZYtoYZ(const Array<OneD,const NekDouble> &inarray,
-													  Array<OneD, NekDouble> &outarray, 
-													  bool UseNumMode = false);
-			
-			int m_num_homogeneous_directions;
-			
-			Array<OneD,int> m_num_points_per_proc;     // number of homogenous points on each process for each of the three homogeneous direction
-			
-			Array<OneD,int> m_num_homogeneous_points;  // total number homogeneous point per homogeneous direction
-						
-			Array<OneD,int> m_num_homogeneous_coeffs;  // total number homogeneous coefficients 
-			
-			Array<OneD,int> m_num_processes;
-			
-			int m_rank_id;                            // rank of the process
-			
-			Array<OneD, unsigned int> m_planes_IDs;    // IDs of the planes on the process
-			
-			Array<OneD, unsigned int> m_K;             // Fourier wave numbers associated with the planes on the process
-			
-			Array<OneD,int> m_SizeMap;                // MPI_Alltoallv map containing the size of send/rec buffer
-			  
-			Array<OneD,int> m_OffsetMap;              // MPI_Alltoallv map containing the offset of send/rec buffer in the global vector
-			
+        public:
+            
+            LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0, 
+                                               LibUtilities::CommSharedPtr hcomm);
+            
+            LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0,
+                                               const LibUtilities::BasisKey &HomoBasis1,
+                                               LibUtilities::CommSharedPtr hcomm);
+            
+            LIB_UTILITIES_EXPORT Transposition(const LibUtilities::BasisKey &HomoBasis0,
+                                               const LibUtilities::BasisKey &HomoBasis1,
+                                               const LibUtilities::BasisKey &HomoBasis2,
+                                               LibUtilities::CommSharedPtr hcomm);
+            
+            LIB_UTILITIES_EXPORT ~Transposition();
+            
+            LIB_UTILITIES_EXPORT unsigned int GetK(int i);
+            
+            LIB_UTILITIES_EXPORT Array<OneD, unsigned int> GetKs(void);
+            
+            LIB_UTILITIES_EXPORT unsigned int GetPlaneID(int i);
+            
+            LIB_UTILITIES_EXPORT Array<OneD, unsigned int> GetPlanesIDs(void);
+            
+            LIB_UTILITIES_EXPORT void Transpose(const Array<OneD,const NekDouble> &inarray,
+                                                Array<OneD, NekDouble> &outarray, 
+                                                bool UseNumMode = false, 
+                                                TranspositionDir dir = eNoTrans);
+            
+        protected:
+            
+            CommSharedPtr         m_hcomm;
+            
+        private:
+            
+            LIB_UTILITIES_EXPORT void TransposeXYtoZ(const Array<OneD,const NekDouble> &inarray,
+                                                     Array<OneD, NekDouble> &outarray, 
+                                                     bool UseNumMode = false);
+            
+            LIB_UTILITIES_EXPORT void TransposeZtoXY(const Array<OneD,const NekDouble> &inarray,
+                                                     Array<OneD, NekDouble> &outarray, 
+                                                     bool UseNumMode = false);
+            
+            LIB_UTILITIES_EXPORT void TransposeXtoYZ(const Array<OneD,const NekDouble> &inarray,
+                                                     Array<OneD, NekDouble> &outarray, 
+                                                     bool UseNumMode = false);
+            
+            LIB_UTILITIES_EXPORT void TransposeYZtoX(const Array<OneD,const NekDouble> &inarray,
+                                                     Array<OneD, NekDouble> &outarray, 
+                                                     bool UseNumMode = false);
+            
+            LIB_UTILITIES_EXPORT void TransposeYZtoZY(const Array<OneD,const NekDouble> &inarray,
+                                                      Array<OneD, NekDouble> &outarray, 
+                                                      bool UseNumMode = false);
+            
+            LIB_UTILITIES_EXPORT void TransposeZYtoYZ(const Array<OneD,const NekDouble> &inarray,
+                                                      Array<OneD, NekDouble> &outarray, 
+                                                      bool UseNumMode = false);
+            
+            int m_num_homogeneous_directions;
+            
+            Array<OneD,int> m_num_points_per_proc;     // number of homogenous points on each processor for each of the three homogeneous direction
+            
+            Array<OneD,int> m_num_homogeneous_points;  // total number of homogeneous point per homogeneous direction
+            
+            Array<OneD,int> m_num_homogeneous_coeffs;  // total number homogeneous coefficients 
+            
+            Array<OneD,int> m_num_processors;
+            
+            int m_rank_id;                            // rank of the process
+            
+            Array<OneD, unsigned int> m_planes_IDs;    // IDs of the planes on the process
+            
+            Array<OneD, unsigned int> m_K;             // Fourier wave numbers associated with the planes on the process
+            
+            Array<OneD,int> m_SizeMap;                // MPI_Alltoallv map containing the size of send/rec buffer
+            
+            Array<OneD,int> m_OffsetMap;              // MPI_Alltoallv map containing the offset of send/rec buffer in the global vector
+            
         };
-		
-		typedef boost::shared_ptr<Transposition>      TranspositionSharedPtr;
-	}
+	
+        typedef boost::shared_ptr<Transposition>      TranspositionSharedPtr;
+    }
 }
 #endif

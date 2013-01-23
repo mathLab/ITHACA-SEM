343e7f8a78acac38f2732d9aec7fafc0dec5a41c
Chris Cantwell <c.cantwell@imperial.ac.uk>
2013-01-23 13:43:39 +0000

    Removed unused code in StdPrismExp.

--- library/StdRegions/StdPrismExp.cpp
+++ library/StdRegions/StdPrismExp.cpp
@@ -334,46 +334,7 @@ namespace Nektar
          * \frac {(1 + \bar \eta_1)} {(1 - \eta_3)} \frac \partial {\partial
          * \bar \eta_1} + \frac {\partial} {\partial \eta_3} \end{Bmatrix}\f$
          */
-#if 0
-        void StdPrismExp::v_PhysDeriv(const Array<OneD, const NekDouble>& u_physical, 
-                                      Array<OneD, NekDouble> &out_dxi1, 
-                                      Array<OneD, NekDouble> &out_dxi2,
-                                      Array<OneD, NekDouble> &out_dxi3 )
-        {
-            // PhysDerivative implementation based on Spen's book page 152.
-            int    Qx = m_base[0]->GetNumPoints();
-            int    Qy = m_base[1]->GetNumPoints();
-            int    Qz = m_base[2]->GetNumPoints();
 
-            Array<OneD, NekDouble> dEta_bar1(Qx*Qy*Qz,0.0);
-            Array<OneD, NekDouble> dXi2     (Qx*Qy*Qz,0.0);
-            Array<OneD, NekDouble> dEta3    (Qx*Qy*Qz,0.0);
-            PhysTensorDeriv(u_physical, dEta_bar1, dXi2, dEta3);
-
-            Array<OneD, const NekDouble> eta_x, eta_y, eta_z;
-            eta_x = m_base[0]->GetZ();
-            eta_y = m_base[1]->GetZ();
-            eta_z = m_base[2]->GetZ();
-
-            int i, j, k, n;
-
-            for (k = 0, n = 0; k < Qz; ++k)
-            {
-                for (j = 0; j < Qy; ++j)
-                {
-                    for (i = 0; i < Qx; ++i, ++n)
-                    {
-                        if (out_dxi1.num_elements() > 0)
-                            out_dxi1[n] = 2.0/(1.0 - eta_z[k]) * dEta_bar1[n];
-                        if (out_dxi2.num_elements() > 0)
-                            out_dxi2[n] = dXi2[n];
-                        if (out_dxi3.num_elements() > 0)
-                            out_dxi3[n] = (1.0+eta_x[i])/(1.0-eta_z[k])*dEta_bar1[n] + dEta3[n];
-                    } 
-                }
-            }
-        }
-#else
         void StdPrismExp::v_PhysDeriv(const Array<OneD, const NekDouble>& u_physical, 
                                       Array<OneD, NekDouble> &out_dxi1, 
                                       Array<OneD, NekDouble> &out_dxi2,
@@ -437,7 +398,6 @@ namespace Nektar
 
             }
         }
-#endif
 
         void StdPrismExp::v_PhysDeriv(const int dir,
                                       const Array<OneD, const NekDouble>& inarray,

473576365a395ad8c9818a53354dab938af2acd8
David Moxey <d.moxey@imperial.ac.uk>
2013-02-14 15:56:49 +0000

    Minor optimisation, store left adjacent edges in DisContField2D.

--- solvers/CompressibleFlowSolver/EquationSystems/CompressibleFlowSystem.cpp
+++ solvers/CompressibleFlowSolver/EquationSystems/CompressibleFlowSystem.cpp
@@ -227,8 +227,8 @@ namespace Nektar
         // user defined boundaries into account
         int e, id1, id2, npts;
         
-        for (e = 0; e < m_fields[0]->
-            GetBndCondExpansions()[bcRegion]->GetExpSize(); ++e)
+        for (e = 0; e < m_fields[0]->GetBndCondExpansions()[bcRegion]
+                 ->GetExpSize(); ++e)
         {
             npts = m_fields[0]->GetBndCondExpansions()[bcRegion]->
                 GetExp(e)->GetTotPoints();
@@ -239,9 +239,8 @@ namespace Nektar
                                     GetBndCondCoeffsToGlobalCoeffsMap(cnt+e));
             
             // For 2D/3D, define: v* = v - 2(v.n)n
-            
             Array<OneD,NekDouble> tmp(npts, 0.0);
-            
+
             // Calculate (v.n)
             for (i = 0; i < m_expdim; ++i)
             {
@@ -250,13 +249,10 @@ namespace Nektar
                              &m_traceNormals[i][id2], 1,
                              &tmp[0], 1,
                              &tmp[0], 1);
-                
             }
+
             // Calculate 2.0(v.n)
-            Vmath::Smul(npts,
-                        -2.0,
-                        &tmp[0],1,
-                        &tmp[0],1);
+            Vmath::Smul(npts, -2.0, &tmp[0], 1, &tmp[0], 1);
             
             // Calculate v* = v - 2.0(v.n)n
             for (i = 0; i < m_expdim; ++i)

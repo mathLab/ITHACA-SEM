d6bbb3e24eb594ed094b31b3cf7b0d76c3698382
Caroline <caroline.roney10@imperial.ac.uk>
2013-07-08 17:03:38 +0100

    Fix for GetVertex Function

--- library/SpatialDomains/Geometry3D.cpp
+++ library/SpatialDomains/Geometry3D.cpp
@@ -75,7 +75,7 @@ namespace Nektar
       /**
        * @brief Return vertex i in this element.
        */
-      const VertexComponentSharedPtr GetVertex(int i) const
+      const VertexComponentSharedPtr Geometry3D::GetVertex(int i) const
       {
           return m_verts[i];
       }

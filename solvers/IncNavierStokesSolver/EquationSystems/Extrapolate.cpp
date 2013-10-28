ec8c80f35dd90b3e7d280f4294eef8aa669ebee6
Ale <alessandrobolis@ab8409.local>
2013-10-28 08:50:56 +0000

    Fixed bug for parallel simulations - partition with no HOPBCs

--- solvers/IncNavierStokesSolver/EquationSystems/Extrapolate.cpp
+++ solvers/IncNavierStokesSolver/EquationSystems/Extrapolate.cpp
@@ -88,87 +88,90 @@ namespace Nektar
         const Array<OneD, const Array<OneD, NekDouble> > &fields,
         const Array<OneD, const Array<OneD, NekDouble> >  &N,
         NekDouble kinvis)
-    {		
-        Array<OneD, NekDouble> tmp;
-        Array<OneD, NekDouble> accelerationTerm;
+    {
+		if(m_pressureHBCs.num_elements()>0)
+		{
+			Array<OneD, NekDouble> tmp;
+			Array<OneD, NekDouble> accelerationTerm;
         
-        m_pressureCalls++;
-        int  n,cnt;
-        int  nint    = min(m_pressureCalls,m_intSteps);
-        int  nlevels = m_pressureHBCs.num_elements();
+			m_pressureCalls++;
+			int  n,cnt;
+			int  nint    = min(m_pressureCalls,m_intSteps);
+			int  nlevels = m_pressureHBCs.num_elements();
 	
-        int acc_order = 0;
+			int acc_order = 0;
         
-        accelerationTerm = Array<OneD, NekDouble>(m_acceleration[0].num_elements(), 0.0);
+			accelerationTerm = Array<OneD, NekDouble>(m_acceleration[0].num_elements(), 0.0);
 		
-        // Rotate HOPBCs storage
-        RollOver(m_pressureHBCs);
+			// Rotate HOPBCs storage
+			RollOver(m_pressureHBCs);
 		
-        // Rotate acceleration term
-        RollOver(m_acceleration);
+			// Rotate acceleration term
+			RollOver(m_acceleration);
 		
-        // Calculate BCs at current level
-        CalcPressureBCs(fields,N,kinvis);
+			// Calculate BCs at current level
+			CalcPressureBCs(fields,N,kinvis);
 
-        // Copy High order values into storage array 
-        for(cnt = n = 0; n < m_PBndConds.num_elements(); ++n)
-        {
-            // High order boundary condition;
-            if(m_PBndConds[n]->GetUserDefined() == SpatialDomains::eHigh)
-            {
-                int nq = m_PBndExp[n]->GetNcoeffs();
-                Vmath::Vcopy(nq,&(m_PBndExp[n]->GetCoeffs()[0]),1,&(m_pressureHBCs[0])[cnt],1);
-                cnt += nq;
-            }
-        }
+			// Copy High order values into storage array 
+			for(cnt = n = 0; n < m_PBndConds.num_elements(); ++n)
+			{
+				// High order boundary condition;
+				if(m_PBndConds[n]->GetUserDefined() == SpatialDomains::eHigh)
+				{
+					int nq = m_PBndExp[n]->GetNcoeffs();
+					Vmath::Vcopy(nq,&(m_PBndExp[n]->GetCoeffs()[0]),1,&(m_pressureHBCs[0])[cnt],1);
+					cnt += nq;
+				}
+			}
 		
-        //Calculate acceleration term at level n based on previous steps
-        if (m_pressureCalls > 2)
-        {
-            acc_order = min(m_pressureCalls-2,m_intSteps);
-            Vmath::Smul(cnt, StifflyStable_Gamma0_Coeffs[acc_order-1],
-                        m_acceleration[0], 1,
-                        accelerationTerm,  1);
+			//Calculate acceleration term at level n based on previous steps
+			if (m_pressureCalls > 2)
+			{
+				acc_order = min(m_pressureCalls-2,m_intSteps);
+				Vmath::Smul(cnt, StifflyStable_Gamma0_Coeffs[acc_order-1],
+							m_acceleration[0], 1,
+							accelerationTerm,  1);
 			
-            for(int i = 0; i < acc_order; i++)
-            {
-                Vmath::Svtvp(cnt, -1*StifflyStable_Alpha_Coeffs[acc_order-1][i],
-                             m_acceleration[i+1], 1,
-                             accelerationTerm,    1,
-                             accelerationTerm,    1);
-            }
-        }
+				for(int i = 0; i < acc_order; i++)
+				{
+					Vmath::Svtvp(cnt, -1*StifflyStable_Alpha_Coeffs[acc_order-1][i],
+								 m_acceleration[i+1], 1,
+								 accelerationTerm,    1,
+								 accelerationTerm,    1);
+				}
+			}
 		
-        // Adding acceleration term to HOPBCs
-        Vmath::Svtvp(cnt, -1.0/m_timestep,
-                     accelerationTerm,  1,
-                     m_pressureHBCs[0], 1,
-                     m_pressureHBCs[0], 1);
+			// Adding acceleration term to HOPBCs
+			Vmath::Svtvp(cnt, -1.0/m_timestep,
+						 accelerationTerm,  1,
+						 m_pressureHBCs[0], 1,
+						 m_pressureHBCs[0], 1);
         
-        // Extrapolate to n+1
-        Vmath::Smul(cnt, StifflyStable_Betaq_Coeffs[nint-1][nint-1],
-                    m_pressureHBCs[nint-1],    1,
-                    m_pressureHBCs[nlevels-1], 1);
+			// Extrapolate to n+1
+			Vmath::Smul(cnt, StifflyStable_Betaq_Coeffs[nint-1][nint-1],
+						m_pressureHBCs[nint-1],    1,
+						m_pressureHBCs[nlevels-1], 1);
         
-        for(n = 0; n < nint-1; ++n)
-        {
-            Vmath::Svtvp(cnt,StifflyStable_Betaq_Coeffs[nint-1][n],
-                         m_pressureHBCs[n],1,m_pressureHBCs[nlevels-1],1,
-                         m_pressureHBCs[nlevels-1],1);
-        }
+			for(n = 0; n < nint-1; ++n)
+			{
+				Vmath::Svtvp(cnt,StifflyStable_Betaq_Coeffs[nint-1][n],
+							 m_pressureHBCs[n],1,m_pressureHBCs[nlevels-1],1,
+							 m_pressureHBCs[nlevels-1],1);
+			}
 	
-        // Copy values of [dP/dn]^{n+1} in the pressure bcs storage.
-        // m_pressureHBCS[nlevels-1] will be cancelled at next time step
-        for(cnt = n = 0; n < m_PBndConds.num_elements(); ++n)
-        {
-            // High order boundary condition;
-            if(m_PBndConds[n]->GetUserDefined() == SpatialDomains::eHigh)
-            {
-                int nq = m_PBndExp[n]->GetNcoeffs();
-                Vmath::Vcopy(nq,&(m_pressureHBCs[nlevels-1])[cnt],1,&(m_PBndExp[n]->UpdateCoeffs()[0]),1);
-                cnt += nq;
-            }
-        }
+			// Copy values of [dP/dn]^{n+1} in the pressure bcs storage.
+			// m_pressureHBCS[nlevels-1] will be cancelled at next time step
+			for(cnt = n = 0; n < m_PBndConds.num_elements(); ++n)
+			{
+				// High order boundary condition;
+				if(m_PBndConds[n]->GetUserDefined() == SpatialDomains::eHigh)
+				{
+					int nq = m_PBndExp[n]->GetNcoeffs();
+					Vmath::Vcopy(nq,&(m_pressureHBCs[nlevels-1])[cnt],1,&(m_PBndExp[n]->UpdateCoeffs()[0]),1);
+					cnt += nq;
+				}
+			}
+		}
     }
     
 	

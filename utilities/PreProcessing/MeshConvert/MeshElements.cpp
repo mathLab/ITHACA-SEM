3052ca82b6d63c308868060d5093bdcfbc680066
David Moxey <d.moxey@imperial.ac.uk>
2014-01-11 20:43:18 +0000

    Add/correct logic for 2D reorientation

--- utilities/PreProcessing/MeshConvert/MeshElements.cpp
+++ utilities/PreProcessing/MeshConvert/MeshElements.cpp
@@ -498,13 +498,14 @@ namespace Nektar
             edgeNodeMap[pair<int,int>(2,3)] = 4 + n;
             edgeNodeMap[pair<int,int>(3,1)] = 4 + 2*n;
 
-            // Add vertices
+            // Add vertices. This logic will determine (in 2D) whether the
+            // element is clockwise (sum > 0) or counter-clockwise (sum < 0).
             NekDouble sum = 0.0;
             for (int i = 0; i < 3; ++i) {
-                int o = (i+1) % 2;
+                int o = (i+1) % 3;
                 vertex.push_back(pNodeList[i]);
-                sum += (vertex[o]->x - vertex[i]->x) *
-                       (vertex[o]->y + vertex[i]->y);
+                sum += (pNodeList[o]->x - pNodeList[i]->x) *
+                       (pNodeList[o]->y + pNodeList[i]->y);
             }
 
             // Create edges (with corresponding set of edge points)
@@ -687,10 +688,14 @@ namespace Nektar
             edgeNodeMap[pair<int,int>(3,4)] = 5 + 2*n;
             edgeNodeMap[pair<int,int>(4,1)] = 5 + 3*n;
 
-            // Add vertices
-            for (int i = 0; i < 4; ++i) 
-            {
+            // Add vertices. This logic will determine (in 2D) whether the
+            // element is clockwise (sum > 0) or counter-clockwise (sum < 0).
+            NekDouble sum = 0.0;
+            for (int i = 0; i < 4; ++i) {
+                int o = (i+1) % 4;
                 vertex.push_back(pNodeList[i]);
+                sum += (pNodeList[o]->x - pNodeList[i]->x) *
+                       (pNodeList[o]->y + pNodeList[i]->y);
             }
 
             // Create edges (with corresponding set of edge points)
@@ -706,6 +711,14 @@ namespace Nektar
                                                       pNodeList[it->first.second-1],
                                                       edgeNodes,
                                                       m_conf.edgeCurveType)));
+            }
+
+            if (pConf.reorient)
+            {
+                if (sum > 0.0)
+                {
+                    reverse(edge.begin(), edge.end());
+                }
             }
 
             if (m_conf.faceNodes)

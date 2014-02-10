bf404c2810050d0e65f42a213db978ad14aea8ed
Bastien Jordi <bastien.jord@gmail.com>
2014-02-10 16:52:25 +0000

    Minor error fixed.

--- utilities/PostProcessing/FieldConvert/ProcessQCriterion.cpp
+++ utilities/PostProcessing/FieldConvert/ProcessQCriterion.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-//  File: ProcessVorticity.cpp
+//  File: ProcessQCriterion.cpp
 //
 //  For more information, please see: http://www.nektar.info/
 //

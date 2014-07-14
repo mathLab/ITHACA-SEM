0219cfe690ccdb7abd1bef07a3518ae9ac3d2489
Spencer Sherwin <ssherw@dyn1215-231.wlan.ic.ac.uk>
2014-07-14 13:57:51 +0100

    Added SubStepping ability for steady advection velocity fields. This code is compiling but not validated/debugged

--- solvers/ADRSolver/EquationSystems/UnsteadyAdvectionDiffusion.h
+++ solvers/ADRSolver/EquationSystems/UnsteadyAdvectionDiffusion.h
@@ -66,6 +66,7 @@ namespace Nektar
         virtual ~UnsteadyAdvectionDiffusion();
         
     protected:
+        bool m_subSteppingScheme; 
         SolverUtils::RiemannSolverSharedPtr     m_riemannSolver;
         SolverUtils::AdvectionSharedPtr         m_advection;
         SolverUtils::DiffusionSharedPtr         m_diffusion;        
@@ -112,15 +113,53 @@ namespace Nektar
             NekDouble time,
             NekDouble lambda);
         
-        /// Get the normal velocity
+        /// Get the normal velocity based on m_velocity
         Array<OneD, NekDouble> &GetNormalVelocity();
+
+        /// Get the normal velocity based on input velfield
+        Array<OneD, NekDouble> &GetNormalVel(
+                          const Array<OneD, const Array<OneD, NekDouble> > &velfield);
         
         /// Initialise the object
         virtual void v_InitObject();
         
         /// Print Summary
         virtual void v_GenerateSummary(SummaryList& s);
+
+        /// PreIntegration step for substepping. 
+        virtual bool v_PreIntegrate(int step);
+
+        // SubsStepping methods -> Probably could be set up in separate class
+        void SubStepAdvance(const LibUtilities::TimeIntegrationSolutionSharedPtr &integrationSoln, 
+                            int nstep,  NekDouble time);
+        NekDouble GetSubstepTimeStep();
+        void SetUpSubSteppingTimeIntegration(int intMethod,
+                         const LibUtilities::TimeIntegrationWrapperSharedPtr &IntegrationScheme);
+
+        void SubStepAdvection(const Array<OneD, const Array<OneD, NekDouble> > &inarray,  
+                              Array<OneD, Array<OneD, NekDouble> > &outarray,
+                              const NekDouble time);
+
+        void SubStepProjection(const Array<OneD, const Array<OneD, NekDouble> > &inarray,  
+                               Array<OneD, Array<OneD, NekDouble> > &outarray, 
+                               const NekDouble time);
+
+        void AddAdvectionPenaltyFlux(const Array<OneD, const Array<OneD, NekDouble> > &velfield, 
+                                     const Array<OneD, const Array<OneD, NekDouble> > &physfield, 
+                                     Array<OneD, Array<OneD, NekDouble> > &Outarray);
         
+
+        Array<OneD, NekDouble> GetMaxStdVelocity(const Array<OneD, Array<OneD,NekDouble> > inarray);
+
+        LibUtilities::TimeIntegrationWrapperSharedPtr m_subStepIntegrationScheme;
+        LibUtilities::TimeIntegrationSchemeOperators  m_subStepIntegrationOps;
+
+        int m_intSteps;
+
+        NekDouble m_cflSafetyFactor;
+        int       m_infosteps;
+        int       m_minsubsteps;
+
     private:
         NekDouble m_waveFreq;
         NekDouble m_epsilon;

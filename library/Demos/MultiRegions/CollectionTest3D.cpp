0b7d6c0417252852cee062c25af0017484c05435
Spencer Sherwin <ssherw@dyn1215-69.wlan.ic.ac.uk>
2014-07-28 18:15:56 +0100

    Demo for 3D collections

---
+++ library/Demos/MultiRegions/CollectionTest3D.cpp
@@ -0,0 +1,125 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <LibUtilities/Memory/NekMemoryManager.hpp>
+#include <LibUtilities/BasicUtils/SessionReader.h>
+#include <LibUtilities/Communication/Comm.h>
+#include <MultiRegions/ContField3D.h>
+#include <SpatialDomains/MeshGraph3D.h>
+
+#include <Collections/Collection.h>
+
+#define NBWD 10000
+
+using namespace Nektar;
+
+int main(int argc, char *argv[])
+{
+    LibUtilities::SessionReaderSharedPtr vSession
+        = LibUtilities::SessionReader::CreateInstance(argc, argv);
+
+    MultiRegions::ContField3DSharedPtr Exp;
+
+    if(argc < 2)
+    {
+        fprintf(stderr,"Usage: CollectionTest3D meshfile [SysSolnType]   or   \n");
+        exit(1);
+    }
+
+    SpatialDomains::MeshGraphSharedPtr graph3D = 
+        SpatialDomains::MeshGraph::Read(vSession);
+
+    Exp = MemoryManager<MultiRegions::ContField3D>::
+        AllocateSharedPtr(vSession,graph3D,vSession->GetVariable(0));
+
+    vector<SpatialDomains::GeometrySharedPtr> geom(Exp->GetExpSize());
+
+    for (int i = 0; i < Exp->GetExpSize(); ++i)
+    {
+        geom[i] = Exp->GetExp(i)->GetGeom();
+    }
+    
+    Collections::Collection c(Exp->GetExp(0), geom,Collections::eSumFac);
+
+#if 1
+    {
+        Array<OneD, NekDouble> coeffs(Exp->GetNcoeffs(), 1.0), tmp;
+        Array<OneD, NekDouble> phys1(Exp->GetNpoints());
+        Array<OneD, NekDouble> phys2(Exp->GetNpoints());
+
+        Timer t;
+        t.Start();
+        for (int i = 0; i < NBWD; ++i)
+        {
+#if 1
+            for(int j = 0; j < Exp->GetNumElmts(); ++j)
+            {
+                Exp->GetExp(j)->BwdTrans(coeffs+Exp->GetCoeff_Offset(j), tmp = phys1+Exp->GetPhys_Offset(j));
+            }
+#else
+            Exp->BwdTrans(coeffs, phys1);
+#endif
+        }
+        t.Stop();
+        NekDouble orig = t.TimePerTest(NBWD);
+        cout << "ExpList: " << orig << endl; 
+
+        t.Start();
+        for (int i = 0; i < NBWD; ++i)
+        {
+            c.ApplyOperator(Collections::eBwdTrans, coeffs, phys2);
+        }
+        t.Stop();
+        NekDouble col = t.TimePerTest(NBWD);
+        cout << "Collection: " << t.TimePerTest(NBWD) << endl;
+
+        cout << "Ratio: " << (orig/col) << endl;
+
+        Vmath::Vsub(phys1.num_elements(), phys1, 1, phys2, 1, phys1, 1);
+
+        cout << Vmath::Vmax(phys1.num_elements(), phys1, 1) << endl;
+    }
+#endif
+
+#if 0 
+    {
+        const int nq = Exp->GetNpoints();
+        Array<OneD, NekDouble> xc(nq), yc(nq);
+        Array<OneD, NekDouble> input(nq), outexp0(nq), outexp1(nq), outcol(2*nq);
+
+        Exp->GetCoords(xc, yc);
+        
+        for (int i = 0; i < nq; ++i)
+        {
+            input[i] = sin(xc[i])*cos(yc[i]);
+        }
+
+        Timer t;
+        t.Start();
+        for (int i = 0; i < NBWD; ++i)
+        {
+            Exp->PhysDeriv(input, outexp0, NullNekDouble1DArray);
+        }
+        t.Stop();
+        NekDouble orig = t.TimePerTest(NBWD);
+        cout << "ExpList: " << orig << endl; 
+
+        t.Start();
+        for (int i = 0; i < NBWD; ++i)
+        {
+            c.ApplyOperator(Collections::ePhysDeriv, input, outcol);
+        }
+        t.Stop();
+        NekDouble col = t.TimePerTest(NBWD);
+        cout << "Collection: " << t.TimePerTest(NBWD) << endl;
+        cout << "Ratio: " << (orig/col) << endl;
+
+        Vmath::Vsub(nq, outexp0, 1, outcol, 1, outexp0, 1);
+        cout << "Error: " << Vmath::Vmax(nq, outexp0, 1) << endl;
+    }
+#endif
+    vSession->Finalise();
+
+    return 0;
+}
+

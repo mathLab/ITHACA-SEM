a9423797f10ada3f88005e0a7702955273d27534
David Moxey <d.moxey@imperial.ac.uk>
2014-08-03 11:01:50 +0100

    Add MacPorts path

--- cmake/FindPETSc.cmake
+++ cmake/FindPETSc.cmake
@@ -75,6 +75,8 @@ find_path (PETSC_DIR include/petsc.h
   /usr/lib/petscdir/3.4 /usr/lib/petscdir/3.4.2
   /usr/lib/petscdir/3.3 /usr/lib/petscdir/3.2 /usr/lib/petscdir/3.1
   /usr/lib/petscdir/3.0.0 /usr/lib/petscdir/2.3.3 /usr/lib/petscdir/2.3.2
+  # MacPorts paths
+  /opt/local/lib/petsc
   $ENV{HOME}/petsc
   DOC "PETSc Directory")
 

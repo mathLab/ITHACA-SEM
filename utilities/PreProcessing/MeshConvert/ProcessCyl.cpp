2c1db781539d3c11366cb6b8dc8b195087d430f2
Chris Cantwell <c.cantwell@imperial.ac.uk>
2014-09-05 15:34:25 +0100

    Tidied up code

--- utilities/PreProcessing/MeshConvert/ProcessCyl.cpp
+++ utilities/PreProcessing/MeshConvert/ProcessCyl.cpp
@@ -14,10 +14,11 @@ namespace Nektar
 {
     namespace Utilities
     {
-        ModuleKey ProcessCyl::className = 
+        ModuleKey ProcessCyl::className =
             GetModuleFactory().RegisterCreatorFunction(
                 ModuleKey(eProcessModule, "cyl"),
                 ProcessCyl::create);
+
         /**
          * @brief Default constructor.
          */
@@ -30,29 +31,29 @@ namespace Nektar
             m_config["N"] = ConfigOption(false, "7",
                 "Number of points along edge.");
         }
-      
+
+
         /**
          * @brief Destructor.
          */
         ProcessCyl::~ProcessCyl()
         {
-            
         }
+
 
         void ProcessCyl::Process()
         {
-            int surfTag = m_config["surf"].as<int>();
             set<pair<int,int> > tmp;
+            int                 surfTag         = m_config["surf"].as<int>();
+            int                 prismedge[2][3] = {{0,5,4}, {2,6,7}};
+            Node                zax(0,0,0,1);
+            int                 dim             = m_mesh->m_expDim;
 
-            int prismedge[2][3] = {{0,5,4}, {2,6,7}};
-            Node zax(0,0,0,1);
-            
-            for (int i = 0; i < m_mesh->m_element[m_mesh->m_expDim].size(); ++i)
+            for (int i = 0; i < m_mesh->m_element[dim].size(); ++i)
             {
-                ElementSharedPtr el = m_mesh->m_element[m_mesh->m_expDim][i];
-                int nSurf = m_mesh->m_expDim == 3 ? el->GetFaceCount() : 
-                    el->GetEdgeCount();
-                        
+                ElementSharedPtr el = m_mesh->m_element[dim][i];
+                int nSurf = dim == 3 ? el->GetFaceCount() : el->GetEdgeCount();
+
                 for (int j = 0; j < nSurf; ++j)
                 {
                     int bl = el->GetBoundaryLink(j);
@@ -61,7 +62,7 @@ namespace Nektar
                         continue;
                     }
 
-                    ElementSharedPtr bEl  = m_mesh->m_element[m_mesh->m_expDim-1][bl];
+                    ElementSharedPtr bEl  = m_mesh->m_element[dim - 1][bl];
                     vector<int>      tags = bEl->GetTagList();
 
                     if (find(tags.begin(), tags.end(), surfTag) ==
@@ -71,36 +72,29 @@ namespace Nektar
                     }
 
                     ASSERTL0(j == 1 || j == 3, "rofl");
-                    
+
                     // Check all edge interior points.
                     for (int k = 0; k < 3; ++k)
                     {
                         EdgeSharedPtr edge = el->GetEdge(prismedge[(j-1)/2][k]);
-
-                        // See if edge is parallel to z axis
-                        /*
-                        if (fabs((*(edge->n1) - *(edge->n2)).dot(zax)) < 1e-6)
-                        {
-                            continue;
-                        }
-                        */
-
                         GenerateEdgeNodes(edge);
                     }
                 }
             }
         }
 
+
         void ProcessCyl::GenerateEdgeNodes(EdgeSharedPtr edge)
         {
             NodeSharedPtr n1 = edge->m_n1;
             NodeSharedPtr n2 = edge->m_n2;
 
-            int nq = m_config["N"].as<int>();
-            double r = m_config["r"].as<double>();
+            int nq    = m_config["N"].as<int>();
+            double r  = m_config["r"].as<double>();
             double t1 = atan2(n1->m_y, n1->m_x);
             double t2 = atan2(n2->m_y, n2->m_x);
-            double dt, dz;
+            double dt;
+            double dz;
 
             if (t1 < -M_PI/2.0 && t2 > 0.0)
             {
@@ -110,7 +104,7 @@ namespace Nektar
             {
                 t2 += 2*M_PI;
             }
-            
+
             dt = (t2-t1) / (nq-1);
             dz = (n2->m_z - n1->m_z) / (nq-1);
 
@@ -119,7 +113,9 @@ namespace Nektar
             for (int i = 1; i < nq-1; ++i)
             {
                 edge->m_edgeNodes[i-1] = NodeSharedPtr(
-                    new Node(0, r*cos(t1 + i*dt), r*sin(t1 + i*dt), n1->m_z + i*dz));
+                    new Node(0, r*cos(t1 + i*dt),
+                                r*sin(t1 + i*dt),
+                                n1->m_z + i*dz));
             }
             edge->m_curveType = LibUtilities::ePolyEvenlySpaced;
         }

c3edfdfdf6e34d353bb0be39f9a87df7684b5d78
David Moxey <d.moxey@imperial.ac.uk>
2014-11-06 14:45:07 +0000

    Refactor Loki installation for Nektar++Config.cmake

--- cmake/FindLoki.cmake
+++ cmake/FindLoki.cmake
@@ -1,36 +1,33 @@
+#
+# FindLoki.cmake: Find Loki headers
+#
 
-# Use LOKI_ADDITIONAL_INCLUDE_DIRS to add additional search directories.
-
-FIND_PATH(LOKI_INCLUDE_DIR loki/Typelist.h 
-	${LOKI_ADDITIONAL_INCLUDE_DIRS}
-	/usr/include 
-	/usr/local/include 
-	${CMAKE_SOURCE_DIR}/ThirdParty/loki/include/ 
-	${CMAKE_SOURCE_DIR}/ThirdParty/loki-0.1.3/include 
-        ${CMAKE_SOURCE_DIR}/ThirdParty/loki-0.1.3/loki-0.1.3/include 
-	${CMAKE_SOURCE_DIR}/../ThirdParty/loki/include/
-	${CMAKE_SOURCE_DIR}/../ThirdParty/loki-0.1.3/include/
-        ${CMAKE_SOURCE_DIR}/../ThirdParty/loki-0.1.3/loki-0.1.3/include 
-	"C:\\Program Files\\Microsoft Visual Studio .NET 2003\\Vc7\\include" 
-	"C:\\Program Files\\Microsoft Visual Studio 8\\VC\\include" 
-)
-
+# Try to find system Loki headers
+FIND_PATH(LOKI_INCLUDE_DIR loki/Typelist.h)
 
 IF (LOKI_INCLUDE_DIR)
-  SET(LOKI_FOUND TRUE)
-ENDIF (LOKI_INCLUDE_DIR)
+    SET(LOKI_FOUND TRUE)
+    SET(LOKI_SYSTEM TRUE)
+ENDIF ()
 
-SET (LOKI_LIB_DIR ${LOKI_INCLUDE_DIR}../lib )
+IF (NOT LOKI_FOUND)
+    FIND_PATH(LOKI_INCLUDE_DIR loki/Typelist.h
+        PATHS ${TPSRC}/loki-0.1.3/include NO_DEFAULT_PATH)
+
+    IF (LOKI_INCLUDE_DIR)
+        SET(LOKI_FOUND TRUE)
+
+        # Copy from source to TPDIST
+        MESSAGE(STATUS ${LOKI_INCLUDE_DIR})
+        FILE(COPY ${LOKI_INCLUDE_DIR}/loki/ DESTINATION ${TPDIST}/include/loki/)
+        SET(LOKI_INCLUDE_DIR ${TPDIST}/include CACHE PATH "" FORCE)
+    ENDIF()
+ENDIF()
 
 IF (LOKI_FOUND)
-  IF (NOT Loki_FIND_QUIETLY)
-     MESSAGE(STATUS "Found Loki: ${LOKI_INCLUDE_DIR}")
-  ENDIF (NOT Loki_FIND_QUIETLY)
-ELSE(LOKI_FOUND)
-  IF (Loki_FIND_REQUIRED)
-     MESSAGE(FATAL_ERROR "Could not find Loki")
-  ENDIF (Loki_FIND_REQUIRED)
-ENDIF (LOKI_FOUND)
+    MESSAGE(STATUS "Found Loki: ${LOKI_INCLUDE_DIR}")
+ELSE()
+    MESSAGE(FATAL_ERROR "Could not find Loki")
+ENDIF()
 
 MARK_AS_ADVANCED(LOKI_INCLUDE_DIR)
-MARK_AS_ADVANCED(LOKI_LIB_DIR)
--- cmake/ThirdPartyLoki.cmake
+++ cmake/ThirdPartyLoki.cmake
@@ -1,17 +1,16 @@
 # Loki
-OPTION(THIRDPARTY_BUILD_LOKI 
-    "Build TinyXML library from ThirdParty." ON)
+OPTION(THIRDPARTY_BUILD_LOKI
+    "Build Loki library from ThirdParty." ON)
 
 IF (THIRDPARTY_BUILD_LOKI)
     IF (NOT EXISTS ${TPSRC}/loki-0.1.3.tar.bz2)
-        file(DOWNLOAD ${TPURL}/loki-0.1.3.tar.bz2 ${TPSRC}/loki-0.1.3.tar.bz2)
+        FILE(DOWNLOAD ${TPURL}/loki-0.1.3.tar.bz2 ${TPSRC}/loki-0.1.3.tar.bz2)
     ENDIF()
-    execute_process(
+    EXECUTE_PROCESS(
         COMMAND ${CMAKE_COMMAND} -E tar jxf ${TPSRC}/loki-0.1.3.tar.bz2
         WORKING_DIRECTORY ${TPSRC}
-    )   
+    )
 ENDIF()
 
 INCLUDE (FindLoki)
-
 INCLUDE_DIRECTORIES(SYSTEM ${LOKI_INCLUDE_DIR})

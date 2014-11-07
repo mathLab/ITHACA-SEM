cad20d3c30a60f55a8e259187fe16eab191925bf
David Moxey <d.moxey@imperial.ac.uk>
2014-11-07 22:53:37 +0000

    Refactor PETSc, ARPACK

--- cmake/ThirdPartyPETSc.cmake
+++ cmake/ThirdPartyPETSc.cmake
@@ -1,22 +1,37 @@
+########################################################################
+#
+# ThirdParty configuration for Nektar++
+#
+# PETSc
+#
+########################################################################
+
 OPTION(NEKTAR_USE_PETSC
     "Enable PETSc parallel matrix solver support." OFF)
 
-CMAKE_DEPENDENT_OPTION(THIRDPARTY_BUILD_PETSC
-    "Build PETSc if needed" OFF
-    "NEKTAR_USE_PETSC" OFF)
-
 IF (NEKTAR_USE_PETSC)
-    ADD_DEFINITIONS(-DNEKTAR_USING_PETSC)
+    SET(PETSC_FIND_QUIETLY ON)
+    INCLUDE(FindPETSc)
+
+    IF (PETSC_FOUND)
+        SET(BUILD_PETSC OFF)
+    ELSE()
+        SET(BUILD_PETSC ON)
+    ENDIF()
+
+    CMAKE_DEPENDENT_OPTION(THIRDPARTY_BUILD_PETSC
+        "Build PETSc if needed" ${BUILD_PETSC}
+        "NEKTAR_USE_PETSC" OFF)
 
     IF (THIRDPARTY_BUILD_PETSC)
         INCLUDE(ExternalProject)
 
-        SET(PETSC_C_COMPILER "${CMAKE_C_COMPILER}")
+        SET(PETSC_C_COMPILER   "${CMAKE_C_COMPILER}")
         SET(PETSC_CXX_COMPILER "${CMAKE_CXX_COMPILER}")
 
         IF (NEKTAR_USE_MPI)
             IF (NOT MPI_BUILTIN)
-                SET(PETSC_C_COMPILER "${MPI_C_COMPILER}")
+                SET(PETSC_C_COMPILER   "${MPI_C_COMPILER}")
                 SET(PETSC_CXX_COMPILER "${MPI_CXX_COMPILER}")
             ENDIF (NOT MPI_BUILTIN)
         ELSE (NEKTAR_USE_MPI)
@@ -44,20 +59,21 @@ IF (NEKTAR_USE_PETSC)
                 --with-petsc-arch=c-opt
                 --with-fc=0
                 ${PETSC_NO_MPI}
-                )
+        )
 
-        INCLUDE_DIRECTORIES(${TPDIST}/include)
-        SET(PETSC_LIBRARIES "${TPDIST}/lib/libpetsc.a")
+        SET(PETSC_LIBRARIES petsc CACHE FILEPATH
+            "PETSc library" FORCE)
+        SET(PETSC_INCLUDES ${TPDIST}/include CACHE FILEPATH
+            "PETSc includes" FORCE)
+
+        LINK_DIRECTORIES(${TPDIST}/lib)
         MESSAGE(STATUS "Build PETSc: ${PETSC_LIBRARIES}")
-    ELSE (THIRDPARTY_BUILD_PETSC)
-        INCLUDE(FindPETSc)
-        IF (NOT PETSC_FOUND)
-            MESSAGE(FATAL_ERROR "Could not find PETSc")
-        ELSE (NOT PETSC_FOUND)
-            MESSAGE(STATUS "Found PETSc: ${PETSC_LIBRARIES}")
-        ENDIF (NOT PETSC_FOUND)
-        INCLUDE_DIRECTORIES(${PETSC_INCLUDES})
-    ENDIF (THIRDPARTY_BUILD_PETSC)
-ENDIF( NEKTAR_USE_PETSC )
+        SET(PETSC_CONFIG_INCLUDE_DIR ${TPINC})
+    ELSE ()
+        MESSAGE(STATUS "Found PETSc: ${PETSC_LIBRARIES}")
+        SET(PETSC_CONFIG_INCLUDE_DIR ${PETSC_INCLUDES})
+    ENDIF ()
 
-INCLUDE_DIRECTORIES(${PETSC_INCLUDES})
+    ADD_DEFINITIONS(-DNEKTAR_USING_PETSC)
+    INCLUDE_DIRECTORIES(SYSTEM ${PETSC_INCLUDES})
+ENDIF( NEKTAR_USE_PETSC )

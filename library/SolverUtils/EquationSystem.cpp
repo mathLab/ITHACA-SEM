f9ad41b5a82851be885198b20bb055fa6e43409b
Kilian Lackhove <lackhove@ekt.tu-darmstadt.de>
2015-01-08 17:33:33 +0100

    try to fix failing windows regression test pt1

--- library/SolverUtils/EquationSystem.cpp
+++ library/SolverUtils/EquationSystem.cpp
@@ -807,7 +807,16 @@ namespace Nektar
                 {
                     try
                     {
+#ifdef _WIN32
+                        // We need this to make sure boost::format has always
+                        // two digits in the exponents of Scientific notation.
+                        unsigned int old_exponent_format;
+                        old_exponent_format = _set_output_format(_TWO_DIGIT_EXPONENT);
                         filename = boost::str(boost::format(filename) % m_time);
+                        _set_output_format(old_exponent_format);
+#else
+                        filename = boost::str(boost::format(filename) % m_time);
+#endif
                     }
                     catch (...)
                     {

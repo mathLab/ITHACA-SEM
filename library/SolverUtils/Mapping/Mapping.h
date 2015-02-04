60b82ed8b6d819e7cff4e1089a701b9eef4e27d6
Douglas Serson <d.serson@gmail.com>
2015-02-04 12:54:39 +0000

    Add Mapping base class to deal with coordinate transformations

---
+++ library/SolverUtils/Mapping/Mapping.h
@@ -0,0 +1,384 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: Mapping.h
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// License for the specific language governing rights and limitations under
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: Abstract base class for mappings.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef NEKTAR_SOLVERUTILS_MAPPING
+#define NEKTAR_SOLVERUTILS_MAPPING
+
+#include <string>
+
+#include <LibUtilities/BasicUtils/NekFactory.hpp>
+#include <LibUtilities/BasicUtils/SharedArray.hpp>
+#include <MultiRegions/ExpList.h>
+#include <SolverUtils/SolverUtilsDeclspec.h>
+
+namespace Nektar
+{
+namespace SolverUtils
+{
+    //  Forward declaration
+    class Mapping;
+
+    /// A shared pointer to an EquationSystem object
+    SOLVER_UTILS_EXPORT typedef boost::shared_ptr<Mapping> MappingSharedPtr;
+
+    /// Declaration of the mapping factory
+    typedef LibUtilities::NekFactory<std::string, Mapping,
+            const LibUtilities::SessionReaderSharedPtr&,
+            const Array<OneD, MultiRegions::ExpListSharedPtr>&,
+            const TiXmlElement*> MappingFactory;
+
+    /// Declaration of the mapping factory singleton
+    SOLVER_UTILS_EXPORT MappingFactory& GetMappingFactory();
+
+    /**
+     * @class Mapping
+     * @brief Defines a mapping to be applied to the coordinate system
+     */
+    class Mapping
+    {
+        public:
+            SOLVER_UTILS_EXPORT virtual ~Mapping() {}
+
+            /// Initialise the mapping object
+            SOLVER_UTILS_EXPORT void InitObject(
+                const Array<OneD, MultiRegions::ExpListSharedPtr>&       pFields,
+                const TiXmlElement* pMapping);
+            
+            SOLVER_UTILS_EXPORT static MappingSharedPtr Load(
+                        const LibUtilities::SessionReaderSharedPtr& pSession,
+                        const Array<OneD, MultiRegions::ExpListSharedPtr>& pFields);
+            
+            /////////////////////////////////////////////////////////////
+            //
+            //    Functions for transforming results to and from the
+            //          Cartesian coordinate system 
+            //          (useful for pre and post processing)
+            /////////////////////////////////////////////////////////////
+              
+            /// Convert a contravariant vector to the Cartesian system
+            SOLVER_UTILS_EXPORT void ContravarToCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_ContravarToCartesian( inarray, outarray);
+            }
+            
+            /// Convert a covariant vector to the Cartesian system
+            SOLVER_UTILS_EXPORT void CovarToCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_CovarToCartesian( inarray, outarray);
+            }
+            
+            /// Convert a contravariant vector to the transformed system
+            SOLVER_UTILS_EXPORT void ContravarFromCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_ContravarFromCartesian( inarray, outarray);
+            }
+            
+            /// Convert a covariant vector to the transformed system
+            SOLVER_UTILS_EXPORT void CovarFromCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_CovarFromCartesian( inarray, outarray);
+            }
+            
+            /// Calculate a partial derivative at the Cartesian system
+            SOLVER_UTILS_EXPORT void CartesianDeriv(
+                Direction edir,
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_CartesianDeriv( edir, inarray, outarray);
+            }       
+            
+            /////////////////////////////////////////////////////////////
+            //
+            //   Basic tensor calculus functions
+            //    
+            /////////////////////////////////////////////////////////////   
+
+            /// Get the Jacobian of the transformation
+            SOLVER_UTILS_EXPORT void GetJacobian(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_GetJacobian( outarray);
+            } 
+            
+            /// Get the metric tensor g_(i,j)
+            SOLVER_UTILS_EXPORT void GetMetricTensor(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_GetMetricTensor( outarray);
+            }  
+            
+            /// Get the inverse of metric tensor g^(i,j)
+            SOLVER_UTILS_EXPORT void GetInvMetricTensor(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_GetInvMetricTensor( outarray);
+            }
+            
+            /// Lower index using v_(i) = g_(i,j)*v^(j)
+            SOLVER_UTILS_EXPORT void LowerIndex(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_LowerIndex( inarray, outarray);
+            }     
+            
+            /// Raise index using v^(i) = g^(i,j)*v_(j)
+            SOLVER_UTILS_EXPORT void RaiseIndex(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_RaiseIndex( inarray, outarray);
+            }
+            
+            // Apply the Christoffel symbols to a contravariant vector
+            //          outarray = {i,pk}*u^p
+            SOLVER_UTILS_EXPORT void ApplyChristoffelContravar(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_ApplyChristoffelContravar( inarray, outarray);
+            }
+            
+            // Apply the Christoffel symbols to a convariant vector
+            //          outarray = {p,ik}*u_p
+            SOLVER_UTILS_EXPORT void ApplyChristoffelCovar(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_ApplyChristoffelCovar( inarray, outarray);
+            }
+
+            
+            /////////////////////////////////////////////////////////////
+            //
+            //   Functions used at IncNs
+            //    
+            /////////////////////////////////////////////////////////////             
+            
+            // Generalized divergence operator
+            //         D = u^i_,i = 1/J*d(J*u^i)/dx^i
+            SOLVER_UTILS_EXPORT void Divergence(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, NekDouble>                            &outarray)
+            {
+                v_Divergence( inarray, outarray);
+            }  
+            
+            // Generalized velocity Laplacian operator
+            //         L = g^jk u^i_{,jk}
+            SOLVER_UTILS_EXPORT void VelocityLaplacian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_VelocityLaplacian( inarray, outarray);
+            } 
+            
+            // Correction needed for convective terms = N(u) - (u \nabla) u
+            //     inarray is the velocity field
+            SOLVER_UTILS_EXPORT void IncNSAdvectionCorrection(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_IncNSAdvectionCorrection( inarray, outarray);
+            } 
+            
+            // Correction needed for pressure terms =  -g^(ij)p_j + (\nabla p)/J
+            //     inarray is the pressure field
+            SOLVER_UTILS_EXPORT void IncNSPressureCorrection(
+                const Array<OneD, NekDouble>                      &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_IncNSPressureCorrection( inarray, outarray);
+            }
+            
+            // Correction needed for viscous terms = g^jk u^i_{,jk}-(\nabla^2 u)
+            //     inarray is the velocity field
+            SOLVER_UTILS_EXPORT void IncNSViscousCorrection(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray)
+            {
+                v_IncNSViscousCorrection( inarray, outarray);
+            }
+            
+            
+            /////////////////////////////////////////////////////////////
+            //
+            //   Functions defining mapping properties
+            //    
+            /////////////////////////////////////////////////////////////             
+            
+            // Define if mapping is constant or time-dependent
+            SOLVER_UTILS_EXPORT bool IsTimeDependent()
+            {
+                return v_IsTimeDependent();
+            }          
+            
+            // Define if the Jacobian of the transformation is constant
+            SOLVER_UTILS_EXPORT bool HasConstantJacobian()
+            {
+                return v_HasConstantJacobian();
+            }      
+            
+            
+            //
+            //  Function to update time-dependent mappings
+            //
+            SOLVER_UTILS_EXPORT void UpdateMapping()
+            {
+                v_UpdateMapping();
+            }
+            
+
+        protected:
+            /// Session reader
+            LibUtilities::SessionReaderSharedPtr        m_session;
+            /// Fields
+            Array<OneD, MultiRegions::ExpListSharedPtr> m_fields;
+            // Arrays with geometric parameters of the mapping
+            Array<OneD, Array<OneD, NekDouble> >        m_GeometricInfo;
+
+
+            /// Constructor
+            SOLVER_UTILS_EXPORT Mapping(
+                const LibUtilities::SessionReaderSharedPtr&,
+                const Array<OneD, MultiRegions::ExpListSharedPtr>&   pFields);
+
+            // Evaluators
+            SOLVER_UTILS_EXPORT void EvaluateFunction(
+                    Array<OneD, MultiRegions::ExpListSharedPtr> pFields,
+                    LibUtilities::SessionReaderSharedPtr        pSession,
+                    std::string                                 pFieldName, 
+                    Array<OneD, NekDouble>&                     pArray,
+                    const std::string& pFunctionName,
+                    NekDouble pTime = NekDouble(0));
+
+            SOLVER_UTILS_EXPORT void EvaluateTimeFunction(
+                    LibUtilities::SessionReaderSharedPtr        pSession,
+                    std::string                                 pFieldName, 
+                    Array<OneD, NekDouble>&                     pArray,
+                    const std::string&                          pFunctionName,
+                    NekDouble pTime = NekDouble(0));            
+            
+            // Virtual functions
+            SOLVER_UTILS_EXPORT virtual void v_InitObject(
+                const Array<OneD, MultiRegions::ExpListSharedPtr>&   pFields,
+                const TiXmlElement* pForce) = 0;
+
+            SOLVER_UTILS_EXPORT virtual void v_ContravarToCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+
+            SOLVER_UTILS_EXPORT virtual void v_CovarToCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;            
+
+            SOLVER_UTILS_EXPORT virtual void v_ContravarFromCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+
+            SOLVER_UTILS_EXPORT virtual void v_CovarFromCartesian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0; 
+            
+            SOLVER_UTILS_EXPORT virtual void v_CartesianDeriv(
+                Direction edir,
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0; 
+            
+            SOLVER_UTILS_EXPORT virtual void v_GetJacobian(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_GetMetricTensor(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_GetInvMetricTensor(
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_LowerIndex(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_RaiseIndex(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+
+            SOLVER_UTILS_EXPORT virtual void v_ApplyChristoffelContravar(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_ApplyChristoffelCovar(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_Divergence(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, NekDouble>                            &outarray) =0; 
+            
+            SOLVER_UTILS_EXPORT virtual void v_VelocityLaplacian(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;  
+            
+            SOLVER_UTILS_EXPORT virtual void v_IncNSAdvectionCorrection(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_IncNSPressureCorrection(
+                const Array<OneD, NekDouble>                      &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_IncNSViscousCorrection(
+                const Array<OneD, Array<OneD, NekDouble> >        &inarray,
+                Array<OneD, Array<OneD, NekDouble> >              &outarray) =0;
+            
+            SOLVER_UTILS_EXPORT virtual bool v_IsTimeDependent() =0;  
+            
+            SOLVER_UTILS_EXPORT virtual bool v_HasConstantJacobian() =0;
+            
+            SOLVER_UTILS_EXPORT virtual void v_UpdateMapping() =0;
+            
+    };
+}
+}
+
+#endif

5908394ba9e7984ad9a806583cb8dda8b6de1752
Douglas Serson <d.serson@gmail.com>
2015-03-06 11:24:42 +0000

    Fix wrong signs in SVV filter of StdHexExp

--- library/StdRegions/StdHexExp.cpp
+++ library/StdRegions/StdHexExp.cpp
@@ -2324,7 +2324,7 @@ namespace Nektar
                     {
                         if((i >= cutoff)||(j >= cutoff)||(k >= cutoff))
                         {
-                            orthocoeffs[i*nmodes_a*nmodes_b + j*nmodes_c + k] *= (SvvDiffCoeff*exp(-fac[i]+fac[j]+fac[k]));
+                            orthocoeffs[i*nmodes_a*nmodes_b + j*nmodes_c + k] *= (SvvDiffCoeff*exp( -(fac[i]+fac[j]+fac[k]) ));
                         }
                         else
                         {

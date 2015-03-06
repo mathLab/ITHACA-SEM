5d1b0a839f9ce01afc14c06ab4a68f512ac00aa7
Chris Cantwell <c.cantwell@imperial.ac.uk>
2015-03-06 09:52:15 +0000

    Fix decl header in Collection.h

--- library/Collections/Collection.h
+++ library/Collections/Collection.h
@@ -38,7 +38,7 @@
 
 #include <StdRegions/StdExpansion.h>
 #include <SpatialDomains/Geometry.h>
-#include <Collections/CollectionDeclspec.h>
+#include <Collections/CollectionsDeclspec.h>
 #include <Collections/Operator.h>
 #include <Collections/CoalescedGeomData.h>
 

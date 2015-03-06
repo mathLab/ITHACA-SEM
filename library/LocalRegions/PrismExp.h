83072830182fb43c7b5f88642e6c3775a9894a49
Douglas Serson <d.serson@gmail.com>
2015-03-06 11:07:52 +0000

    Use Jacobian  to make SVV operator symmetric for deformed elements

--- library/LocalRegions/PrismExp.h
+++ library/LocalRegions/PrismExp.h
@@ -178,6 +178,10 @@ namespace Nektar
                             const Array<OneD, const NekDouble> &inarray,
                                   Array<OneD,NekDouble> &outarray,
                             const StdRegions::StdMatrixKey &mkey);
+            
+            LOCAL_REGIONS_EXPORT virtual void v_SVVLaplacianFilter(
+                    Array<OneD, NekDouble> &array,
+                    const StdRegions::StdMatrixKey &mkey);
             //---------------------------------------
             // Matrix creation functions
             //---------------------------------------
@@ -200,6 +204,7 @@ namespace Nektar
             virtual void v_GetSimplexEquiSpacedConnectivity(
                 Array<OneD, int> &conn,
                 bool standard = true);
+            
 
         private:
             LibUtilities::NekManager<MatrixKey, DNekScalMat,

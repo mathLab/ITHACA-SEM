999d532f68e925a8ac572e76e3dc5a7debf1d7c0
ssherw <s.sherwin@imperial.ac.uk>
2015-05-18 22:12:57 +0100

    Updated CMakeList to put TARGET_LINK_LIBRARIES further down file. Also re-added xmove, ymove and zmove into ReadCurves in MeshGraph

--- library/SpatialDomains/MeshGraph.cpp
+++ library/SpatialDomains/MeshGraph.cpp
@@ -1151,6 +1151,48 @@ namespace Nektar
                 zscale = expEvaluator.Evaluate(expr_id);
             }
 
+            NekDouble xmove,ymove,zmove;
+
+            // check to see if any moving parameters are in
+            // attributes and determine these values
+
+            //LibUtilities::ExpressionEvaluator expEvaluator;
+            const char *xmov =  element->Attribute("XMOVE");
+            if(!xmov)
+            {
+                xmove = 0.0;
+            }
+            else
+            {
+                std::string xmovstr = xmov;
+                int expr_id = expEvaluator.DefineFunction("",xmovstr);
+                xmove = expEvaluator.Evaluate(expr_id);
+            }
+
+            const char *ymov =  element->Attribute("YMOVE");
+            if(!ymov)
+            {
+                ymove = 0.0;
+            }
+            else
+            {
+                std::string ymovstr = ymov;
+                int expr_id = expEvaluator.DefineFunction("",ymovstr);
+                ymove = expEvaluator.Evaluate(expr_id);
+            }
+
+            const char *zmov = element->Attribute("ZMOVE");
+            if(!zmov)
+            {
+                zmove = 0.0;
+            }
+            else
+            {
+                std::string zmovstr = zmov;
+                int expr_id = expEvaluator.DefineFunction("",zmovstr);
+                zmove = expEvaluator.Evaluate(expr_id);
+            }
+
             int err;
 
             /// Look for elements in CURVE block.

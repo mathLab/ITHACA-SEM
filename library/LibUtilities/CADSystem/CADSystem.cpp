352f629782a2b5740fa54558755813579c8778de
Michael Turner <m.turner14@imperial.ac.uk>
2015-06-04 15:29:48 +0100

    working shell for opencascade CAD engine

---
+++ library/LibUtilities/CADSystem/CADSystem.cpp
@@ -0,0 +1,16 @@
+
+
+#include <LibUtilities/CADSystem/CADSystem.h>
+#include "occ.h"
+
+using namespace std;
+namespace Nektar{
+    namespace LibUtilities{
+
+	std::string CADSystem::GetName()
+	{
+	    return m_name;
+	}
+
+    }
+}

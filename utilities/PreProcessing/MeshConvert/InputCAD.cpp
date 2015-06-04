e2373b72fefebf763f5a53fde59d541b4ee67bb1
Michael Turner <m.turner14@imperial.ac.uk>
2015-06-04 22:13:30 +0100

    missing file

---
+++ utilities/PreProcessing/MeshConvert/InputCAD.cpp
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File: InputCAD.cpp
+//
+//  For more information, please see: http://www.nektar.info/
+//
+//  The MIT License
+//
+//  Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+//  Department of Aeronautics, Imperial College London (UK), and Scientific
+//  Computing and Imaging Institute, University of Utah (USA).
+//
+//  License for the specific language governing rights and limitations under
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included
+//  in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//  Description: GMSH converter.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "MeshElements.h"
+#include "InputCAD.h"
+
+namespace Nektar
+{
+    namespace Utilities
+    {
+        ModuleKey InputCAD::className =
+        GetModuleFactory().RegisterCreatorFunction(
+            ModuleKey(eInputModule, "STEP"), InputCAD::create,
+            "Reads CAD geometry and will generate the mesh file.");
+        
+        /**
+         * @brief Set up InputNekpp object.
+         */
+        InputCAD::InputCAD(MeshSharedPtr m) : InputModule(m)
+        {
+            
+        }
+        
+        InputCAD::~InputCAD()
+        {
+            
+        }
+        
+        /**
+         * Gmsh file contains a list of nodes and their coordinates, along with
+         * a list of elements and those nodes which define them. We read in and
+         * store the list of nodes in #m_node and store the list of elements in
+         * #m_element. Each new element is supplied with a list of entries from
+         * #m_node which defines the element. Finally some mesh statistics are
+         * printed.
+         *
+         * @param   pFilename           Filename of Gmsh file to read.
+         */
+        void InputCAD::Process()
+        {
+            cout << "hello" << endl;
+        }
+    }
+}

162c2a45205b9f5e90d2f987c32a1394060168a0
Michael Turner <m.turner14@imperial.ac.uk>
2015-06-04 17:33:22 +0100

    added cad loader and cad data structures

--- library/LibUtilities/CADSystem/CADSystem.cpp
+++ library/LibUtilities/CADSystem/CADSystem.cpp
@@ -1,16 +1,143 @@
 
-
-#include <LibUtilities/CADSystem/CADSystem.h>
 #include "occ.h"
+#include <LibUtilities/CADSystem/CADSystem.h>
+
 
 using namespace std;
 namespace Nektar{
-    namespace LibUtilities{
-
-	std::string CADSystem::GetName()
-	{
-	    return m_name;
-	}
-
+namespace LibUtilities{
+    
+    CADCurve::CADCurve(int i, TopoDS_Shape in) : ID(i)
+    {
+        gp_Trsf transform;
+        gp_Pnt ori(0.0,0.0,0.0);
+        transform.SetScale(ori,1.0/1000.0);
+        TopLoc_Location mv(transform);
+        in.Move(mv);
+        occCurve = BRepAdaptor_Curve(TopoDS::Edge(in));
     }
+    
+    CADSurf::CADSurf(int i, TopoDS_Shape in, vector<int> ein) : ID(i), edges(ein)
+    {
+        gp_Trsf transform;
+        gp_Pnt ori(0.0,0.0,0.0);
+        transform.SetScale(ori,1.0/1000.0);
+        TopLoc_Location mv(transform);
+        s = BRep_Tool::Surface(TopoDS::Face(in));
+        in.Move(mv);
+        occSurface = BRepAdaptor_Surface(TopoDS::Face(in));
+    }
+
+    string CADSystem::GetName()
+    {
+        return m_name;
+    }
+
+    bool CADSystem::LoadCAD()
+    {
+        string ext;
+        size_t pos = m_name.find(".");
+        ext = m_name.substr(pos);
+
+        TopoDS_Shape shape;
+        
+        if(ext.compare(".STEP") == 0 ||
+           ext.compare(".step") == 0 ||
+           ext.compare(".stp")  == 0 ||
+           ext.compare(".STP")  == 0 )
+        {
+            STEPControl_Reader reader;
+            reader = STEPControl_Reader();
+            reader.ReadFile(m_name.c_str());
+            reader.NbRootsForTransfer();
+            reader.TransferRoots();
+            shape = reader.OneShape();
+            if(shape.IsNull())
+            {
+                return false;
+            }
+        }
+        else if(ext.compare(".IGES") == 0 ||
+                ext.compare(".iges") == 0 ||
+                ext.compare(".igs")  == 0 ||
+                ext.compare(".IGS")  == 0 )
+        {
+            IGESControl_Reader reader;
+            reader = IGESControl_Reader();
+            reader.ReadFile(m_name.c_str());
+            reader.NbRootsForTransfer();
+            reader.TransferRoots();
+            shape = reader.OneShape();
+            if(shape.IsNull())
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+        
+        TopTools_IndexedMapOfShape unfilteredEdges;
+        TopTools_IndexedMapOfShape mapOfFaces;
+        TopTools_IndexedMapOfShape mapOfEdges;
+        TopExp::MapShapes(shape,TopAbs_FACE,mapOfFaces);
+        TopExp::MapShapes(shape,TopAbs_EDGE,unfilteredEdges);
+        
+        for(int i=1; i<=unfilteredEdges.Extent(); i++)
+        {
+            TopoDS_Shape edge = unfilteredEdges.FindKey(i);
+            BRepAdaptor_Curve curve = BRepAdaptor_Curve(TopoDS::Edge(edge));
+            if(curve.GetType() == 7)
+            {
+                continue;
+            }
+            mapOfEdges.Add(edge);
+        }
+
+        m_numCurve = mapOfFaces.Extent();
+        m_numSurf =  mapOfEdges.Extent();
+        
+        for(int i=1; i<=mapOfEdges.Extent(); i++)
+        {
+            TopoDS_Shape edge = mapOfEdges.FindKey(i);
+            
+            AddCurve(i, edge);
+        }
+        
+        for(int i = 1; i <= mapOfFaces.Extent(); i++)
+        {
+            TopoDS_Shape face= mapOfFaces.FindKey(i);
+            
+            TopTools_IndexedMapOfShape localEdges;
+            TopTools_IndexedMapOfShape unfilteredLocalEdges;
+            TopExp::MapShapes(face,TopAbs_EDGE,unfilteredLocalEdges);
+
+            for(int j = 1; j <= unfilteredLocalEdges.Extent(); j++)
+            {
+                TopoDS_Shape edge = unfilteredLocalEdges.FindKey(j);
+                BRepAdaptor_Curve curve = BRepAdaptor_Curve(TopoDS::Edge(edge));
+                if(curve.GetType() != 7)
+                {
+                    localEdges.Add(edge);
+                }
+            }
+            
+            vector<int> edges;
+            edges.resize(localEdges.Extent());
+            
+            for(int j=0; j<localEdges.Extent(); j++)
+            {
+                edges[j] = mapOfEdges.FindIndex(localEdges.FindKey(j+1));
+            }
+            
+            AddSurf(i, face, edges);
+        }
+        
+        return true;
+    }
+    
+    
+
+}
 }
--- library/LibUtilities/CADSystem/CADSystem.h
+++ library/LibUtilities/CADSystem/CADSystem.h
@@ -15,7 +15,29 @@
 #include <LibUtilities/Memory/NekMemoryManager.hpp>
 
 namespace Nektar {
-    namespace LibUtilities {
+namespace LibUtilities {
+    
+    class CADCurve
+    {
+    public:
+        CADCurve(int i, TopoDS_Shape in);
+    
+    private:
+        int ID;
+        BRepAdaptor_Curve occCurve;
+    };
+        
+    class CADSurf
+    {
+    public:
+        CADSurf(int i, TopoDS_Shape in, std::vector<int> ein);
+        
+    private:
+        int ID;
+        BRepAdaptor_Surface occSurface;
+        Handle(Geom_Surface) s;
+        std::vector<int> edges;
+    };
 
 	class CADSystem
 	{
@@ -26,13 +48,31 @@ namespace Nektar {
 	    {
 	    }
 	    LIB_UTILITIES_EXPORT std::string GetName();
+        LIB_UTILITIES_EXPORT bool LoadCAD();
 
 	private:
+        
+        void AddCurve(int i, TopoDS_Shape in)
+        {
+            CADCurve newCurve(i, in);
+            m_curves.push_back(newCurve);
+        }
+        void AddSurf(int i, TopoDS_Shape in, std::vector<int> ein)
+        {
+            CADSurf newSurf(i, in, ein);
+            m_surfs.push_back(newSurf);
+        }
+        
 	    std::string m_name;
+        int m_numCurve;
+        int m_numSurf;
+        std::vector<CADCurve> m_curves;
+        std::vector<CADSurf> m_surfs;
 	};
 
 	typedef boost::shared_ptr<CADSystem> CADSystemSharedPtr;
-    }
+
+}
 }
 
 #endif

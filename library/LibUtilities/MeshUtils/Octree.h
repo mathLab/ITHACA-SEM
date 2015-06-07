ac9e8230c52335cb5ed862c5f9699da08fad56d5
Michael Turner <m.turner14@imperial.ac.uk>
2015-06-07 11:27:03 +0100

    added basics of octree and passed cad properly

---
+++ library/LibUtilities/MeshUtils/Octree.h
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File: Octree.h
+//
+//  For more information, please see: http://www.nektar.info/
+//
+//  The MIT License
+//
+//  Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+//  Department of Aeronautics, Imperial College London (UK), and Scientific
+//  Computing and Imaging Institute, University of Utah (USA).
+//
+//  License for the specific language governing rights and limitations under
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included
+//  in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//  Description: cad object methods.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef NEKTAR_LIB_UTILITIES_MESHUTILS_OCTREE_OCTREE_H
+#define NEKTAR_LIB_UTILITIES_MESHUTILS_OCTREE_OCTREE_H
+
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+
+#include <LibUtilities/CADSystem/CADSystem.h>
+
+#include <LibUtilities/LibUtilitiesDeclspec.h>
+#include <LibUtilities/Memory/NekMemoryManager.hpp>
+
+
+namespace Nektar {
+    namespace LibUtilities {
+        namespace MeshUtils {
+        
+            class Octree
+            {
+            public:
+                friend class MemoryManager<Octree>;
+                
+                LIB_UTILITIES_EXPORT Octree(const CADSystemSharedPtr &cad) : m_cad(cad)
+                {
+                };
+                
+                
+                
+            private:
+                
+                CADSystemSharedPtr m_cad;
+               
+            };
+            
+            typedef boost::shared_ptr<Octree> OctreeSharedPtr;
+        }
+        
+    }
+}
+
+#endif

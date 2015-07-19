045ac924ced656dd86aaee28989fd2e681e48810
Michael Turner <m.turner14@imperial.ac.uk>
2015-07-19 14:14:00 +0100

    auto occ install for mac, clean up other cmake bits

--- cmake/ThirdPartyOCC.cmake
+++ cmake/ThirdPartyOCC.cmake
@@ -1,6 +1,34 @@
 OPTION(NEKTAR_USE_OCC "Use opencascade for geometry interface." OFF)
 
 IF(NEKTAR_USE_OCC)
-	INCLUDE(FindOCC)
-	INCLUDE_DIRECTORIES(${OCC_INCLUDE_DIR})
+
+	SET(BUILD_OCC ON)
+
+	OPTION(THIRDPARTY_DOWNLOAD_OCC
+	    "Get OpenCascade from ThirdParty." ${BUILD_OCC})
+
+	IF (THIRDPARTY_DOWNLOAD_OCC)
+	    INCLUDE(ExternalProject)
+	    EXTERNALPROJECT_ADD(
+	        opencascade-6.8
+	        PREFIX ${TPSRC}
+	        URL http://ae-nektar.ae.ic.ac.uk/~mt4313/OCC680osx64.tgz
+	        URL_MD5 626292523b0691304f0fa271989fbc44
+	        STAMP_DIR ${TPBUILD}/stamp
+	        DOWNLOAD_DIR ${TPSRC}
+	        SOURCE_DIR ${TPSRC}/opencascade-6.8
+	        INSTALL_DIR ${TPDIST}
+			UPDATE_COMMAND ""
+			CONFIGURE_COMMAND ""
+	        BUILD_COMMAND ""
+			INSTALL_COMMAND ""
+	    )
+	SET(OCC_LIBS PTKernel TKernel TKMath TKBRep TKIGES TKSTEP TKSTEPAttr TKSTEP209 TKSTEPBase TKShapeSchema TKGeomBase TKGeomAlgo TKG3d TKG2d TKXSBase TKPShape TKTopAlgo)
+	LINK_DIRECTORIES(${TPSRC}/opencascade-6.8/i686/lib)
+	INCLUDE_DIRECTORIES(SYSTEM ${TPSRC}/opencascade-6.8/i686/inc)
+
+	ENDIF (THIRDPARTY_DOWNLOAD_OCC)
+
+
+
 ENDIF(NEKTAR_USE_OCC)

6a445b2f9bf3c0291ff6dd13e2861e25670deab9
Michael Turner <m.turner14@imperial.ac.uk>
2015-07-22 13:39:57 +0100

    build cadsystem, install correctly

--- cmake/ThirdPartyOCC.cmake
+++ cmake/ThirdPartyOCC.cmake
@@ -8,7 +8,7 @@ IF(NEKTAR_USE_OCC)
 
 	IF (THIRDPARTY_DOWNLOAD_OCC)
 		IF(WIN32)
-			message(SEND_ERROR "Cannot use opencascade with netkar++ on windows")
+			message(SEND_ERROR "Cannot use opencascade with Nektar++ on windows")
 		ELSEIF(APPLE)
 	    	INCLUDE(ExternalProject)
 		    EXTERNALPROJECT_ADD(
@@ -17,17 +17,18 @@ IF(NEKTAR_USE_OCC)
 		        URL http://ae-nektar.ae.ic.ac.uk/~mt4313/OCC680osx64.tgz
 		        URL_MD5 626292523b0691304f0fa271989fbc44
 		        STAMP_DIR ${TPBUILD}/stamp
+				BINARY_DIR ${TPBUILD}/opencascade-6.8
 		        DOWNLOAD_DIR ${TPSRC}
 		        SOURCE_DIR ${TPSRC}/opencascade-6.8
 		        INSTALL_DIR ${TPDIST}
 				UPDATE_COMMAND ""
 				CONFIGURE_COMMAND ""
 		        BUILD_COMMAND ""
-				INSTALL_COMMAND ""
+				INSTALL_COMMAND cp -a ${TPSRC}/opencascade-6.8/i686/lib/. ${TPDIST}/lib/ COMMAND cp -a ${TPSRC}/opencascade-6.8/i686/inc/. ${TPDIST}/include/
 		    )
 			SET(OCC_LIBS PTKernel TKernel TKMath TKBRep TKIGES TKSTEP TKSTEPAttr TKSTEP209 TKSTEPBase TKShapeSchema TKGeomBase TKGeomAlgo TKG3d TKG2d TKXSBase TKPShape TKTopAlgo)
-			LINK_DIRECTORIES(${TPSRC}/opencascade-6.8/i686/lib)
-			INCLUDE_DIRECTORIES(SYSTEM ${TPSRC}/opencascade-6.8/i686/inc)
+			LINK_DIRECTORIES(${TPDIST}/lib)
+			INCLUDE_DIRECTORIES(SYSTEM ${TPDIST}/include)
 		ELSE()
 			INCLUDE(ExternalProject)
 		    EXTERNALPROJECT_ADD(
@@ -42,11 +43,12 @@ IF(NEKTAR_USE_OCC)
 				UPDATE_COMMAND ""
 				CONFIGURE_COMMAND ""
 		        BUILD_COMMAND ""
-				INSTALL_COMMAND ""
+				INSTALL_COMMAND INSTALL_COMMAND cp -a ${TPSRC}/opencascade-6.8/i686/lib/. ${TPDIST}/lib/ COMMAND cp -a ${TPSRC}/opencascade-6.8/i686/inc/. ${TPDIST}/include/
 		    )
+
 			SET(OCC_LIBS PTKernel TKernel TKMath TKBRep TKIGES TKSTEP TKSTEPAttr TKSTEP209 TKSTEPBase TKShapeSchema TKGeomBase TKGeomAlgo TKG3d TKG2d TKXSBase TKPShape TKTopAlgo)
-			LINK_DIRECTORIES(${TPSRC}/opencascade-6.8/lib)
-			INCLUDE_DIRECTORIES(SYSTEM ${TPSRC}/opencascade-6.8/inc)
+			LINK_DIRECTORIES(${TPDIST}/lib)
+			INCLUDE_DIRECTORIES(SYSTEM ${TPDIST}/include)
 		ENDIF()
 
 	ENDIF (THIRDPARTY_DOWNLOAD_OCC)

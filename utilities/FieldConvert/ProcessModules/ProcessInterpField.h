80077108ee842cf630dc09f11d92349c69380ab5
Kilian Lackhove <lackhove@ekt.tu-darmstadt.de>
2015-11-23 15:57:22 +0100

    FielConvert interpfield: port to Interpolator

--- utilities/FieldConvert/ProcessModules/ProcessInterpField.h
+++ utilities/FieldConvert/ProcessModules/ProcessInterpField.h
@@ -64,14 +64,6 @@ class ProcessInterpField : public ProcessModule
     private:
         FieldSharedPtr m_fromField;
 
-        void InterpolateField(vector<MultiRegions::ExpListSharedPtr> &field0,
-                              vector<MultiRegions::ExpListSharedPtr> &field1,
-                              Array<OneD, NekDouble>                  x,
-                              Array<OneD, NekDouble>                  y,
-                              Array<OneD, NekDouble>                  z,
-                              NekDouble                               clamp_low,
-                              NekDouble                               clamp_up,
-                              NekDouble                               def_value);
 };
 }
 }

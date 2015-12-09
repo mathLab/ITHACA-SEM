340d4c96574914d71c1cea9c5dbeba6293415993
David Moxey <d.moxey@imperial.ac.uk>
2015-12-09 12:01:16 +0000

    Update to use ideal -> element mapping (2D only)

--- utilities/FieldConvert/ProcessModules/ProcessQualityMetric.cpp
+++ utilities/FieldConvert/ProcessModules/ProcessQualityMetric.cpp
@@ -41,6 +41,7 @@ using namespace std;
 
 #include <LibUtilities/BasicUtils/SharedArray.hpp>
 #include <LibUtilities/BasicUtils/ParseUtils.hpp>
+#include <LibUtilities/Foundations/Interp.h>
 
 namespace Nektar
 {
@@ -70,8 +71,8 @@ void ProcessQualityMetric::Process(po::variables_map &vm)
         cout << "ProcessQualityMetric: Process Jacobian fld" << endl;
     }
 
-    Array<OneD, NekDouble> phys   = m_f->m_exp[0]->UpdatePhys();
-    Array<OneD, NekDouble> coeffs = m_f->m_exp[0]->UpdateCoeffs();
+    Array<OneD, NekDouble> &phys   = m_f->m_exp[0]->UpdatePhys();
+    Array<OneD, NekDouble> &coeffs = m_f->m_exp[0]->UpdateCoeffs();
 
     for(int i =0; i < m_f->m_exp[0]->GetExpSize(); ++i)
     {
@@ -79,9 +80,17 @@ void ProcessQualityMetric::Process(po::variables_map &vm)
         LocalRegions::ExpansionSharedPtr Elmt = m_f->m_exp[0]->GetExp(i);
         int offset = m_f->m_exp[0]->GetPhys_Offset(i);
         Array<OneD, NekDouble> q = GetQ(Elmt);
+        Array<OneD, NekDouble> out = phys + offset;
 
-        Vmath::Vcopy(q.num_elements(), &q[0], 1, &phys[offset], 1);
+        // Project onto output stuff
+        LibUtilities::PointsKeyVector pFrom = Elmt->GetGeom()->GetPointsKeys();
+        LibUtilities::PointsKeyVector pTo   = Elmt->GetPointsKeys();
+        LibUtilities::Interp2D(pFrom[0], pFrom[1], q, pTo[0], pTo[1], out);
+        
+        //Vmath::Vcopy(q.num_elements(), &q[0], 1, &phys[offset], 1);
     }
+
+    m_f->m_exp[0]->FwdTrans_IterPerExp(phys, coeffs);
 
     std::vector<LibUtilities::FieldDefinitionsSharedPtr> FieldDef
         = m_f->m_exp[0]->GetFieldDefinitions();
@@ -97,12 +106,71 @@ void ProcessQualityMetric::Process(po::variables_map &vm)
     m_f->m_data     = FieldData;
 }
 
+/*
+ * @brief Generate mapping between a simplex and the reference element.
+ *
+ * Mapping that requires the coordinates of the three vertices (x,y) of the
+ * triangular element and outputs the function coefficients that defines the
+ * mapping to the reference element for each coordinate (xfunc and yfunc)
+ *
+ * @param x   co-ordinates of triangle/tetrahedron
+ * @param xf  components of mapping
+ */
+inline DNekMat MappingIdealToRef(SpatialDomains::GeometrySharedPtr geom)
+{
+    int n = geom->GetNumVerts(), i, j;
+
+    DNekMat map   (n, n, 1.0, eFULL);
+    DNekMat mapref(n, n, 1.0, eFULL);
+
+    // Extract coordinate information.
+    for (i = 0; i < n; ++i)
+    {
+        for (j = 0; j < n-1; ++j)
+        {
+            map(j,i) = (*geom->GetVertex(i))[j];
+        }
+    }
+
+    // Set up reference triangle or tetrahedron mapping.
+    if (n == 3)
+    {
+        mapref(0,0) = -1.0; mapref(1,0) = -1.0;
+        mapref(0,1) =  1.0; mapref(1,1) = -1.0;
+        mapref(0,2) = -1.0; mapref(1,2) =  1.0;
+    }
+    else if (n == 4)
+    {
+        mapref(0,0) = -1.0; mapref(1,0) = -1.0; mapref(2,0) = -1.0;
+        mapref(0,1) =  1.0; mapref(1,1) = -1.0; mapref(2,1) = -1.0;
+        mapref(0,2) = -1.0; mapref(1,2) =  1.0; mapref(2,2) = -1.0;
+        mapref(0,3) = -1.0; mapref(1,3) = -1.0; mapref(2,3) =  1.0;
+    }
+
+    map.Invert();
+
+    DNekMat newmap = mapref * map;
+    DNekMat mapred(n-1, n-1, 1.0, eFULL);
+
+    for (i = 0; i < n-1; ++i)
+    {
+        for (j = 0; j < n-1; ++j)
+        {
+            mapred(i,j) = newmap(i,j);
+        }
+    }
+
+    return mapred;
+}
+
 Array<OneD, NekDouble> ProcessQualityMetric::GetQ(LocalRegions::ExpansionSharedPtr e)
 {
-    SpatialDomains::GeometrySharedPtr geom = e->GetGeom();
-    StdRegions::StdExpansionSharedPtr chi = e->GetGeom()->GetXmap();
-    LibUtilities::PointsKeyVector     p    = chi->GetPointsKeys();
+    SpatialDomains::GeometrySharedPtr    geom = e->GetGeom();
+    StdRegions::StdExpansionSharedPtr    chi  = e->GetGeom()->GetXmap();
+    LibUtilities::PointsKeyVector        p    = chi->GetPointsKeys();
     SpatialDomains::GeomFactorsSharedPtr gfac = geom->GetGeomFactors();
+
+    DNekMat i2rm = MappingIdealToRef(geom);
 
     SpatialDomains::DerivStorage deriv = gfac->GetDeriv(p);
 
@@ -146,18 +214,32 @@ Array<OneD, NekDouble> ProcessQualityMetric::GetQ(LocalRegions::ExpansionSharedP
 
     for (int k = 0; k < pts; ++k)
     {
+        DNekMat jac     (expDim, expDim, 0.0, eFULL);
+        DNekMat jacIdeal(expDim, expDim, 0.0, eFULL);
+
+        for (int i = 0; i < expDim; ++i)
+        {
+            for (int j = 0; j < expDim; ++j)
+            {
+                jac(j,i) = deriv[i][j][k];
+            }
+        }
+
+        jacIdeal = jac * i2rm;
+
+        NekDouble jacDet = jacIdeal(0,0) * jacIdeal(1,1) - jacIdeal(0,1)*jacIdeal(1,0);
         NekDouble frob = 0.0;
 
-        for (int i = 0; i < deriv.num_elements(); ++i)
+        for (int i = 0; i < expDim; ++i)
         {
-            for (int j = 0; j < deriv[i].num_elements(); ++j)
+            for (int j = 0; j < expDim; ++j)
             {
-                frob += deriv[i][j][k]*deriv[i][j][k];
+                frob += jacIdeal(i,j) * jacIdeal(i,j);
             }
         }
 
         NekDouble delta = 0.0; // fixme
-        NekDouble sigma = 0.5*(jac[k] + sqrt(jac[k]*jac[k] + 4*delta*delta));
+        NekDouble sigma = 0.5*(jacDet + sqrt(jacDet*jacDet + 4*delta*delta));
 
         eta[k] = expDim * pow(sigma,2.0/expDim) / frob;
         if(eta[k] > 1 || eta[k] < 0)

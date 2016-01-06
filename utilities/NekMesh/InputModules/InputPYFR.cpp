e6c1ea8e94286fafee198cb00bd66c431ca5e45a
Michael Turner <m.turner14@imperial.ac.uk>
2016-01-06 11:33:09 +0000

    basic quad read working for pyfr

--- utilities/NekMesh/InputModules/InputPYFR.cpp
+++ utilities/NekMesh/InputModules/InputPYFR.cpp
@@ -33,6 +33,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <boost/algorithm/string.hpp>
 
 #include <H5Cpp.h>
 #ifndef H5_NO_NAMESPACE
@@ -63,22 +64,129 @@ InputPYFR::InputPYFR(MeshSharedPtr m) : InputModule(m)
 
 InputPYFR::~InputPYFR()
 {
-    string filename = m_config["infile"].as<string>();
-
-    H5std_string FILE_NAME( filename );
-    H5File file( FILE_NAME, H5F_ACC_RDONLY );
-
-    Group g = file.openGroup("/");
-    for(int i = 0; i < g.getNumObjs(); i++)
-    {
-        cout << g.getObjnameByIdx(i) << endl;
-    }
 
 }
 
 void InputPYFR::Process()
 {
+    string filename = m_config["infile"].as<string>();
 
+    H5std_string FILE_NAME( filename );
+    H5File file( FILE_NAME, H5F_ACC_RDONLY );
+
+    vector<string> elements;
+    vector<string> bcs;
+    string con, uuid;
+
+    Group g = file.openGroup("/");
+    for(int i = 0; i < g.getNumObjs(); i++)
+    {
+        string obj = g.getObjnameByIdx(i);
+        string pre = obj.substr(0, obj.find("_"));
+
+        if(pre == "bcon")
+        {
+            bcs.push_back(obj);
+        }
+        else if(pre == "spt")
+        {
+            elements.push_back(obj);
+        }
+        else if(pre == "mesh")
+        {
+            uuid = obj;
+        }
+        else if(pre == "con")
+        {
+            con = obj;
+        }
+        else
+        {
+            ASSERTL0(false, "hdf5 object not recognised");
+        }
+    }
+
+    m_mesh->m_expDim = 2;
+    m_mesh->m_spaceDim = 2;
+    m_mesh->m_nummode = 2;
+
+    for(int i = 0; i < elements.size(); i++)
+    {
+        // all this is to just get the data out of hdf5, why so complicated!!!
+        DataSet d = g.openDataSet(elements[i]);
+        DataSpace s = d.getSpace();
+        int rank = s.getSimpleExtentNdims();
+        hsize_t* dims_out = new hsize_t[rank];
+        int ndims = s.getSimpleExtentDims( dims_out, NULL);
+        hsize_t* offset = new hsize_t[ndims];	// hyperslab offset in the file
+        memset(offset, 0, rank * sizeof(hsize_t)) ;
+        s.selectHyperslab( H5S_SELECT_SET, dims_out, offset);
+        DataSpace memspace( ndims, dims_out); //describe hyperslab in memory space
+        memspace.selectHyperslab( H5S_SELECT_SET, dims_out, offset);
+        hsize_t totSize = 1 ;
+        for(int j = 0 ; j < ndims ; j++)
+        {
+            totSize *= dims_out[j];
+        }
+        float* data = new float[totSize] ;
+        d.read( data, PredType::NATIVE_FLOAT, memspace, s );
+
+        Array<ThreeD, NekDouble> dataArray(dims_out[0],dims_out[1],dims_out[2]);
+        for(int j = 0; j < dims_out[0]; j++)
+        {
+            for(int k = 0; k < dims_out[1]; k++)
+            {
+                for(int l = 0; l < dims_out[2]; l++)
+                {
+                    dataArray[j][k][l] = data[j*dims_out[1]*dims_out[2] + k*dims_out[2] + l];
+                }
+            }
+        }
+
+        vector<string> tmp;
+        boost::split(tmp, elements[i], boost::is_any_of("_"));
+
+        LibUtilities::ShapeType st;
+        if(boost::iequals(tmp[1], "quad"))
+        {
+            st = LibUtilities::eQuadrilateral;
+        }
+        else
+        {
+            ASSERTL0(false,"element type not recognised");
+        }
+
+        map<int, int> nodemap; //nektar order to pyfr
+        if(st == LibUtilities::eQuadrilateral)
+        {
+            nodemap[0] = 0;
+            nodemap[1] = 1;
+            nodemap[2] = 3;
+            nodemap[3] = 2;
+        }
+
+        for(int j = 0; j < dims_out[1]; j++) //loop over all elements
+        {
+            vector<NodeSharedPtr> ns(4);
+            for(int k = 0; k < 4; k++)
+            {
+                ns[k] = boost::shared_ptr<Node>(new Node(0, dataArray[nodemap[k]][j][0],
+                                                dataArray[nodemap[k]][j][1], 0.0));
+            }
+            vector<int> tags;
+            tags.push_back(0);
+            ElmtConfig conf(LibUtilities::eQuadrilateral, 1, false, false);
+            ElementSharedPtr E = GetElementFactory().CreateInstance(
+                                    LibUtilities::eQuadrilateral, conf, ns, tags);
+            m_mesh->m_element[m_mesh->m_expDim].push_back(E);
+        }
+    }
+
+    ProcessVertices();
+    ProcessEdges();
+    ProcessFaces();
+    ProcessElements();
+    ProcessComposites();
 }
 
 }

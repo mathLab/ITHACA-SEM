a4b4e97c4012d5c049f90f7c3723509ea903c2a3
ssherw <s.sherwin@imperial.ac.uk>
2016-02-10 02:14:11 +0000

    Set defult SubStepCFL to 0.5 to be consistent with user-guide

--- solvers/IncNavierStokesSolver/EquationSystems/SubSteppingExtrapolate.cpp
+++ solvers/IncNavierStokesSolver/EquationSystems/SubSteppingExtrapolate.cpp
@@ -55,7 +55,7 @@ namespace Nektar
         : Extrapolate(pSession,pFields,pPressure,pVel,advObject)
     {
         m_session->LoadParameter("IO_InfoSteps", m_infosteps, 0);
-        m_session->LoadParameter("SubStepCFL", m_cflSafetyFactor, 0.6);
+        m_session->LoadParameter("SubStepCFL", m_cflSafetyFactor, 0.5);
         m_session->LoadParameter("MinSubSteps", m_minsubsteps,1);
         m_session->LoadParameter("MaxSubSteps", m_maxsubsteps,100);
 

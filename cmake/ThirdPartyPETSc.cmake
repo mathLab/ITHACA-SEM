d6f073f6bb879f9a2d3e3eaa37c9c5de9c1cb322
Kilian Lackhove <lackhove@ekt.tu-darmstadt.de>
2016-03-11 10:05:07 +0100

    PETSc: use cmake compilers for building PETSc with MPI

--- cmake/ThirdPartyPETSc.cmake
+++ cmake/ThirdPartyPETSc.cmake
@@ -49,7 +49,10 @@ IF (NEKTAR_USE_PETSC)
             BINARY_DIR ${TPBUILD}/petsc-3.5.2
             URL http://www.nektar.info/thirdparty/petsc-lite-3.5.2.tar.gz
             URL_MD5 "d707336a98d7cb31d843804d020edc94"
-            CONFIGURE_COMMAND ./configure
+            CONFIGURE_COMMAND
+                OMPI_CC=${CMAKE_C_COMPILER}
+                OMPI_CXX=${CMAKE_CXX_COMPILER}
+                ./configure
                 --with-cc=${PETSC_C_COMPILER}
                 --with-cxx=${PETSC_CXX_COMPILER}
                 --with-shared-libraries=0

aa5fba0235347eb67e5aab0856c0b488d970f5a3
Douglas Serson <d.serson@gmail.com>
2016-04-05 12:00:20 +0100

    Add documentation to ProcessCurve::v_GenerateEdgeNodes

--- utilities/NekMesh/ProcessModules/ProcessCurve.cpp
+++ utilities/NekMesh/ProcessModules/ProcessCurve.cpp
@@ -78,6 +78,29 @@ ProcessCurve::~ProcessCurve()
 {
 }
 
+/**
+ * This function generates curved edge information in 2D, by including
+ * equispaced nodes to an edge following the prescribed function \f$y = f(x)\f$.
+ *
+ * An approximately equispaced node distribution is obtained by initially
+ * considering a uniform distribution in the \f$ x\f$ direction, and then
+ * adjusting the interior nodes using the iteration
+ *  \f[ x^{n}_{i+1} = x^{n-1}_{i+1} + \Delta x^{n}_{i+1}   \f]
+ * \f[ \Delta x^{n}_{i+1} = \Delta x^{n}_{i} +
+ *    \gamma (\overline{\Delta s} - \Delta s^{n}_{i}) sign(\Delta x^{n}_{i}) \f]
+ *
+ * where \f$ x^{n}\f$ is the \f$ x\f$ coordinate of the \f$ n^{th}\f$ node,
+ *  \f$ i\f$ is the iteration counter, \f$ \Delta s^{n}\f$ is the Cartesian
+ * distance between nodes \f$ n\f$ and \f$ n-1\f$ and \f$ \overline{\Delta s}\f$
+ * is the average of these distances. The relaxation factor \f$ \gamma\f$ and
+ * the fixed number of iterations \f$ Niter\f$ are parameters of the module.
+ *
+ * In case the correction to \f$ \Delta x\f$ leads to an invalid distribution
+ * (i.e. reversing directions), the relaxation parameter is successively halved
+ * until it leads to a valid distribution.
+ *
+ * @param edge Edge which will be modified
+ */
 void ProcessCurve::v_GenerateEdgeNodes(EdgeSharedPtr edge)
 {
     NodeSharedPtr n1 = edge->m_n1;

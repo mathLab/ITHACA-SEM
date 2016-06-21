73e7275dfb29a0c98f73e37504c1b3f9e0549824
David Moxey <d.moxey@imperial.ac.uk>
2016-06-21 16:48:33 +0100

    Bump gsmpi to 1.2.1 to fix verbose issue

--- cmake/ThirdPartyMPI.cmake
+++ cmake/ThirdPartyMPI.cmake
@@ -51,14 +51,14 @@ IF( NEKTAR_USE_MPI )
 
     IF (THIRDPARTY_BUILD_GSMPI)
         EXTERNALPROJECT_ADD(
-            gsmpi-1.2
-            URL ${TPURL}/gsmpi-1.2.tar.bz2
-            URL_MD5 35901be16791bfdeafa9c4d0e06d189b
+            gsmpi-1.2.1
+            URL http://ae-nektar.ae.ic.ac.uk/~dmoxey/gsmpi-1.2.1.tar.bz2
+            URL_MD5 fcb0fa1cdd901d800fa1f602b5f8e7ad
             STAMP_DIR ${TPBUILD}/stamp
             DOWNLOAD_DIR ${TPSRC}
-            SOURCE_DIR ${TPSRC}/gsmpi-1.2
-            BINARY_DIR ${TPBUILD}/gsmpi-1.2
-            TMP_DIR ${TPBUILD}/gsmpi-1.2-tmp
+            SOURCE_DIR ${TPSRC}/gsmpi-1.2.1
+            BINARY_DIR ${TPBUILD}/gsmpi-1.2.1
+            TMP_DIR ${TPBUILD}/gsmpi-1.2.1-tmp
             INSTALL_DIR ${TPDIST}
             CONFIGURE_COMMAND 
                 ${CMAKE_COMMAND}
@@ -67,7 +67,7 @@ IF( NEKTAR_USE_MPI )
                 -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
                 -DCMAKE_BUILD_TYPE:STRING=Debug
                 -DCMAKE_INSTALL_PREFIX:PATH=${TPDIST}
-                ${TPSRC}/gsmpi-1.2
+                ${TPSRC}/gsmpi-1.2.1
         )
         SET(GSMPI_LIBRARY gsmpi CACHE FILEPATH
             "GSMPI path" FORCE)
@@ -78,7 +78,7 @@ IF( NEKTAR_USE_MPI )
         MESSAGE(STATUS "Build GSMPI: ${TPDIST}/lib/lib${GSMPI_LIBRARY}.a")
         MESSAGE(STATUS "Build XXT: ${TPDIST}/lib/lib${XXT_LIBRARY}.a")
     ELSE (THIRDPARTY_BUILD_GSMPI)
-        ADD_CUSTOM_TARGET(gsmpi-1.2 ALL)
+        ADD_CUSTOM_TARGET(gsmpi-1.2.1 ALL)
         INCLUDE (FindGSMPI)
         INCLUDE (FindXXT)
     ENDIF (THIRDPARTY_BUILD_GSMPI)

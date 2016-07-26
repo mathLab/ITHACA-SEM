09366d64a34d55cf59d3815a85941ea1f157493c
Michael Turner <m.turner14@imperial.ac.uk>
2016-07-26 14:30:25 +0100

    tidy code

--- utilities/NekMesh/OutputModules/OutputSTL.cpp
+++ utilities/NekMesh/OutputModules/OutputSTL.cpp
@@ -29,7 +29,7 @@
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 //  DEALINGS IN THE SOFTWARE.
 //
-//  Description: Gmsh file format output.
+//  Description: STL surface writer.
 //
 ////////////////////////////////////////////////////////////////////////////////
 

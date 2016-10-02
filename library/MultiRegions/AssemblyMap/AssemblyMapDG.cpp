85d1fd070e2f88edad68b61b10a9b526fb35ce7d
Douglas Serson <d.serson@gmail.com>
2016-10-02 13:46:31 -0300

    Fix bug in DG with hybrid meshes

--- library/MultiRegions/AssemblyMap/AssemblyMapDG.cpp
+++ library/MultiRegions/AssemblyMap/AssemblyMapDG.cpp
@@ -615,14 +615,14 @@ namespace Nektar
                             m_elmtToTrace[eid][j]->as<LocalRegions::SegExp>();
 
                         id  = locSegExp->GetGeom1D()->GetEid();
-                        order_e = locExpVector[eid]->GetEdgeNcoeffs(j);
+                        order_e = locExpansion->GetEdgeNcoeffs(j);
 
                         map<int,int> orientMap;
                         Array<OneD, unsigned int> map1(order_e), map2(order_e);
                         Array<OneD, int> sign1(order_e), sign2(order_e);
 
-                        locExpVector[eid]->GetEdgeToElementMap(j, StdRegions::eForwards, map1, sign1);
-                        locExpVector[eid]->GetEdgeToElementMap(j, locExpVector[eid]->GetEorient(j), map2, sign2);
+                        locExpansion->GetEdgeToElementMap(j, StdRegions::eForwards, map1, sign1);
+                        locExpansion->GetEdgeToElementMap(j, locExpansion->GetEorient(j), map2, sign2);
 
                         for (k = 0; k < map1.num_elements(); ++k)
                         {
@@ -660,14 +660,14 @@ namespace Nektar
                                            ->as<LocalRegions::Expansion2D>();
 
                         id  = locFaceExp->GetGeom2D()->GetFid();
-                        order_e = locExpVector[eid]->GetFaceNcoeffs(j);
+                        order_e = locExpansion->GetFaceNcoeffs(j);
 
                         map<int,int> orientMap;
                         Array<OneD, unsigned int> map1(order_e), map2(order_e);
                         Array<OneD, int> sign1(order_e), sign2(order_e);
 
-                        locExpVector[eid]->GetFaceToElementMap(j, StdRegions::eDir1FwdDir1_Dir2FwdDir2, map1, sign1);
-                        locExpVector[eid]->GetFaceToElementMap(j, locExpVector[eid]->GetForient(j), map2, sign2);
+                        locExpansion->GetFaceToElementMap(j, StdRegions::eDir1FwdDir1_Dir2FwdDir2, map1, sign1);
+                        locExpansion->GetFaceToElementMap(j, locExpansion->GetForient(j), map2, sign2);
 
                         for (k = 0; k < map1.num_elements(); ++k)
                         {

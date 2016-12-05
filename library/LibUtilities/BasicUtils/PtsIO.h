19b85a78a30053e230d92c689befccc032c2e85e
Kilian Lackhove <lackhove@ekt.tu-darmstadt.de>
2016-12-05 12:40:59 +0100

    removed unused PtsIO::Write and Import

--- library/LibUtilities/BasicUtils/PtsIO.h
+++ library/LibUtilities/BasicUtils/PtsIO.h
@@ -61,12 +61,6 @@ using namespace std;
 typedef std::map<std::string, std::string> PtsMetaDataMap;
 static PtsMetaDataMap NullPtsMetaDataMap;
 
-LIB_UTILITIES_EXPORT void Import(const string &inFile,
-                                 PtsFieldSharedPtr &ptsField);
-
-LIB_UTILITIES_EXPORT void Write(const string &outFile,
-                                const PtsFieldSharedPtr &ptsField);
-
 class PtsIO : public FieldIOXml
 {
 public:

d224427735402083a2c5c6a47b26282aa5336137
Michael Turner <mturner4350@gmail.com>
2017-02-20 10:17:57 +0000

    fix

--- library/SpatialDomains/TriGeom.cpp
+++ library/SpatialDomains/TriGeom.cpp
@@ -275,7 +275,7 @@ namespace Nektar
                     x1 = (*face2[j])(0)-cx;
                     y1 = (*face2[j])(1)-cy;
                     z1 = (*face2[j])(2)-cz;
-                    if (sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y)+(z1-z)*(z1-z)) < LibUtilities::kGeomFactorsTol)
+                    if (sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y)+(z1-z)*(z1-z)) < NekConstants::kGeomFactorsTol)
                     {
                         vmap[j] = i;
                         break;

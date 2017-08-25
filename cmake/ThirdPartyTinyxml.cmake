53e9530d0a1eea60a908f18eac8586178794a0c1
Michael Turner <mturner4350@gmail.com>
2017-08-25 09:57:09 +0100

    swap to patch method

--- cmake/ThirdPartyTinyxml.cmake
+++ cmake/ThirdPartyTinyxml.cmake
@@ -20,7 +20,7 @@ ELSE()
     SET(BUILD_TINYXML ON)
 ENDIF ()
 
-OPTION(THIRDPARTY_BUILD_TINYXML 
+OPTION(THIRDPARTY_BUILD_TINYXML
     "Build TinyXML library from ThirdParty." ${BUILD_TINYXML})
 
 IF (THIRDPARTY_BUILD_TINYXML)
@@ -28,8 +28,8 @@ IF (THIRDPARTY_BUILD_TINYXML)
     EXTERNALPROJECT_ADD(
         tinyxml-2.6.2
         PREFIX ${TPSRC}
-        URL ${TPURL}/tinyxml_2_6_2-1.tar.bz2
-        URL_MD5 6dee12297219626f4e0d4b713f364bce
+        URL ${TPURL}/tinyxml_2_6_2.tar.bz2
+        URL_MD5 240beaeb45f63b154c9801eef7561eac
         STAMP_DIR ${TPBUILD}/stamp
         DOWNLOAD_DIR ${TPSRC}
         SOURCE_DIR ${TPSRC}/tinyxml-2.6.2
@@ -44,6 +44,15 @@ IF (THIRDPARTY_BUILD_TINYXML)
             -DCMAKE_CXX_FLAGS:STRING=-DTIXML_USE_STL
             ${TPSRC}/tinyxml-2.6.2
         )
+
+    ExternalProject_Add_Step(tinyxml-2.6.2 patch-script
+      COMMAND patch tinyxmlparser.cpp -i ${CMAKE_SOURCE_DIR}/cmake/scripts/tinyxml.patch
+      COMMENT "patching"
+      DEPENDEES mkdir update patch download
+      DEPENDERS configure
+      WORKING_DIRECTORY ${TPSRC}/tinyxml-2.6.2
+      )
+
 
     THIRDPARTY_LIBRARY(TINYXML_LIBRARY STATIC tinyxml
         DESCRIPTION "TinyXML library")

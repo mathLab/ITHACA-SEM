01899b0cda6cb902e33d0b994c2af246bdbecb5e
Douglas Serson <d.serson@gmail.com>
2017-11-03 19:28:05 -0200

    Project geometric factor of ForcingQuasi1D

--- solvers/CompressibleFlowSolver/Forcing/ForcingQuasi1D.cpp
+++ solvers/CompressibleFlowSolver/Forcing/ForcingQuasi1D.cpp
@@ -107,6 +107,11 @@ void ForcingQuasi1D::v_InitObject(
                                             m_geomFactor, 1);
     Vmath::Neg(pFields[0]->GetTotPoints(), m_geomFactor, 1);
 
+    // Project m_geomFactor to solution space
+    Array<OneD, NekDouble> tmpCoeff (pFields[0]->GetNcoeffs(), 0.0);
+    pFields[0]->FwdTrans_IterPerExp(m_geomFactor, tmpCoeff);
+    pFields[0]->BwdTrans(tmpCoeff, m_geomFactor);
+
     m_Forcing = Array<OneD, Array<OneD, NekDouble> > (m_NumVariable);
     for (int i = 0; i < m_NumVariable; ++i)
     {

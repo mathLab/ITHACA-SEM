eeccccb03bacdd2db409f3e87005285f5dcd36e4
Kurt Sansom <kayarre@gmail.com>
2018-03-05 15:23:01 -0800

    modify vtkExtras to account for multiple versions

--- utilities/Extras/VtkToPng.cpp
+++ utilities/Extras/VtkToPng.cpp
@@ -103,8 +103,9 @@ int main(int argc, char * argv[])
 #else
     mapper->SetInputData(data);
 #endif
-    
-#if (VTK_MAJOR_VERSION <= 8 && VTK_MINOR_VERSION <= 1) //deprecated as of vtk 8.1
+
+#if VTK_MAJOR_VERSION < 8 || (VTK_MAJOR_VERSION == 8 && VTK_MINOR_VERSION <= 1)
+    //deprecated as of vtk 8.1
     mapper->ImmediateModeRenderingOn();
 #endif
     
@@ -144,7 +145,9 @@ int main(int argc, char * argv[])
     vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter =
             vtkSmartPointer<vtkWindowToImageFilter>::New();
     windowToImageFilter->SetInput(renderWindow);
-#if (VTK_MAJOR_VERSION <= 8 && VTK_MINOR_VERSION <= 1) //deprecated as of vtk 8.1
+
+#if VTK_MAJOR_VERSION < 8 || (VTK_MAJOR_VERSION == 8 && VTK_MINOR_VERSION <= 1)
+    //deprecated as of vtk 8.1
     windowToImageFilter->SetMagnification(4);
 #else
     windowToImageFilter->SetScale(4, 4);

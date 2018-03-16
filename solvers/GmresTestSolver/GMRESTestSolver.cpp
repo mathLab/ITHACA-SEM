2a52313d6dc36221b6b9dfd64cdc37bd5c4fbcfe
z.yan <z.yan17@imperial.ac.uk>
2018-03-16 21:28:31 +0000

    gmres_direct inverse ok

--- solvers/GmresTestSolver/GMRESTestSolver.cpp
+++ solvers/GmresTestSolver/GMRESTestSolver.cpp
@@ -32,52 +32,461 @@
 // Description: Advection Diffusion Reaction framework solver
 //
 ///////////////////////////////////////////////////////////////////////////////
+#include <LibUtilities/BasicUtils/VmathArray.hpp> 
+//#include <MultiRegions/AssemblyMap/AssemblyMap.h>
 
-#include <LibUtilities/BasicUtils/SessionReader.h>
-#include <MultiRegions/GlobalLinSysIterativeSolver.h>
-// #include <MultiRegions/AssemblyMap/AssemblyMap.h>
+
+#define GMRES_DEBUG
 
 using namespace std;
 using namespace Nektar;
+//using namespace MultiRegions;
+
+#ifdef GMRES_DEBUG        
+            
+            NekDouble       m_rhs_magnitude;
+            NekDouble       m_tolerance; 
+            bool            m_converged;
+            bool            m_verbose=true;
+            bool            m_root=true;       
+            unsigned int    m_maxrestart;
+            unsigned int    m_totalIterations;
+            unsigned int    m_maxdirction;
+
+            // stores the dimension of the linear sysytem
+            unsigned int    m_nlinsys;
+            
+            // stores the A of the linear system Ax = f
+            Array<OneD, NekDouble> m_mat;
+
+            // stores the f of the linear system Ax = f
+            Array<OneD,       NekDouble> m_rhs;
+
+#endif
+
+        void v_DoMatrixMultiply(
+                const Array<OneD, NekDouble>& pInput,
+                      Array<OneD, NekDouble>& pOutput)
+        {
+            //ASSERTL1(m_nlinsys > pInput.num_elements()+pInput.GetOffset(),"Array out of bounds");
+            //ASSERTL1(m_nlinsys > pInput.num_elements()+pInput.GetOffset(),"Array out of bounds");
+            for(int i=0;i<m_nlinsys;++i)
+            {
+                pOutput[i]  =   Vmath::Dot(m_nlinsys,&m_mat[i*m_nlinsys],&pInput[0]);
+                //pOutput[i]  =   sqrt(pOutput[i]);
+            }
+            return;
+        }
+
+
+        // void initializeLinSys(const LibUtilities::SessionReaderSharedPtr &session)
+        void initializeLinSys()
+        {
+            
+            // m_nlinsys                   = session->GetParameter("LinSysDimens");
+            // m_maxdirction               = session->GetParameter("MaxDirction" );
+            // m_maxrestart                = session->GetParameter("MaxRestart" );
+            // m_tolerance                 = session->GetParameter("Tolerance" );
+
+            m_nlinsys                   = 4;
+            m_maxdirction               = 4;
+            m_maxrestart                = 1;
+            m_tolerance                 = 1.0e-10;
+            
+            m_rhs       =  Array<OneD, NekDouble>(m_nlinsys);
+            m_mat       =  Array<OneD, NekDouble>(m_nlinsys*m_nlinsys);
+            Vmath::Fill(m_nlinsys,1.0,&m_rhs[0],1);
+
+            // initial the matrix(A) in Ax = f
+            for(int i=0; i<m_nlinsys*m_nlinsys; ++i)
+            {
+                m_mat[i]    =   1.0+i;
+            }
+            for(int i=0; i<m_nlinsys; ++i)
+            {
+                m_mat[i*m_nlinsys+i]    =   m_mat[i*m_nlinsys+i]*10.0;
+            }
+            NekDouble tmp;
+            for(int i=0; i<m_nlinsys; ++i)
+            {
+                cout <<"i="<<i<<"   :";
+                for(int j=0; j<m_nlinsys; ++j)
+                {
+                    tmp =   m_mat[i*m_nlinsys+j];
+                    cout<< tmp<<"    ";
+                }
+                cout <<endl;
+                
+            }
+            return;
+        }
+
+
+
+
+        /**  
+         * Solve a global linear system(Ax=f, r =f-Ax) using the conjugate gradient method.  
+         * We solve only for the non-Dirichlet modes. The operator is evaluated  
+         * using an auxiliary function v_DoMatrixMultiply defined by the  
+         * specific solver. Distributed math routines are used to support  
+         * parallel execution of the solver.  
+         *  
+         * The implemented algorithm uses a reduced-communication reordering of  
+         * the standard PCG method (Demmel, Heath and Vorst, 1993)  
+         *  
+         * @param       pInput      Input residual(f)  of all DOFs.  
+         * @param       pOutput     Solution vector(x) of all DOFs.  
+         */
+        NekDouble DoGmresRestart(
+            const bool                         rested,
+            const int                          nGlobal,
+            const Array<OneD,const NekDouble> &pInput,
+                  Array<OneD,      NekDouble> &pOutput,
+            const int                          nDir)
+        {
+
+#ifndef GMRES_DEBUG        
+            // Get the communicator for performing data exchanges
+            LibUtilities::CommSharedPtr vComm
+                = m_expList.lock()->GetComm()->GetRowComm();
+#endif
+            int nNonDir = nGlobal - nDir;
+
+            Array<TwoD, NekDouble> han    (m_maxdirction+1,m_maxdirction, 0.0);
+            Array<OneD, NekDouble> eta    (m_maxdirction+1, 0.0);
+            Array<OneD, NekDouble> cs     (m_maxdirction+1, 0.0);
+            Array<OneD, NekDouble> sn     (m_maxdirction+1, 0.0);
+            Array<OneD, NekDouble> yk     (m_maxdirction+1, 0.0);
+            Array<OneD, NekDouble> tmp0;
+            Array<OneD, NekDouble> tmp1;
+
+            // Allocate array storage
+            int nqk =   (m_maxdirction+1)*nGlobal;
+            int iqk0 =0;
+            int iqk1 =0;
+            Array<OneD, NekDouble> qk_a   (nqk, 0.0);
+
+
+            NekDouble alpha, beta, eps, dd, hh,temp_dbl;
+            //Array<OneD, NekDouble> vExchange(3,0.0);
+            NekDouble   vExchange=0.0;
+            for(int nd=0;nd<m_maxdirction+1;++nd)
+            {
+                iqk0 = nd*nGlobal;
+                Vmath::Zero(nGlobal,&qk_a[iqk0],1);
+                //Vmath::Vcopy(nDir,pOutput,1,qk_a[nd],1);
+            }            
+            
+            if(rested)
+            {
+                // qk_a[0] = A*x0
+                iqk0 = 0;
+                tmp0 = qk_a+iqk0;
+                v_DoMatrixMultiply(pOutput, tmp0);
+                //v_DoMatrixMultiply(pOutput+nDir, pOutput);
+            }
+
+            beta = -1.0;
+            // q_k[0] = f-A*x0
+            iqk0 = 0;
+            Vmath::Svtvp(nNonDir, beta, &qk_a[iqk0]+nDir, 1, &pInput[0]+nDir, 1, &qk_a[iqk0]+nDir, 1);
+
+#ifndef GMRES_DEBUG        
+            // evaluate initial residual error for exit check
+            vExchange    = Vmath::Dot2(nNonDir,
+                                       &qk_a[iqk0]+nDir,
+                                       &qk_a[iqk0]+nDir,
+                                       &m_map[0] + nDir);
+            vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+#else
+            // evaluate initial residual error for exit check
+            vExchange    = Vmath::Dot(nNonDir,
+                                       &qk_a[iqk0]+nDir,
+                                       &qk_a[iqk0]+nDir);
+#endif            
+            eps          = vExchange;
+            
+            
+            // If input residual is less than tolerance skip solve.
+            if (eps < m_tolerance * m_tolerance * m_rhs_magnitude)
+            {
+                m_converged = true;
+                return eps;
+            }
+
+            eta[0]       = sqrt(eps);
+            alpha        = 1.0/eta[0] ;
+            iqk0 = 0;
+            Vmath::Smul(nNonDir,alpha,&qk_a[iqk0]+nDir,1,&qk_a[iqk0]+nDir,1);
+
+
+            int nswp = 0;
+            cout <<"(m_maxdirction-1)="<< (m_maxdirction-1)<<endl;
+            cout <<endl;
+            for(int nd=0; nd<(m_maxdirction); ++nd)
+            {
+                iqk0 = nd*nGlobal;
+                iqk1 = (nd+1)*nGlobal;
+                
+                //tmp1 = qk_a[nd+1][0];
+                //m_precon->DoPreconditioner(r_A, tmp = w_A + nDir);
+                tmp0 = qk_a+iqk0;
+                tmp1 = qk_a+iqk1;
+                v_DoMatrixMultiply(tmp0, tmp1);
+                
+#ifdef GMRES_DEBUG        
+                cout<<"debug output"<<endl;
+                for(int jj=0;jj<m_maxdirction; ++jj)
+                {
+                    cout << tmp1[jj]<<endl;
+                }
+#endif            
+
+                for(int i=0;i<nd+1;++i)
+                {
+                    // evaluate initial residual error for exit check
+                    // tmp0 = qk_a[nDir];
+                    int iqki = i*nGlobal;
+#ifndef GMRES_DEBUG        
+                    vExchange    = Vmath::Dot2(nNonDir,
+                                               &qk_a[iqki]+nDir,
+                                               &qk_a[iqk1]+nDir,
+                                               &m_map[0] + nDir);
+                    vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+#else
+                    // evaluate initial residual error for exit check
+                    vExchange    = Vmath::Dot(nNonDir,
+                                               &qk_a[iqki]+nDir,
+                                               &qk_a[iqk1]+nDir);
+#endif 
+                    han[i][nd] = vExchange;
+                    // q_k[0] = f-A*x0
+                    beta = -1.0*vExchange;
+                    Vmath::Svtvp(nNonDir, beta, &qk_a[iqki]+nDir, 1, &qk_a[iqk1]+nDir, 1, &qk_a[iqk1]+nDir, 1);
+                }
+#ifndef GMRES_DEBUG        
+                vExchange    = Vmath::Dot2(nNonDir,
+                                           &qk_a[iqk1]+nDir,
+                                           &qk_a[iqk1]+nDir,
+                                           &m_map[0] + nDir);
+#else
+                cout<<"debug output"<<endl;
+                for(int jj=0;jj<m_maxdirction; ++jj)
+                {
+                    cout << qk_a[iqk1+jj]<<endl;
+                }
+                
+                // evaluate initial residual error for exit check
+                vExchange    = Vmath::Dot(nNonDir,
+                                           &qk_a[iqk1]+nDir,
+                                           &qk_a[iqk1]+nDir);
+#endif 
+#ifndef GMRES_DEBUG        
+                vComm->AllReduce(vExchange, Nektar::LibUtilities::ReduceSum);
+#endif
+                han[nd+1][nd] = sqrt(vExchange);
+                // q_k[0] = f-A*x0
+                alpha        = 1.0/han[nd+1][nd] ;
+                Vmath::Smul(nNonDir,alpha,&qk_a[iqk1]+nDir,1,&qk_a[iqk1]+nDir,1);
+
+                for(int i=0;i<nd;++i)
+                {
+                    temp_dbl        = cs[i]*han[i][nd] - sn[i]*han[i+1][nd];
+                    han[i+1][nd]    = sn[i]*han[i][nd] + cs[i]*han[i+1][nd];
+                    han[i][nd]      = temp_dbl;
+                }
+                dd = han[nd][nd];
+                hh = han[nd+1][nd];
+                if(hh==0.0)
+                {
+                    cs[nd] = 1.0;
+                    sn[nd] = 0.0;
+                }
+                else if (abs(hh) > abs(dd))
+                {
+                    temp_dbl = -dd/hh;
+                    sn[nd] = 1.0 / sqrt(1.0 + temp_dbl*temp_dbl);
+                    cs[nd] = temp_dbl * sn[nd];
+                }
+                else
+                {
+                    temp_dbl = -hh/dd;
+                    cs[nd] = 1.0 / sqrt(1.0 + temp_dbl*temp_dbl);
+                    sn[nd] = temp_dbl * cs[nd];
+                }
+
+                temp_dbl = sn[nd]*han[nd][nd]+cs[nd]*han[nd+1][nd];
+
+                han[nd][nd] = cs[nd]*han[nd][nd]-sn[nd]*han[nd+1][nd];
+                han[nd+1][nd] = 0.0;
+
+                dd = cs[nd]*eta[nd] ;
+                //eta[nd+1] = -sn[nd]*eta[nd] ;
+
+                temp_dbl        = cs[nd]*eta[nd] - sn[nd]*eta[nd+1];
+                eta[nd+1]       = sn[nd]*eta[nd] + cs[nd]*eta[nd+1];
+                eta[nd]         = temp_dbl;
+
+
+
+                eps          = eta[nd+1]*eta[nd+1];
+
+                nswp++;
+                m_totalIterations++;
+
+                // If input residual is less than tolerance skip solve.
+                if (eps < m_tolerance * m_tolerance * m_rhs_magnitude)
+                {
+                    m_converged = true;
+                    break;
+                }
+            }
+
+            for(int i=0;i<nswp;++i)
+            {
+                yk[i] = eta[i];
+            }
+
+            for(int i=nswp-1;i>-1;--i)
+            {
+                cout <<"yk"<<i<<"="<<yk[i]<<endl;
+                cout <<"han"<<i<<"="<<han[i][i]<<endl;
+                yk[i] = yk[i]/han[i][i];
+                cout <<"yk"<<i<<"="<<yk[i]<<endl;
+                for(int j=0;j<i;++j)
+                {
+                    yk[j] = yk[j]-han[j][i]*yk[i];
+                }
+                cout <<"yk"<<i<<"="<<yk[i]<<endl;
+            }
+
+
+            //tmp1 = qk_a[nDir][0];
+            for(int i=0;i<nswp;++i)
+            {
+                // q_k[0] = f-A*x0
+                beta = yk[i];
+                int iqki = i*nGlobal;
+                //tmp0 = qk_a[nDir][0];
+                Vmath::Svtvp(nNonDir, beta, &qk_a[iqki]+nDir, 1, &pOutput[0]+nDir, 1, &pOutput[0]+nDir, 1);
+            }
+            return eps;
+            
+        }
+
+
+        void DoGMRES(
+            const int                          nGlobal,
+            const Array<OneD,const NekDouble> &pInput,
+                  Array<OneD,      NekDouble> &pOutput,
+            const int                          nDir)
+        {
+#ifndef GMRES_DEBUG        
+            if (!m_precon)
+            {
+                v_UniqueMap();
+                m_precon = CreatePrecon(plocToGloMap);
+                m_precon->BuildPreconditioner();
+            }
+#endif
+
+            /*
+            // Get the communicator for performing data exchanges
+            LibUtilities::CommSharedPtr vComm
+                = m_expList.lock()->GetComm()->GetRowComm();
+            */
+            
+            // Get vector sizes
+            NekDouble eps = 0.0;
+            int nNonDir = nGlobal - nDir;
+
+            // zero homogeneous out array ready for solution updates
+            // Should not be earlier in case input vector is same as
+            // output and above copy has been peformed
+            Vmath::Zero(nNonDir, &pOutput[nDir],1);
+            //Vmath::Zero(nGlobal, &qk_a[iqk0],1);
+
+
+#ifndef GMRES_DEBUG        
+            if(m_rhs_magnitude == NekConstants::kNekUnsetDouble)
+            {
+                NekVector<NekDouble> inGlob (nGlobal, pInput, eWrapper);
+                Set_Rhs_Magnitude(inGlob);
+            }
+#else
+            m_rhs_magnitude = 1.0;
+#endif
+
+            m_totalIterations = 0;
+            m_converged       = false;
+            
+            bool restarted = false;
+            for(int nrestart=0;nrestart<m_maxrestart;++nrestart)
+            {
+                eps = DoGmresRestart(restarted, nNonDir,pInput,pOutput,nDir);
+   
+                if(m_converged)
+                {
+                    if (m_verbose && m_root)
+                    {
+                        cout << "GMRES iterations made = " << m_totalIterations 
+                             << " using tolerance of "  << m_tolerance 
+                             << " (error = " << sqrt(eps/m_rhs_magnitude) 
+                             << ", rhs_mag = " << sqrt(m_rhs_magnitude) <<  ")" 
+                             << endl;
+                    }
+                    return;
+                }
+                restarted = true;
+            }
+            
+            if(m_root)
+            {
+                cout << "GMRES iterations made = " << m_totalIterations 
+                     << " using tolerance of "  << m_tolerance 
+                     << " (error = " << sqrt(eps/m_rhs_magnitude)
+                     << ", rhs_mag = " << sqrt(m_rhs_magnitude) <<  ")"
+                     << endl;
+            }
+            ROOTONLY_NEKERROR(ErrorUtil::efatal,
+                              "Exceeded maximum number of iterations");
+
+        }
+
 
 int main(int argc, char *argv[])
 {
-    LibUtilities::SessionReaderSharedPtr session;
-    //LibUtilities::FieldIOSharedPtr       fld;
-    //SpatialDomains::MeshGraphSharedPtr   graph;
-    //MultiRegions::ContField2DSharedPtr   field;
-    //LibUtilities::EquationSharedPtr      icond, ex_sol;
-    //StdRegions::ConstFactorMap           factors;
-
-    MultiRegions::GlobalLinSysKey pKey(StdRegions::MatrixType::eMass);
-    std::weak_ptr<MultiRegions::ExpList> pExp;
+    // LibUtilities::SessionReaderSharedPtr pppsession;
     
-    MultiRegions::AssemblyMapSharedPtr NoUsePtr;
-
-    // /media/zy3416/DATADRIVE1/development/nektar++/nektar++/build/solvers/DiffusionSolver/DiffusionSolver-g
-
-    try
-    {
         // Create session reader.
-        session = LibUtilities::SessionReader::CreateInstance(argc, argv);
+        // pppsession = LibUtilities::SessionReader::CreateInstance(argc, argv);
 
-        // Get some information about the session
-        string       sessionName    = session->GetSessionName();
-        string       outFile        = sessionName + ".fld";
+        // // Get some information about the session
+        // string       sessionName    = pppsession->GetSessionName();
+        // string       outFile        = sessionName + ".fld";
         
-        MultiRegions::GlobalLinSysIterativeSolver LinSolver(pKey,pExp,NoUsePtr);
 
-        LinSolver.initializeLinSys(session);
+        cout <<"test output"<<endl;
 
-        int ndim = LinSolver.getdimension();
+        initializeLinSys();
+
+        int ndim = m_nlinsys;
 
         Array<OneD, NekDouble> soltn_cg(ndim);
 
-        LinSolver.DoConjugateGradient(ndim, LinSolver.getrhs(), soltn_cg, NoUsePtr, 0);
+        //DoConjugateGradient(ndim, , soltn_cg, NoUsePtr, 0);
 
         Array<OneD, NekDouble> soltn_gmres(ndim);
 
-        LinSolver.DoConjugateGradient(ndim, LinSolver.getrhs(), soltn_gmres, NoUsePtr, 0);
+        DoGMRES(ndim, m_rhs, soltn_gmres, 0);
+
+#ifdef GMRES_DEBUG        
+        cout<<"debug soltn_gmres"<<endl;
+        for(int jj=0;jj<m_maxdirction; ++jj)
+        {
+            cout << soltn_gmres[jj]<<endl;
+        }
+#endif   
 
         NekDouble l2error=0.0;
         NekDouble tmp;
@@ -89,23 +498,9 @@ int main(int argc, char *argv[])
 
         cout << "The L2 difference between CG&GMRES is"<<l2error<<endl;
 
-
-
-        
-
-
-
         // Finalise session
-        session->Finalise();
-    }
-    catch (const std::runtime_error& e)
-    {
-        return 1;
-    }
-    catch (const std::string& eStr)
-    {
-        cout << "Error: " << eStr << endl;
-    }
+        // pppsession->Finalise();
+
 
     return 0;
 }

f01adf46982cb7ed23fa76964d8670993441d174
Emilia Juda <eaj14@ic.ac.uk>
2018-05-01 20:03:23 +0100

    script small update

--- library/Demos/Python/MemoryTest.py
+++ library/Demos/Python/MemoryTest.py
@@ -5,6 +5,7 @@ import sys
 
 session = SessionReader.CreateInstance(sys.argv)
 graph = MeshGraph.Read(session)
+exp = ExpList2D(session, graph)
 
 print("Loaded session: %s" % session.GetSessionName())
 print("Loaded MeshGraph of dimension: %d" % graph.GetMeshDimension())

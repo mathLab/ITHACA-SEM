da9276f09217d532f07959b390ddad7900cd46f5
Spencer Sherwin <ssherw@clapham.ae.ic.ac.uk>
2019-01-24 22:38:42 +0000

    updated last ExpList2D reference and python test now passing

--- library/MultiRegions/Python/MultiRegions.cpp
+++ library/MultiRegions/Python/MultiRegions.cpp
@@ -36,12 +36,10 @@
 #include <LibUtilities/Python/NekPyConfig.hpp>
 
 void export_ExpList();
-void export_ExpList2D();
 
 BOOST_PYTHON_MODULE(_MultiRegions)
 {
     np::initialize();
 
     export_ExpList();
-    export_ExpList2D();
 }

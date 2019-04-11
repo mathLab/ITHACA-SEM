0662be01509b6d1350bc1aa52f45612a9bc24355
Giacomo Castiglioni <giacastiglioni@gmail.com>
2019-04-11 14:17:37 +0100

    Removed namespace indentation, added 2 regression tests with minimal mesh to cover all possible face orientations

--- library/UnitTests/LocalRegions/TestReOrientFacePhysMap.cpp
+++ library/UnitTests/LocalRegions/TestReOrientFacePhysMap.cpp
@@ -36,118 +36,119 @@
 
 namespace Nektar
 {
-    namespace Expansion3DTests
-    {
-        BOOST_AUTO_TEST_CASE(TestReOrientQuadFacePhysMap)
-        {
-            // Dummy 3D expansion
-            LocalRegions::Expansion3DSharedPtr exp3d;
+namespace Expansion3DTests
+{
 
-            // Initialiaze 3*3 face id map for quad
-            int nq0 = 3;
-            int nq1 = 3;
-            int nvert = 4;
-            Array<OneD,int> idmap(nq0*nq1, -1);
+BOOST_AUTO_TEST_CASE(TestReOrientQuadFacePhysMap)
+{
+    // Dummy 3D expansion
+    LocalRegions::Expansion3DSharedPtr exp3d;
 
-            // Test different orientations
-            StdRegions::Orientation orient;
+    // Initialiaze 3*3 face id map for quad
+    int nq0 = 3;
+    int nq1 = 3;
+    int nvert = 4;
+    Array<OneD,int> idmap(nq0*nq1, -1);
 
-            orient = StdRegions::eDir1FwdDir1_Dir2FwdDir2;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 0);
-            BOOST_CHECK_EQUAL(idmap[1], 1);
-            BOOST_CHECK_EQUAL(idmap[2], 2);
-            BOOST_CHECK_EQUAL(idmap[3], 3);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 5);
-            BOOST_CHECK_EQUAL(idmap[6], 6);
-            BOOST_CHECK_EQUAL(idmap[7], 7);
-            BOOST_CHECK_EQUAL(idmap[8], 8);
+    // Test different orientations
+    StdRegions::Orientation orient;
 
-            orient = StdRegions::eDir1FwdDir1_Dir2BwdDir2;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 6);
-            BOOST_CHECK_EQUAL(idmap[1], 7);
-            BOOST_CHECK_EQUAL(idmap[2], 8);
-            BOOST_CHECK_EQUAL(idmap[3], 3);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 5);
-            BOOST_CHECK_EQUAL(idmap[6], 0);
-            BOOST_CHECK_EQUAL(idmap[7], 1);
-            BOOST_CHECK_EQUAL(idmap[8], 2);
+    orient = StdRegions::eDir1FwdDir1_Dir2FwdDir2;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 0);
+    BOOST_CHECK_EQUAL(idmap[1], 1);
+    BOOST_CHECK_EQUAL(idmap[2], 2);
+    BOOST_CHECK_EQUAL(idmap[3], 3);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 5);
+    BOOST_CHECK_EQUAL(idmap[6], 6);
+    BOOST_CHECK_EQUAL(idmap[7], 7);
+    BOOST_CHECK_EQUAL(idmap[8], 8);
 
-            orient = StdRegions::eDir1BwdDir1_Dir2FwdDir2;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 2);
-            BOOST_CHECK_EQUAL(idmap[1], 1);
-            BOOST_CHECK_EQUAL(idmap[2], 0);
-            BOOST_CHECK_EQUAL(idmap[3], 5);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 3);
-            BOOST_CHECK_EQUAL(idmap[6], 8);
-            BOOST_CHECK_EQUAL(idmap[7], 7);
-            BOOST_CHECK_EQUAL(idmap[8], 6);
+    orient = StdRegions::eDir1FwdDir1_Dir2BwdDir2;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 6);
+    BOOST_CHECK_EQUAL(idmap[1], 7);
+    BOOST_CHECK_EQUAL(idmap[2], 8);
+    BOOST_CHECK_EQUAL(idmap[3], 3);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 5);
+    BOOST_CHECK_EQUAL(idmap[6], 0);
+    BOOST_CHECK_EQUAL(idmap[7], 1);
+    BOOST_CHECK_EQUAL(idmap[8], 2);
 
-            orient = StdRegions::eDir1BwdDir1_Dir2BwdDir2;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 8);
-            BOOST_CHECK_EQUAL(idmap[1], 7);
-            BOOST_CHECK_EQUAL(idmap[2], 6);
-            BOOST_CHECK_EQUAL(idmap[3], 5);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 3);
-            BOOST_CHECK_EQUAL(idmap[6], 2);
-            BOOST_CHECK_EQUAL(idmap[7], 1);
-            BOOST_CHECK_EQUAL(idmap[8], 0);
+    orient = StdRegions::eDir1BwdDir1_Dir2FwdDir2;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 2);
+    BOOST_CHECK_EQUAL(idmap[1], 1);
+    BOOST_CHECK_EQUAL(idmap[2], 0);
+    BOOST_CHECK_EQUAL(idmap[3], 5);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 3);
+    BOOST_CHECK_EQUAL(idmap[6], 8);
+    BOOST_CHECK_EQUAL(idmap[7], 7);
+    BOOST_CHECK_EQUAL(idmap[8], 6);
 
-            orient = StdRegions::eDir1FwdDir2_Dir2FwdDir1;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 0);
-            BOOST_CHECK_EQUAL(idmap[1], 3);
-            BOOST_CHECK_EQUAL(idmap[2], 6);
-            BOOST_CHECK_EQUAL(idmap[3], 1);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 7);
-            BOOST_CHECK_EQUAL(idmap[6], 2);
-            BOOST_CHECK_EQUAL(idmap[7], 5);
-            BOOST_CHECK_EQUAL(idmap[8], 8);
+    orient = StdRegions::eDir1BwdDir1_Dir2BwdDir2;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 8);
+    BOOST_CHECK_EQUAL(idmap[1], 7);
+    BOOST_CHECK_EQUAL(idmap[2], 6);
+    BOOST_CHECK_EQUAL(idmap[3], 5);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 3);
+    BOOST_CHECK_EQUAL(idmap[6], 2);
+    BOOST_CHECK_EQUAL(idmap[7], 1);
+    BOOST_CHECK_EQUAL(idmap[8], 0);
 
-            orient = StdRegions::eDir1FwdDir2_Dir2BwdDir1;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 2);
-            BOOST_CHECK_EQUAL(idmap[1], 5);
-            BOOST_CHECK_EQUAL(idmap[2], 8);
-            BOOST_CHECK_EQUAL(idmap[3], 1);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 7);
-            BOOST_CHECK_EQUAL(idmap[6], 0);
-            BOOST_CHECK_EQUAL(idmap[7], 3);
-            BOOST_CHECK_EQUAL(idmap[8], 6);
+    orient = StdRegions::eDir1FwdDir2_Dir2FwdDir1;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 0);
+    BOOST_CHECK_EQUAL(idmap[1], 3);
+    BOOST_CHECK_EQUAL(idmap[2], 6);
+    BOOST_CHECK_EQUAL(idmap[3], 1);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 7);
+    BOOST_CHECK_EQUAL(idmap[6], 2);
+    BOOST_CHECK_EQUAL(idmap[7], 5);
+    BOOST_CHECK_EQUAL(idmap[8], 8);
 
-            orient = StdRegions::eDir1BwdDir2_Dir2FwdDir1;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 6);
-            BOOST_CHECK_EQUAL(idmap[1], 3);
-            BOOST_CHECK_EQUAL(idmap[2], 0);
-            BOOST_CHECK_EQUAL(idmap[3], 7);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 1);
-            BOOST_CHECK_EQUAL(idmap[6], 8);
-            BOOST_CHECK_EQUAL(idmap[7], 5);
-            BOOST_CHECK_EQUAL(idmap[8], 2);
+    orient = StdRegions::eDir1FwdDir2_Dir2BwdDir1;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 2);
+    BOOST_CHECK_EQUAL(idmap[1], 5);
+    BOOST_CHECK_EQUAL(idmap[2], 8);
+    BOOST_CHECK_EQUAL(idmap[3], 1);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 7);
+    BOOST_CHECK_EQUAL(idmap[6], 0);
+    BOOST_CHECK_EQUAL(idmap[7], 3);
+    BOOST_CHECK_EQUAL(idmap[8], 6);
 
-            orient = StdRegions::eDir1BwdDir2_Dir2BwdDir1;
-            exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
-            BOOST_CHECK_EQUAL(idmap[0], 8);
-            BOOST_CHECK_EQUAL(idmap[1], 5);
-            BOOST_CHECK_EQUAL(idmap[2], 2);
-            BOOST_CHECK_EQUAL(idmap[3], 7);
-            BOOST_CHECK_EQUAL(idmap[4], 4);
-            BOOST_CHECK_EQUAL(idmap[5], 1);
-            BOOST_CHECK_EQUAL(idmap[6], 6);
-            BOOST_CHECK_EQUAL(idmap[7], 3);
-            BOOST_CHECK_EQUAL(idmap[8], 0);
+    orient = StdRegions::eDir1BwdDir2_Dir2FwdDir1;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 6);
+    BOOST_CHECK_EQUAL(idmap[1], 3);
+    BOOST_CHECK_EQUAL(idmap[2], 0);
+    BOOST_CHECK_EQUAL(idmap[3], 7);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 1);
+    BOOST_CHECK_EQUAL(idmap[6], 8);
+    BOOST_CHECK_EQUAL(idmap[7], 5);
+    BOOST_CHECK_EQUAL(idmap[8], 2);
 
-        }
-    }
-}
+    orient = StdRegions::eDir1BwdDir2_Dir2BwdDir1;
+    exp3d->ReOrientFacePhysMap(nvert, orient, nq0, nq1, idmap);
+    BOOST_CHECK_EQUAL(idmap[0], 8);
+    BOOST_CHECK_EQUAL(idmap[1], 5);
+    BOOST_CHECK_EQUAL(idmap[2], 2);
+    BOOST_CHECK_EQUAL(idmap[3], 7);
+    BOOST_CHECK_EQUAL(idmap[4], 4);
+    BOOST_CHECK_EQUAL(idmap[5], 1);
+    BOOST_CHECK_EQUAL(idmap[6], 6);
+    BOOST_CHECK_EQUAL(idmap[7], 3);
+    BOOST_CHECK_EQUAL(idmap[8], 0);
+};
+
+} // namespace Expansion3DTests
+} // namespace Nektar

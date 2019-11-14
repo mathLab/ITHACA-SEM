4548aa598abf2a8a6733985fe075197986bb289a
David Moxey <dave@xyloid.org>
2019-11-14 11:23:50 +0000

    Add option for fixed-width stylesheet and remove a few noisy Doxygen warnings

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,5 +1,6 @@
 # Doxygen support
-# adapted from http://www.bluequartz.net/projects/EIM_Segmentation/             SoftwareDocumentation/html/usewithcmakeproject.html
+# adapted from http://www.bluequartz.net/projects/EIM_Segmentation/
+#    SoftwareDocumentation/html/usewithcmakeproject.html
 OPTION(NEKTAR_BUILD_DOC "Build source code documentation using doxygen" OFF)
 
 CMAKE_DEPENDENT_OPTION(NEKTAR_BUILD_DOC_QHP 
@@ -10,6 +11,9 @@ CMAKE_DEPENDENT_OPTION(NEKTAR_BUILD_DOC_XCODE
     "NEKTAR_BUILD_DOC" OFF)
 CMAKE_DEPENDENT_OPTION(NEKTAR_BUILD_DOC_ECLIPSE
     "Use Doxygen to create documentation for Eclipse" OFF
+    "NEKTAR_BUILD_DOC" OFF)
+CMAKE_DEPENDENT_OPTION(NEKTAR_BUILD_DOC_FIXEDWIDTH
+    "Use a fixed-width style sheet for doxygen output" OFF
     "NEKTAR_BUILD_DOC" OFF)
 
 IF (NEKTAR_BUILD_DOC)
@@ -28,6 +32,11 @@ IF (NEKTAR_BUILD_DOC)
         SET(DOXYGEN_GENERATE_ECLIPSEHELP "YES")
     ENDIF()
 
+    SET(DOXYGEN_EXTRA_CSS "")
+    IF(NEKTAR_BUILD_DOC_FIXEDWIDTH)
+        SET(DOXYGEN_EXTRA_CSS "docs/doxygen/doxygen-fixed-width.css")
+    ENDIF()
+
     INSTALL(DIRECTORY ${PROJECT_BINARY_DIR}/docs/doxygen/html/
         DESTINATION ${NEKTAR_DOC_DIR}/doxygen)
 
@@ -35,6 +44,9 @@ IF (NEKTAR_BUILD_DOC)
                    ${PROJECT_BINARY_DIR}/docs/doxygen/Doxyfile @ONLY IMMEDIATE)
 
     ADD_CUSTOM_TARGET(doc
+        COMMAND ${CMAKE_COMMAND} -E copy
+            ${CMAKE_SOURCE_DIR}/docs/doxygen/doxygen-fixed-width.css
+            ${PROJECT_BINARY_DIR}/docs/doxygen/doxygen-fixed-width.css
         COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/docs/doxygen/Doxyfile
         SOURCES ${PROJECT_BINARY_DIR}/docs/doxygen/Doxyfile)
 

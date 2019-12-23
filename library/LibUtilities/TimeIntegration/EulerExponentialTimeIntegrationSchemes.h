aa78286295c14aa4d1e6e124e515b14ed9244031
allen <allen@sci.utah.edu>
2019-12-23 13:37:28 -0700

    added member var order and whitespace clean up

--- library/LibUtilities/TimeIntegration/EulerExponentialTimeIntegrationSchemes.h
+++ library/LibUtilities/TimeIntegration/EulerExponentialTimeIntegrationSchemes.h
@@ -63,11 +63,11 @@ class EulerExponentialTimeIntegrationScheme : public TimeIntegrationScheme
 public:
     EulerExponentialTimeIntegrationScheme() : TimeIntegrationScheme()
     {
-        unsigned int order = 1;
+        m_order = 3;
 
-        m_integration_phases = TimeIntegrationSchemeDataVector(order);
+        m_integration_phases = TimeIntegrationSchemeDataVector(m_order);
 
-        for( unsigned int n=0; n<order; ++n )
+        for( unsigned int n=0; n<m_order; ++n )
         {
             m_integration_phases[n] = TimeIntegrationSchemeDataSharedPtr(
                 new TimeIntegrationSchemeData(this));
@@ -113,8 +113,8 @@ public:
 
         // Coefficients
 
-	// When multiple steps are taken B[0][0] and V[0][1...s] must be
-	// weighted so the time contribution is correct.
+        // When multiple steps are taken B[0][0] and V[0][1...s] must be
+        // weighted so the time contribution is correct.
 
         // B Phi function for first row first column
         phase->m_B[0][0][0] = 1.0 / phase->m_order; // phi_func(0 or 1)
@@ -183,103 +183,103 @@ public:
         
         for( unsigned int k=0; k<phase->m_nvars; ++k )
         {
-	    NekDouble phi[phase->m_order];
+            NekDouble phi[phase->m_order];
 
-	    // B Phi function for first row first column
-	    if( GetName() == "LawsonEuler" )
-	    {
-		phi[0] =
-		  phi_function(0, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
-	    }
-	    else if( GetName() == "NorsettEuler" )
-	    {
-		phi[0] =
-		  phi_function(1, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
-	    }
-	    else
-	    {
+            // B Phi function for first row first column
+            if( GetName().find( "LawsonEuler" ) == 0 )
+            {
+                phi[0] =
+                  phi_function(0, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
+            }
+            else if( GetName().find( "NorsettEuler" ) == 0 )
+            {
+                phi[0] =
+                  phi_function(1, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
+            }
+            else
+            {
               ASSERTL1(false,
                        "Cannot call EulerExponential directly "
                        "use LawsonEuler or NorsettEuler.");
-	    }
+            }
 
-	    // Set up for multiple steps. For multiple steps one needs
-	    // to weight the phi functions in much the same way there
-	    // are weights for multi-step Adams-Bashfort methods.
+            // Set up for multiple steps. For multiple steps one needs
+            // to weight the phi functions in much the same way there
+            // are weights for multi-step Adams-Bashfort methods.
 
-	    // For order N the weights are an N x N matrix with
-	    // values: W[j][i] = std::pow(i, j) and phi_func = W phi.
-	    // There are other possible wieghting schemes
-	    if( phase->m_order == 1 )
-	    {
-	        // Nothing to do as the value is set above and the
-	        // wieght is just 1.
-	    }
-	    else if( phase->m_order == 2 )
-	    {
-	        // For Order 2 the weights are simply : 1 1
-	        //                                      0 1
+            // For order N the weights are an N x N matrix with
+            // values: W[j][i] = std::pow(i, j) and phi_func = W phi.
+            // There are other possible wieghting schemes
+            if( phase->m_order == 1 )
+            {
+                // Nothing to do as the value is set above and the
+                // wieght is just 1.
+            }
+            else if( phase->m_order == 2 )
+            {
+                // For Order 2 the weights are simply : 1 1
+                //                                      0 1
 
-	        // If one were to solve the system of equations it
-	        // simply results in subtracting the second order
-	        // value from the first order value.
-		phi[1] =
-		    phi_function(2, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
+                // If one were to solve the system of equations it
+                // simply results in subtracting the second order
+                // value from the first order value.
+                phi[1] =
+                    phi_function(2, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
 
-		phi[0] -= phi[1];
-	    }
-	    else if( phase->m_order == 3 )
-	    {
-	        NekDouble phi_func[phase->m_order];
+                phi[0] -= phi[1];
+            }
+            else if( phase->m_order == 3 )
+            {
+                NekDouble phi_func[phase->m_order];
 
-		phi_func[0] = phi[0];
-		phi_func[1] =
-		    phi_function(2, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
-		phi_func[2] =
-		    phi_function(3, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
+                phi_func[0] = phi[0];
+                phi_func[1] =
+                    phi_function(2, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
+                phi_func[2] =
+                    phi_function(3, deltaT, phase->m_L[0][k], phase->m_L[1][k]);
 
-		NekDouble W[3][3];
+                NekDouble W[3][3];
 
-		// Set up teh wieghts and calculate the determinant.
-		// for( unsigned int j=0; j<phase->m_order; ++j )
-		// {
-		//     for( unsigned int i=0; i<phase->m_order; ++i )
-		//     {
-		// 	W[j][i] = std::pow(i, j);
-		//     }
-		// }
-		// NekDouble W_det = Determinant<3>(W);
+                // Set up teh wieghts and calculate the determinant.
+                // for( unsigned int j=0; j<phase->m_order; ++j )
+                // {
+                //     for( unsigned int i=0; i<phase->m_order; ++i )
+                //     {
+                //      W[j][i] = std::pow(i, j);
+                //     }
+                // }
+                // NekDouble W_det = Determinant<3>(W);
 
-		// No need to calculate the determinant value as it is fixed;
-		NekDouble W_det = 2.0;
+                // No need to calculate the determinant value as it is fixed;
+                NekDouble W_det = 2.0;
 
-		// Solve the series of equations using Cramer's rule.
-		for( unsigned int m=0; m<phase->m_order; ++m )
-		{
-		    // Assemble the working matrix for this solution.
-		    for( unsigned int j=0; j<phase->m_order; ++j )
-		    {
-		        for( unsigned int i=0; i<phase->m_order; ++i )
-			{
-			    // Fill in the mth column for the mth
-			    // solution using the phi function value
-			    // otherwise utilize the weights.
-			    W[i][j] = (j == m) ? phi_func[i] : std::pow(j, i);
-			}
-		    }
+                // Solve the series of equations using Cramer's rule.
+                for( unsigned int m=0; m<phase->m_order; ++m )
+                {
+                    // Assemble the working matrix for this solution.
+                    for( unsigned int j=0; j<phase->m_order; ++j )
+                    {
+                        for( unsigned int i=0; i<phase->m_order; ++i )
+                        {
+                            // Fill in the mth column for the mth
+                            // solution using the phi function value
+                            // otherwise utilize the weights.
+                            W[i][j] = (j == m) ? phi_func[i] : std::pow(j, i);
+                        }
+                    }
 
-		    // Get the mth solutiion.
-		    phi[m] = Determinant<3>(W) / W_det;
-		}
-	    }
-	    else
-	    {
+                    // Get the mth solutiion.
+                    phi[m] = Determinant<3>(W) / W_det;
+                }
+            }
+            else
+            {
                 ASSERTL1(false, "Not set up for more than 3rd Order.");
-	    }
+            }
 
-	    // Create the phi based Butcher tableau matrices. Note
-	    // these matrices are set up using a general formational
-	    // based on the number of steps (i.e. the order).
+            // Create the phi based Butcher tableau matrices. Note
+            // these matrices are set up using a general formational
+            // based on the number of steps (i.e. the order).
             phase->m_A_phi[k] =
               Array<TwoD, NekDouble>(phase->m_numstages, phase->m_numstages, 0.0);
             phase->m_B_phi[k] =
@@ -290,7 +290,7 @@ public:
               Array<TwoD, NekDouble>(phase->m_numsteps, phase->m_numsteps, 0.0);
 
             // B Phi function for first row first column.
-	    phase->m_B_phi[k][0][0] = phi[0];
+            phase->m_B_phi[k][0][0] = phi[0];
 
             // B evaluation value shuffling first row second column.
             if( phase->m_order > 1 )
@@ -306,7 +306,7 @@ public:
             // V Phi function for first row additional columns.
             for( int n=1; n<phase->m_order; ++n )
             {
-	        phase->m_V_phi[k][0][n] = phi[n];
+                phase->m_V_phi[k][0][n] = phi[n];
             }
 
             // V evaluation value shuffling row n column n-1.
@@ -318,6 +318,10 @@ public:
 
         std::cout << *phase << std::endl;
     }
+
+    protected:
+        unsigned int m_order;
+
 }; // end class EulerExponentialTimeIntegrator
 
 class LawsonEulerTimeIntegrationScheme : public EulerExponentialTimeIntegrationScheme
@@ -334,7 +338,7 @@ public:
 
     LUE virtual std::string GetName() const
     {
-        return std::string("LawsonEuler");
+        return std::string("LawsonEuler" + std::to_string(m_order));
     }
 }; // end class LawsonEulerTimeIntegrationScheme
   
@@ -352,7 +356,7 @@ public:
 
     LUE virtual std::string GetName() const
     {
-        return std::string("NorsettEuler");
+        return std::string("NorsettEuler" + std::to_string(m_order));
     }
 }; // end class NorsettEulerTimeIntegrationScheme
   

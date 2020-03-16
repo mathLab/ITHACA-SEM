578c9e11df9a93033a39f642adabd86257d17f49
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2020-03-16 15:16:47 +0000

    Modify MPIEXEC path on Win32 so parallel test run.

--- cmake/ThirdPartyMPI.cmake
+++ cmake/ThirdPartyMPI.cmake
@@ -27,6 +27,14 @@ IF( NEKTAR_USE_MPI )
 
         INCLUDE_DIRECTORIES(SYSTEM ${MPI_CXX_INCLUDE_PATH} )
         MESSAGE(STATUS "Found MPI: ${MPI_CXX_LIBRARIES}")
+		# If we're using WIN32, mpiexec is likely to be in a directory with
+		# a space in the path so we need to quote the path so that it is run
+		# correctly by the Tester...
+		MESSAGE(STATUS "Found MPI Executable: ${MPIEXEC}")
+		IF (WIN32)
+			SET(MPIEXEC "\\\"${MPIEXEC}\\\"" CACHE STRING "MPI job launching command")
+			MESSAGE(STATUS "Updated MPIEXEC for Win32 platform: ${MPIEXEC}")
+		ENDIF ()
     ELSE()
         SET(MPI_BUILTIN ON)
         MESSAGE(STATUS "Found MPI: built in")

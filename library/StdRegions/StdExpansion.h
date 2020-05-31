e73fb3ef75f3a47419838d08f4745dc8d2613b77
vidhi <vidhi.zala@utah.edu>
2020-05-31 16:41:54 -0600

    changed int to auto

--- library/StdRegions/StdExpansion.h
+++ library/StdRegions/StdExpansion.h
@@ -1594,7 +1594,7 @@ namespace Nektar
                 const Array<OneD, const NekDouble> &bw =
                     m_base[DIR]->GetBaryWeights();
 
-                const int nquad = z.size();
+                const auto nquad = z.size();
 
                 for (int i = 0; i < nquad; ++i)
                 {

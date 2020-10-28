62cd909abbdd201e51a8c9d7116466f90d6cb17d
gl19 <g.lyu19@imperial.ac.uk>
2020-10-28 02:52:43 +0000

    Oct 28. Better organized.

--- library/FieldUtils/ProcessModules/ProcessNFactor.cpp
+++ library/FieldUtils/ProcessModules/ProcessNFactor.cpp
@@ -38,8 +38,6 @@
 #include "ProcessNFactor.h"
 
 #include <LibUtilities/Foundations/Interp.h>
-
-
 #include <LibUtilities/BasicUtils/SharedArray.hpp>
 #include <MultiRegions/ExpList.h>
 
@@ -153,8 +151,6 @@ int ProcessNFactor::CleanRepeatedPts(Array<OneD, Array<OneD, NekDouble> > A)
 }
 
 
-
-
 void ProcessNFactor::Process(po::variables_map &vm)
 {
     ProcessBoundaryExtract::Process(vm);
@@ -173,25 +169,22 @@ void ProcessNFactor::Process(po::variables_map &vm)
     // Sampling setting
     const NekDouble distance_n = 0.005; // from wall to wall + H in normal direction
     const int       npts_n     = 21;    // npts in wall normal direction, use npts points for export
-    const NekDouble delta = 0.1;        //
+    const NekDouble delta = 0.1;        // needs to be smaller than 1.
     //-------------------------------------------------------------------------
 
-    
-    int i;
+    // Step 1 - get data type
     int nfields = m_f->m_variables.size();
     int expdim  = m_f->m_graph->GetSpaceDimension();
     m_spacedim  = expdim + m_f->m_numHomogeneousDir;
 
-    std::cout<<"Inside the N-factor module!" << std::endl;
-    std::cout<< nfields<< ", " << expdim << ", " << m_spacedim <<std::endl;
-    std::cout<< m_f->m_exp[0]->GetNumElmts() <<std::endl;
-
-    // Declare arrays
-    // This part needs to be updated to suitable for compressible cases. [!]
-    Array<OneD, MultiRegions::ExpListSharedPtr> BndExp(m_spacedim + 1); //uvw+p
-    Array<OneD, MultiRegions::ExpListSharedPtr> BndElmtExp(nfields);
+    string str_inc = "u";
+    string str_com = "rho";
+    if      (m_f->m_variables[0].compare(str_inc)==0){cout <<"incompressible"<<endl;}
+    else if (m_f->m_variables[0].compare(str_com)==0){cout <<"compressible"<<endl;}
+    else {ASSERTL0(false, "Other type of fields, not coded yet.");}
 
 
+    // Step 2 - get boundary info
     // Create map of boundary ids for partitioned domains
     SpatialDomains::BoundaryConditions bcs(m_f->m_session,
                                            m_f->m_exp[0]->GetGraph());
@@ -204,63 +197,57 @@ void ProcessNFactor::Process(po::variables_map &vm)
         BndRegionMap[breg_it.first] = cnt++;
     }
 
+    // Get boundary id
     // m_f->m_bndRegionsToWrite.size() is the number of input bnd
     // eg. =3 if bnd=0,1,2; =1 if bnd=0
     int bnd = BndRegionMap[m_f->m_bndRegionsToWrite[0]];
-    cout << "bnd = " << bnd << endl;
-
-    // Get expansion list for boundary and for elements containing this
-    // bnd
-    for (i = 0; i < (m_spacedim + 1); ++i) {
+    
+    // Get expansion list for boundary and the number of points
+    Array<OneD, MultiRegions::ExpListSharedPtr> BndExp(nfields); 
+    for (int i = 0; i < nfields; ++i) {
         BndExp[i] = m_f->m_exp[i]->UpdateBndCondExpansion(bnd);
     }
-    for (i = 0; i < nfields; i++) {
-        m_f->m_exp[i]->GetBndElmtExpansion(bnd, BndElmtExp[i]);
-    }
-
-
-    // Get number of points in expansions
-    // For 2.5D cases, nqb = numElem * numPoints * HomModesZ
-    // where numPoints = numModes + 1 = P + 2 by default
-    // and nqe = nqb * numPoints. Why is this?
-    int nqb = BndExp[0]->GetTotPoints();
-    int nqe = BndElmtExp[0]->GetTotPoints(); // seems to be not used [!]
-
-    // Get inward-pointing wall-normal vectors 
+    int nqb = BndExp[0]->GetTotPoints(); // points for all HomModesZ planes
+    
+    // Get inward-pointing wall-normal vectors for all points on bnd
     Array<OneD, Array<OneD, NekDouble> > normals; 
     m_f->m_exp[0]->GetBoundaryNormals(bnd, normals);
-    // Reverse normals, to get correct orientation for the body
-    // normals[i][j], where i is direction (x/y/z) varying from 0 to 2;
-    // j is the point varying from 0 to (nqb-1)
-    for (i = 0; i < m_spacedim; ++i) {
+    for (int i = 0; i < m_spacedim; ++i) {
         Vmath::Neg(nqb, normals[i], 1);
     }
-    
-    cout <<"normals1 "<< normals[1][nqb-2]<<" "<< normals[1][nqb-1] << endl; 
-    cout << "nqb = " << nqb << ", nqe = " << nqe <<endl;
-
-
+ 
+    // Get coordinates for all points on the bnd
     Array<OneD, Array<OneD, NekDouble> > xyz_bnd(3);
     for (int i=0; i<3; ++i) {
         xyz_bnd[i] = Array<OneD, NekDouble>(nqb, 0.0);
     }
-    BndExp[0]->GetCoords(xyz_bnd[0],xyz_bnd[1],xyz_bnd[2]);
-
-    for (int i=0;i<nqb/4;++i){   // 0 ~ nqb/4, where 4 if for HomModesZ=4
-        cout << i << " - " <<xyz_bnd[0][i] <<", "<<xyz_bnd[1][i]<<", "<<xyz_bnd[2][i]<<endl;
-    }
-
+    BndExp[0]->GetCoords(xyz_bnd[0], xyz_bnd[1], xyz_bnd[2]);
 
     //=========================================================================
-    //------------------------------ Set origins ------------------------------
+    // Step 3 - set origins for sampling
+    // only support 1D interpolation now
     // set dimensions
     const int dim_para = BndExp[0]->GetExp(0)->GetNumBases(); // dimension for parametric coordinate system, eg. =1
     const int dim_phys = BndExp[0]->GetCoordim(0); // dimension for the physical space that the parametric coordinate system located on, eg. =2
-    cout << "dim_para = " << dim_para <<", dim_coor = " << dim_phys <<endl;
-
+    
     // set point key
+    LibUtilities::PointsType to_pointstype = LibUtilities::PointsType::ePolyEvenlySpaced;
+    /*
+    if (dim_para==1) {
+        to_pointstype = LibUtilities::PointsType::ePolyEvenlySpaced;
+    } 
+    else {
+        // dim_para=2, the bnd element could be quadrilateral or triangular  
+        if () {
+
+        }
+        else {
+
+        }
+    }
+    */
     LibUtilities::PointsKey from_key = BndExp[0]->GetExp(0)->GetBasis(0)->GetPointsKey();
-    LibUtilities::PointsKey to_key( to_nPtsPerElmt, LibUtilities::PointsType::ePolyEvenlySpaced ); //[!] important!
+    LibUtilities::PointsKey to_key(to_nPtsPerElmt, to_pointstype); //[!] important!
     const int from_nPtsPerElmt = from_key.GetNumPoints();
 
     // declare arrays to save points
@@ -309,212 +296,156 @@ void ProcessNFactor::Process(po::variables_map &vm)
 
     }
  
-    // sort array 
-    /*
-    // Build max heap, starting from the last non-leaf node
-    for (int i = floor(nOrigs / 2) - 1; i >= 0; --i) {
-        heapify_max(origs, nOrigs, i);
-    }
-    // Move current root to end [0]->[length-1]
-    // and adjust the reduced heap, startig from root
-    for (int length = nOrigs; length > 1; --length) {
-        for (int j=0; j<6; ++j) { std::swap(origs[j][0], origs[j][length-1]); }
-
-        heapify_max(origs, length-1, 0);
-    }
-    */
+    // sort array and remove repeated origin points
     HeapSort(origs);
-
-    // remove repeated origin points
-    /*
-    int nOrigs_new = nOrigs; // new length
-    for (int i=1; i<nOrigs_new; ++i) {
-        // For each i, check indax smaller than i
-        for (int j=i-1; j>=0; --j) {
-            
-            if ( abs(origs[0][i]-origs[0][j])>NekConstants::kNekZeroTol ) {
-                break; // The array has already sorted regards to x
-            }
-            else {
-                if (abs(origs[1][i]-origs[1][i-1])<NekConstants::kNekZeroTol &&
-                    abs(origs[2][i]-origs[2][i-1])<NekConstants::kNekZeroTol) {
-                    
-                    // repeated points found, [i]==[j]
-                    // move [i+1] to [nOrigs-1] forword
-                    for (int k=0; k<6; ++k) {
-                        for (int t=i+1; t<nOrigs; ++t) {
-                            origs[k][t-1] = origs[k][t];
-                        }
-                        origs[k][nOrigs-1] = origs[k][j];
-                    }
-
-                    --nOrigs_new; // key origins -- 
-                    --i;         // check the same i again
-                }
-            }
-
-        }
-    }
-    */
     int nOrigs_new = CleanRepeatedPts(origs);
-
-    cout << "len1 = "<< nOrigs <<", len2 = " << nOrigs_new << endl;
-    for (int j=0; j<nOrigs_new; ++j){
-        cout <<"-array_3- " << origs[0][j] <<", "<< origs[1][j]<<", "<< origs[2][j] <<", "
-                            << origs[3][j] <<", "<< origs[4][j]<<", "<< origs[5][j] <<endl;
-    }  
+     
     //-------------------------------------------------------------------------
     //=========================================================================
 
-    
-
-    // Sampling points
+    // Step 4 - set sampling points
     // h = 1- tanh((1-ksi)*atanh(sqrt(1-delta)))/sqrt(1-delta), ksi in [0,1]
     // from Agrawal's paper
     Array<OneD, NekDouble> h(npts_n);
-    
     NekDouble tmp1;
     const NekDouble tmp2 = 1.0/(static_cast<NekDouble>(npts_n)-1.0); // 1/(npts-1)
     const NekDouble tmp3 = sqrt(1.0-delta);
     const NekDouble tmp4 = atanh(tmp3);
     const NekDouble tmp5 = 1.0/tmp3;
-    for (int i=0;i<npts_n;++i){
+    for (int i=0; i<npts_n; ++i){
         tmp1 = 1.0 - i * tmp2; // tmp1 = 1-ksi, ksi = i/(npts_n-1) belonging to [0,1]
         h[i] = 1 - tanh(tmp1*tmp4)*tmp5;
         cout << i<<" - ksi = "<<1-tmp1<<", h = "<< h[i] <<endl;
     }
 
-    // Get the sampled y/z on the wall according to given x
-    // Call NekMesh or OpenCascade routine to get y/z based on x array
-    // or directly interpolate using the available x/y/z/normals data on the bnd
-    // temporarily use the following point, assume it's already on the wall
-    NekDouble x_middle = (0.19+0.4)/2;
-    NekDouble y_middle = 0.0;
-    NekDouble z_middle = 0.0;
-    NekDouble normals_x = 0.0;
-    NekDouble normals_y = 1.0;
-    NekDouble normals_z = 0.0;
-
-
-    // Get target x/y/z array
-    Array<OneD, NekDouble> x_target(npts_n, x_middle);
-    Array<OneD, NekDouble> y_target(npts_n, y_middle);
-    Array<OneD, NekDouble> z_target(npts_n, z_middle);
-
-    for (int i=0;i<npts_n;++i){
-        x_target[i] = x_target[i] + distance_n * h[i] * normals_x;
-        y_target[i] = y_target[i] + distance_n * h[i] * normals_y;
-        z_target[i] = z_target[i] + distance_n * h[i] * normals_z;
+    // declare the data array and fill in the coordinates
+    // data[originId][normalId][variableId]
+    Array<OneD, Array<OneD, Array<OneD, NekDouble> > > data(nOrigs_new);
+    for (int i=0; i<nOrigs_new; ++i) {
+        data[i] = Array<OneD, Array<OneD, NekDouble> >(npts_n);
+        for (int j=0; j<npts_n; ++j) {
+            data[i][j] = Array<OneD, NekDouble>(3+BndExp.size(), 0.0); //x/y/z+flow variables
+            
+            tmp1 = distance_n*h[j]; // physical distance in normal direction
+            for (int k=0; k<3; ++k) {
+                data[i][j][k] = origs[k][i] + tmp1*origs[k+3][i]; // x0+dist*nx
+            }
+        }
     }
 
-    for(int i=0;i<npts_n;++i){
-         cout << "#"<<i<<"-- "<<x_target[i]<<", "<<y_target[i]<<", "<<z_target[i]<<endl;
-    }
-   
-
-    // Get variables' value
-    // Mimic the interpolation procedure in 
-    // /disk_two/Nek_Test/nektar++/library/FieldUtils/Interpolator.cpp
-    int nCoordDim = m_f->m_exp[0]->GetCoordim(0); // tell the difference between nCoorDim and Dimension of the space
-    cout << "Dimension = " << nCoordDim <<endl;
-
+    // Step 5 - interpolate the variables for each point
+    int nCoordDim = m_f->m_exp[0]->GetCoordim(0);
     Array<OneD, NekDouble> Lcoords(nCoordDim, 0.0); 
     Array<OneD, NekDouble> coords(3);
-    coords[0] = x_target[20]; //0
-    coords[1] = y_target[20]; //
-    coords[2] = z_target[20]; //
+   
+    for (int i=0; i<nOrigs_new; ++i) {
+        for (int j=0; j<npts_n; ++j) {
 
-    cout << "Point: "<<coords[0]<<", "<< coords[1]<<", "<<coords[2]<<endl;
+            // Get donor element and local coordinates
+            Vmath::Vcopy(3, &data[i][j][0], 1, &coords[0], 1);
+            int elmtid = -1;
+            elmtid = m_f->m_exp[0]->GetExpIndex(coords, Lcoords, 
+                     NekConstants::kGeomFactorsTol, false, elmtid); 
 
-    // Get donor element and local coordinates
-    int elmtid = -1;
-    elmtid = m_f->m_exp[0]->GetExpIndex(
-            coords, Lcoords, NekConstants::kGeomFactorsTol, false, elmtid); // Get donor elmt 
-    cout <<"elmtid = "<<elmtid<<endl;
-    cout <<"Local coord: "<< Lcoords[0] <<", "<< Lcoords[1]<<endl;//", "<< Lcoords[2]<<endl;
+            // Homogeneous case, need to find the right plane
+            int targetPlane = -1;
+            if (m_f->m_exp[0]->GetExpType() == MultiRegions::e3DH1D) {
+                int nPlanes    = m_f->m_exp[0]->GetHomogeneousBasis()->GetZ().size();
+                NekDouble lHom = m_f->m_exp[0]->GetHomoLen();
+                targetPlane = std::round((coords[2]*nPlanes)/lHom);
 
-    // Homogeneous case, need to find the right plane
-    int targetPlane = -1;
-    if (m_f->m_exp[0]->GetExpType() == MultiRegions::e3DH1D) {
+                // Reset from last plane to plane 0 (periodic bnd)
+                if(targetPlane == nPlanes) {targetPlane = 0;}
+            }
 
-        cout << "e3DH1D" << endl;
+            // limit Lcoords to [-1,1]
+            for (int k = 0; k < nCoordDim; ++k) {
+                Lcoords[k] = std::max(Lcoords[k], -1.0);
+                Lcoords[k] = std::min(Lcoords[k],  1.0);
+            }
 
-        int nPlanes    = m_f->m_exp[0]->GetHomogeneousBasis()->GetZ().size();
-        NekDouble lHom = m_f->m_exp[0]->GetHomoLen();
-        targetPlane = std::round((coords[2]*nPlanes)/lHom);
-
-        cout << "Plane = " << targetPlane << endl;
-
-        // Reset from last plane to plane 0 (same physical result)
-        if(targetPlane == nPlanes) {
-            targetPlane = 0;
-        }
-    }
-
-    // limit Lcoords to avoid warnings, ref Interpolator.cpp
-    for (int j = 0; j < 2; ++j) {
-        Lcoords[j] = std::max(Lcoords[j], -1.0);
-        Lcoords[j] = std::min(Lcoords[j], 1.0);
-    }
-    
-    // interpolate the value for each field
-    int offset;
-    NekDouble value;
-    if (elmtid >= 0) {
-        // Get offset
-        offset = m_f->m_exp[0]->GetPhys_Offset(elmtid);
+            //-------------
+            // interpolate the value for each field
+            int offset;
+            NekDouble value;
+            if (elmtid >= 0) {
+                // Get offset
+                offset = m_f->m_exp[0]->GetPhys_Offset(elmtid); 
         
-        // interpolate each field
-        for (int f = 0; f < m_f->m_exp.size(); ++f) {
-            // interpolate a field
-            if (m_f->m_exp[0]->GetExpType() == MultiRegions::e3DH1D){
-                // 2.5D case, interpolate on the target plane
-                auto planeExp = m_f->m_exp[f]->GetPlane(targetPlane);
-                value         = planeExp->GetExp(elmtid)->StdPhysEvaluate(
-                    Lcoords, planeExp->GetPhys() + offset);
+                // interpolate each field
+                for (int f = 0; f < m_f->m_exp.size(); ++f) {
+                    // interpolate a field
+                    if (m_f->m_exp[0]->GetExpType() == MultiRegions::e3DH1D){
+                        // 2.5D case, interpolate on the target plane
+                        auto planeExp = m_f->m_exp[f]->GetPlane(targetPlane);
+                        value         = planeExp->GetExp(elmtid)->StdPhysEvaluate(
+                                        Lcoords, planeExp->GetPhys() + offset);
+                    }
+                    else {
+                        // 2D/3D and other cases
+                        value = m_f->m_exp[f]->GetExp(elmtid)->StdPhysEvaluate(
+                                Lcoords, m_f->m_exp[f]->GetPhys() + offset);
+                    }
+
+                    // Check and save
+                    if ((boost::math::isnan)(value)) {
+                        ASSERTL0(false, "NaN for interpolation.");
+                    }    
+                    else {
+                        data[i][j][3+f] = value;
+                    }
+
+                } // loop f for diferent field
             }
             else {
-                // 1D/2D/3D and other cases [?]
-                value = m_f->m_exp[f]->GetExp(elmtid)->StdPhysEvaluate(
-                    Lcoords, m_f->m_exp[f]->GetPhys() + offset);
+                ASSERTL0(false, "Incorrect Id for donor element.");
             }
 
-            // Check and save
-            if ((boost::math::isnan)(value)){
-                ASSERTL0(false, "new value is not a number");
-            }    
-            else {
-                // Save the value
-                //m_ptsOutField->SetPointVal(m_ptsOutField->GetDim() + f, i, value);
-                cout <<value <<endl;
-            }
+        } // loop j for normal array
+    } // loop i for orgins 
+    
+
+    //---------------output some  middle results --------------------
+    std::cout<< nfields<< ", " << expdim << ", " << m_spacedim <<std::endl;
+    std::cout << "dim_para = " << dim_para <<", dim_coor = " << dim_phys <<std::endl;
+    std::cout<< m_f->m_exp[0]->GetNumElmts() <<std::endl;
+    std::cout<< m_f->m_variables[0]<<", "<<m_f->m_variables[1]<<", "
+             << m_f->m_variables[2]<<", "<<m_f->m_variables[3]<<endl;
+    cout << "m_f->m_exp.size() = " << m_f->m_exp.size() <<endl;
+
+    cout << "bnd = " << bnd << endl;
+    cout << "nqb = " << nqb << endl;
+    cout << "normals1 "<< normals[1][nqb-2]<<" "<< normals[1][nqb-1] << endl; 
+    cout << "Dimension = " << nCoordDim <<endl;
+
+    for (int i=0;i<nqb/4;++i){   // 0 ~ nqb/4, where 4 if for HomModesZ=4
+        cout << i << " - " <<xyz_bnd[0][i] <<", "<<xyz_bnd[1][i]<<", "<<xyz_bnd[2][i]<<endl;
+    }
+
+    cout << "len1 = "<< nOrigs <<", len2 = " << nOrigs_new << endl;
+    for (int j=0; j<nOrigs_new; ++j){
+        cout <<"-array_3- " << origs[0][j] <<", "<< origs[1][j]<<", "<< origs[2][j] <<", "
+                            << origs[3][j] <<", "<< origs[4][j]<<", "<< origs[5][j] <<endl;
+    } 
+
+
+    //---------------------------------------------------------------
+    int i=0;
+    cout << "======Result check======\nInput an origin index:"<<endl;
+    cin >> i;
+    while (i>=0 && i<nOrigs_new) {
+        
+        for (int j=0; j<npts_n; ++j) {
+            cout << "#"<<j<<" - [" <<data[i][j][0] <<", "<<data[i][j][1]<<", "<< data[i][j][2]<<"]\n     "
+                 <<data[i][j][3] <<", "<<data[i][j][4]<<", "<< data[i][j][5]<<", "<< data[i][j][6]<<endl;
+
         }
+        system("pause");
+        cout << "Dumped.\nInput a new origin index:"<<endl;
+        cin >> i;
     }
-    else {
-        cout << "Not coded yet."<<endl;
-    }
+    //---------------------------------------------------------------
 
-
-
-
-
-
-    //----------
-    /*
-    if (m_spacedim == 2)
-    {
-        m_f->m_variables[0] = "Shear_x";
-        m_f->m_variables[1] = "Shear_y";
-        m_f->m_variables[2] = "Shear_mag";
-    }
-    else
-    {
-        m_f->m_variables[0] = "Shear_x";
-        m_f->m_variables[1] = "Shear_y";
-        m_f->m_variables[2] = "Shear_z";
-        m_f->m_variables[3] = "Shear_mag";
-    }
-    */
 }
 
 

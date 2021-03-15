97b9573a127585411d5dd45dc38ae1001658157b
Daniel Lindblad <d.lindblad@imperial.ac.uk>
2021-03-15 07:58:14 +0100

    Fix for comparing Nodes and Floats
    Updated minimum boost version to 1.60.0 to ensure that the float
    comparison routines in boost used by RealComparison() are available.
    These routines were not available prior to 1.60.0.

--- cmake/ThirdPartyBoost.cmake
+++ cmake/ThirdPartyBoost.cmake
@@ -10,7 +10,7 @@
 MESSAGE(STATUS "Searching for Boost:")
 
 # Minimum version and boost libraries required
-SET(MIN_VER "1.56.0")
+SET(MIN_VER "1.60.0")
 SET(NEEDED_BOOST_LIBS thread iostreams filesystem system program_options regex)
 
 SET(Boost_NO_BOOST_CMAKE ON)
